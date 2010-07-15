adobe/chromium

commit 15a963238d6a814adbb7fd4cc9578b1dfc420ddf

Implement side tab view and controller and hook up their creation. Parameterize tab strip layout so it can be vertical in addition to horizontal without breaking up the code too much. Abstracted some of the side tab knowledge into TabWindowController with overrides in BrowserWindowController. Nib change: added a SideTabStripView custom view to BrowserWindow and hooked it to an outlet. Renamed the outlets to better reflect both views. BUG=44773 TEST=Tab layout, full screen, dragging tabs within and to other windows to make sure they reflow and draw correctly. Side tabs themselves are still behind a flag, but this cl touches many normal codepaths. Review URL: http://codereview.chromium.org/2846028
git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50606 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/cocoa/tab_window_controller.mm
@@ -39,7 +39,6 @@
 @end
 
 @implementation TabWindowController
-@synthesize tabStripView = tabStripView_;
 @synthesize tabContentArea = tabContentArea_;
 
 - (id)initWithWindow:(NSWindow*)window {
@@ -49,22 +48,91 @@
   return self;
 }
 
+// Add the side tab strip to the left side of the window's content area,
+// making it fill the full height of the content area.
+- (void)addSideTabStripToWindow {
+  NSView* contentView = [[self window] contentView];
+  NSRect contentFrame = [contentView frame];
+  NSRect sideStripFrame =
+      NSMakeRect(0, 0,
+                 NSWidth([sideTabStripView_ frame]),
+                 NSHeight(contentFrame));
+  [sideTabStripView_ setFrame:sideStripFrame];
+  [contentView addSubview:sideTabStripView_];
+}
+
+// Add the top tab strop to the window, above the content box and add it to the
+// view hierarchy as a sibling of the content view so it can overlap with the
+// window frame.
+- (void)addTopTabStripToWindow {
+  NSRect tabFrame =
+      NSMakeRect(0, NSMaxY(tabFrame),
+                 NSWidth([tabContentArea_ frame]),
+                 NSHeight([topTabStripView_ frame]));
+  [topTabStripView_ setFrame:tabFrame];
+  NSView* contentParent = [[[self window] contentView] superview];
+  [contentParent addSubview:topTabStripView_];
+}
+
 - (void)windowDidLoad {
+  // Cache the difference in height between the window content area and the
+  // tab content area.
+  NSRect tabFrame = [tabContentArea_ frame];
+  NSRect contentFrame = [[[self window] contentView] frame];
+  contentAreaHeightDelta_ = NSHeight(contentFrame) - NSHeight(tabFrame);
+
   if ([self hasTabStrip]) {
-    // Place the tab bar above the content box and add it to the view hierarchy
-    // as a sibling of the content view so it can overlap with the window frame.
-    NSRect tabFrame = [tabContentArea_ frame];
-    tabFrame.origin = NSMakePoint(0, NSMaxY(tabFrame));
-    tabFrame.size.height = NSHeight([tabStripView_ frame]);
-    [tabStripView_ setFrame:tabFrame];
-    [[[[self window] contentView] superview] addSubview:tabStripView_];
+    if ([self useVerticalTabs]) {
+      // No top tabstrip so remove the tabContentArea offset.
+      tabFrame.size.height = contentFrame.size.height;
+      [tabContentArea_ setFrame:tabFrame];
+      [self addSideTabStripToWindow];
+    } else {
+      [self addTopTabStripToWindow];
+    }
   } else {
-    // No tabstrip so remove the tabContentArea offset.
-    NSRect tabFrame = [tabContentArea_ frame];
-    NSRect contentFrame = [[[self window] contentView] frame];
+    // No top tabstrip so remove the tabContentArea offset.
     tabFrame.size.height = contentFrame.size.height;
     [tabContentArea_ setFrame:tabFrame];
   }
+}
+
+// Toggles from one display mode of the tab strip to another. Will automatically
+// call -layoutSubviews to reposition other content.
+- (void)toggleTabStripDisplayMode {
+  // Adjust the size of the tab contents to either use more or less space,
+  // depending on the direction of the toggle. This needs to be done prior to
+  // adding back in the top tab strip as its position is based off the MaxY
+  // of the tab content area.
+  BOOL useVertical = [self useVerticalTabs];
+  NSRect tabContentsFrame = [tabContentArea_ frame];
+  tabContentsFrame.size.height += useVertical ?
+      contentAreaHeightDelta_ : -contentAreaHeightDelta_;
+  [tabContentArea_ setFrame:tabContentsFrame];
+
+  if (useVertical) {
+    // Remove the top tab strip and add the sidebar in.
+    [topTabStripView_ removeFromSuperview];
+    [self addSideTabStripToWindow];
+  } else {
+    // Remove the side tab strip and add the top tab strip as a sibling of the
+    // window's content area.
+    [sideTabStripView_ removeFromSuperview];
+    NSRect tabContentsFrame = [tabContentArea_ frame];
+    tabContentsFrame.size.height -= contentAreaHeightDelta_;
+    [tabContentArea_ setFrame:tabContentsFrame];
+    [self addTopTabStripToWindow];
+  }
+
+  [self layoutSubviews];
+}
+
+// Return the appropriate tab strip based on whether or not side tabs are
+// enabled.
+- (TabStripView*)tabStripView {
+  if ([self useVerticalTabs])
+    return sideTabStripView_;
+  return topTabStripView_;
 }
 
 - (void)removeOverlay {
@@ -237,6 +305,12 @@
   return YES;
 }
 
+- (BOOL)useVerticalTabs {
+  // Subclasses should implement this.
+  NOTIMPLEMENTED();
+  return NO;
+}
+
 - (BOOL)isTabDraggable:(NSView*)tabView {
   return ![lockedTabs_ containsObject:tabView];
 }
@@ -253,6 +327,12 @@
 // during a drag.
 - (void)deferPerformClose {
   closeDeferred_ = YES;
+}
+
+// Called when the size of the window content area has changed. Override to
+// position specific views. Base class implementation does nothing.
+- (void)layoutSubviews {
+  NOTIMPLEMENTED();
 }
 
 @end

chrome/app/nibs/BrowserWindow.xib
@@ -2,14 +2,13 @@
 <archive type="com.apple.InterfaceBuilder3.Cocoa.XIB" version="7.03">
 	<data>
 		<int key="IBDocument.SystemTarget">1050</int>
-		<string key="IBDocument.SystemVersion">9L30</string>
-		<string key="IBDocument.InterfaceBuilderVersion">680</string>
+		<string key="IBDocument.SystemVersion">9L31a</string>
+		<string key="IBDocument.InterfaceBuilderVersion">677</string>
 		<string key="IBDocument.AppKitVersion">949.54</string>
 		<string key="IBDocument.HIToolboxVersion">353.00</string>
 		<object class="NSMutableArray" key="IBDocument.EditedObjectIDs">
 			<bool key="EncodedWithXMLCoder">YES</bool>
-			<integer value="56"/>
-			<integer value="1"/>
+			<integer value="89"/>
 		</object>
 		<object class="NSArray" key="IBDocument.PluginDependencies">
 			<bool key="EncodedWithXMLCoder">YES</bool>
@@ -46,7 +45,7 @@
 				<string key="NSWindowContentMaxSize">{3.40282e+38, 3.40282e+38}</string>
 				<string key="NSWindowContentMinSize">{400, 250}</string>
 				<object class="NSView" key="NSWindowView" id="1006">
-					<reference key="NSNextResponder"/>
+					<nil key="NSNextResponder"/>
 					<int key="NSvFlags">256</int>
 					<object class="NSMutableArray" key="NSSubviews">
 						<bool key="EncodedWithXMLCoder">YES</bool>
@@ -59,14 +58,13 @@
 						</object>
 					</object>
 					<string key="NSFrameSize">{750, 600}</string>
-					<reference key="NSSuperview"/>
 				</object>
 				<string key="NSScreenRect">{{0, 0}, {1440, 878}}</string>
 				<string key="NSMinSize">{400, 272}</string>
 				<string key="NSMaxSize">{3.40282e+38, 3.40282e+38}</string>
 			</object>
 			<object class="NSCustomView" id="1029219716">
-				<reference key="NSNextResponder"/>
+				<nil key="NSNextResponder"/>
 				<int key="NSvFlags">266</int>
 				<object class="NSMutableArray" key="NSSubviews">
 					<bool key="EncodedWithXMLCoder">YES</bool>
@@ -96,8 +94,14 @@
 					</object>
 				</object>
 				<string key="NSFrameSize">{483, 36}</string>
-				<reference key="NSSuperview"/>
 				<string key="NSClassName">TabStripView</string>
+			</object>
+			<object class="NSCustomView" id="529166964">
+				<reference key="NSNextResponder"/>
+				<int key="NSvFlags">272</int>
+				<string key="NSFrameSize">{236, 393}</string>
+				<reference key="NSSuperview"/>
+				<string key="NSClassName">SideTabStripView</string>
 			</object>
 		</object>
 		<object class="IBObjectContainer" key="IBDocument.Objects">
@@ -121,14 +125,6 @@
 				</object>
 				<object class="IBConnectionRecord">
 					<object class="IBOutletConnection" key="connection">
-						<string key="label">tabStripView_</string>
-						<reference key="source" ref="1001"/>
-						<reference key="destination" ref="1029219716"/>
-					</object>
-					<int key="connectionID">65</int>
-				</object>
-				<object class="IBConnectionRecord">
-					<object class="IBOutletConnection" key="connection">
 						<string key="label">tabContentArea_</string>
 						<reference key="source" ref="1001"/>
 						<reference key="destination" ref="1858870"/>
@@ -142,6 +138,22 @@
 						<reference key="destination" ref="131944810"/>
 					</object>
 					<int key="connectionID">87</int>
+				</object>
+				<object class="IBConnectionRecord">
+					<object class="IBOutletConnection" key="connection">
+						<string key="label">topTabStripView_</string>
+						<reference key="source" ref="1001"/>
+						<reference key="destination" ref="1029219716"/>
+					</object>
+					<int key="connectionID">88</int>
+				</object>
+				<object class="IBConnectionRecord">
+					<object class="IBOutletConnection" key="connection">
+						<string key="label">sideTabStripView_</string>
+						<reference key="source" ref="1001"/>
+						<reference key="destination" ref="529166964"/>
+					</object>
+					<int key="connectionID">90</int>
 				</object>
 			</object>
 			<object class="IBMutableOrderedSet" key="objectRecords">
@@ -221,6 +233,11 @@
 						<reference key="object" ref="1858870"/>
 						<reference key="parent" ref="1006"/>
 					</object>
+					<object class="IBObjectRecord">
+						<int key="objectID">89</int>
+						<reference key="object" ref="529166964"/>
+						<reference key="parent" ref="490739442"/>
+					</object>
 				</object>
 			</object>
 			<object class="NSMutableDictionary" key="flattenedProperties">
@@ -248,6 +265,8 @@
 					<string>66.IBPluginDependency</string>
 					<string>67.IBPluginDependency</string>
 					<string>84.IBPluginDependency</string>
+					<string>89.IBEditorWindowLastContentRect</string>
+					<string>89.IBPluginDependency</string>
 				</object>
 				<object class="NSMutableArray" key="dict.values">
 					<bool key="EncodedWithXMLCoder">YES</bool>
@@ -275,6 +294,8 @@
 					<string>com.apple.InterfaceBuilder.CocoaPlugin</string>
 					<string>com.apple.InterfaceBuilder.CocoaPlugin</string>
 					<string>com.apple.InterfaceBuilder.CocoaPlugin</string>
+					<string>{{89, 611}, {236, 393}}</string>
+					<string>com.apple.InterfaceBuilder.CocoaPlugin</string>
 				</object>
 			</object>
 			<object class="NSMutableDictionary" key="unlocalizedProperties">
@@ -297,7 +318,7 @@
 				</object>
 			</object>
 			<nil key="sourceID"/>
-			<int key="maxID">87</int>
+			<int key="maxID">90</int>
 		</object>
 		<object class="IBClassDescriber" key="IBDocument.Classes">
 			<object class="NSMutableArray" key="referencedPartialClassDescriptions">
@@ -311,9 +332,16 @@
 					</object>
 				</object>
 				<object class="IBPartialClassDescription">
+					<string key="className">BrowserWindowController</string>
+					<object class="IBClassDescriptionSource" key="sourceIdentifier">
+						<string key="majorKey">IBProjectSource</string>
+						<string key="minorKey">browser/cocoa/browser_window_controller_private.h</string>
+					</object>
+				</object>
+				<object class="IBPartialClassDescription">
 					<string key="className">ChromeBrowserWindow</string>
 					<string key="superclassName">ChromeEventProcessingWindow</string>
-					<object class="IBClassDescriptionSource" key="sourceIdentifier">
+					<object class="IBClassDescriptionSource" key="sourceIdentifier" id="924273615">
 						<string key="majorKey">IBProjectSource</string>
 						<string key="minorKey">browser/cocoa/chrome_browser_window.h</string>
 					</object>
@@ -350,6 +378,20 @@
 					<string key="className">NSObject</string>
 					<object class="IBClassDescriptionSource" key="sourceIdentifier">
 						<string key="majorKey">IBProjectSource</string>
+						<string key="minorKey">../third_party/GTM/Foundation/GTMNSObject+KeyValueObserving.h</string>
+					</object>
+				</object>
+				<object class="IBPartialClassDescription">
+					<string key="className">NSObject</string>
+					<object class="IBClassDescriptionSource" key="sourceIdentifier">
+						<string key="majorKey">IBProjectSource</string>
+						<string key="minorKey">browser/cocoa/objc_zombie.h</string>
+					</object>
+				</object>
+				<object class="IBPartialClassDescription">
+					<string key="className">NSObject</string>
+					<object class="IBClassDescriptionSource" key="sourceIdentifier">
+						<string key="majorKey">IBProjectSource</string>
 						<string key="minorKey">browser/cocoa/status_bubble_mac.h</string>
 					</object>
 				</object>
@@ -362,9 +404,21 @@
 				</object>
 				<object class="IBPartialClassDescription">
 					<string key="className">NSWindow</string>
+					<reference key="sourceIdentifier" ref="924273615"/>
+				</object>
+				<object class="IBPartialClassDescription">
+					<string key="className">NSWindow</string>
 					<object class="IBClassDescriptionSource" key="sourceIdentifier">
 						<string key="majorKey">IBProjectSource</string>
-						<string key="minorKey">browser/cocoa/nswindow_local_state.h</string>
+						<string key="minorKey">browser/cocoa/themed_window.h</string>
+					</object>
+				</object>
+				<object class="IBPartialClassDescription">
+					<string key="className">SideTabStripView</string>
+					<string key="superclassName">TabStripView</string>
+					<object class="IBClassDescriptionSource" key="sourceIdentifier">
+						<string key="majorKey">IBProjectSource</string>
+						<string key="minorKey">browser/cocoa/side_tab_strip_view.h</string>
 					</object>
 				</object>
 				<object class="IBPartialClassDescription">
@@ -386,11 +440,13 @@
 						<bool key="EncodedWithXMLCoder">YES</bool>
 						<object class="NSMutableArray" key="dict.sortedKeys">
 							<bool key="EncodedWithXMLCoder">YES</bool>
+							<string>sideTabStripView_</string>
 							<string>tabContentArea_</string>
-							<string>tabStripView_</string>
+							<string>topTabStripView_</string>
 						</object>
 						<object class="NSMutableArray" key="dict.values">
 							<bool key="EncodedWithXMLCoder">YES</bool>
+							<string>TabStripView</string>
 							<string>FastResizeView</string>
 							<string>TabStripView</string>
 						</object>

chrome/browser/cocoa/browser_window_controller.mm
@@ -233,16 +233,7 @@
     // this window's Browser and the tab strip view. The controller will handle
     // registering for the appropriate tab notifications from the back-end and
     // managing the creation of new tabs.
-    if (![self useVerticalTabs]) {
-      tabStripController_.reset([[TabStripController alloc]
-                                  initWithView:[self tabStripView]
-                                    switchView:[self tabContentArea]
-                                       browser:browser_.get()]);
-    } else {
-      // TODO(pinkerton): Load SideTabController when written and add it to the
-      // contentView. This should be abstracted into a separate method like
-      // the toolbar controller initialization below.
-    }
+    [self createTabStripController];
 
     // Create the infobar container view, so we can pass it to the
     // ToolbarController.
@@ -1299,8 +1290,6 @@
 
 // (Override of |TabWindowController| method.)
 - (BOOL)hasTabStrip {
-  if ([self useVerticalTabs])
-    return NO;
   return [self supportsWindowFeature:Browser::FEATURE_TABSTRIP];
 }
 
@@ -1638,14 +1627,15 @@ willAnimateFromState:(bookmarks::VisualState)oldState
   isShrinkingFromZoomed_ = NO;
 }
 
+// Override to swap in the correct tab strip controller based on the new
+// tab strip mode.
 - (void)toggleTabStripDisplayMode {
-  // TODO(pinkerton) re-initialize tab strip. Finish writing this method.
-  // Right now, it only switches one direction, which clearly isn't cool.
-  // [self initTabStrip:browser_->tabstrip_model()];
-  [[self tabStripView] removeFromSuperview];
+  [super toggleTabStripDisplayMode];
+  [self createTabStripController];
+}
 
-
-  [self layoutSubviews];
+- (BOOL)useVerticalTabs {
+  return browser_->tabstrip_model()->delegate()->UseVerticalTabs();
 }
 
 @end  // @implementation BrowserWindowController
@@ -1699,7 +1689,7 @@ willAnimateFromState:(bookmarks::VisualState)oldState
 
   // Retain the tab strip view while we remove it from its superview.
   scoped_nsobject<NSView> tabStripView;
-  if ([self hasTabStrip]) {
+  if ([self hasTabStrip] && ![self useVerticalTabs]) {
     tabStripView.reset([[self tabStripView] retain]);
     [tabStripView removeFromSuperview];
   }
@@ -1745,7 +1735,7 @@ willAnimateFromState:(bookmarks::VisualState)oldState
 
   // Add the tab strip after setting the content view and moving the incognito
   // badge (if any), so that the tab strip will be on top (in the z-order).
-  if ([self hasTabStrip])
+  if ([self hasTabStrip] && ![self useVerticalTabs])
     [[[destWindow contentView] superview] addSubview:tabStripView];
 
   [window setWindowController:nil];

chrome/browser/cocoa/browser_window_controller_private.h
@@ -16,8 +16,9 @@
 // "dependencies").
 @interface BrowserWindowController(Private)
 
-// Returns YES if vertical tabs are enabled for this browser.
-- (BOOL)useVerticalTabs;
+// Create the appropriate tab strip controller based on whether or not side
+// tabs are enabled. Replaces the current controller.
+- (void)createTabStripController;
 
 // Saves the window's position in the local state preferences.
 - (void)saveWindowPositionIfNeeded;

chrome/browser/cocoa/browser_window_controller_private.mm
@@ -15,6 +15,7 @@
 #import "chrome/browser/cocoa/find_bar_cocoa_controller.h"
 #import "chrome/browser/cocoa/floating_bar_backing_view.h"
 #import "chrome/browser/cocoa/fullscreen_controller.h"
+#import "chrome/browser/cocoa/side_tab_strip_controller.h"
 #import "chrome/browser/cocoa/tab_strip_controller.h"
 #import "chrome/browser/cocoa/tab_strip_view.h"
 #import "chrome/browser/cocoa/toolbar_controller.h"
@@ -43,8 +44,17 @@ const CGFloat kLocBarBottomInset = 1;
 
 @implementation BrowserWindowController(Private)
 
-- (BOOL)useVerticalTabs {
-  return browser_->tabstrip_model()->delegate()->UseVerticalTabs();
+// Create the appropriate tab strip controller based on whether or not side
+// tabs are enabled.
+- (void)createTabStripController {
+  Class factory = [TabStripController class];
+  if ([self useVerticalTabs])
+    factory = [SideTabStripController class];
+
+  tabStripController_.reset([[factory alloc]
+                              initWithView:[self tabStripView]
+                                switchView:[self tabContentArea]
+                                   browser:browser_.get()]);
 }
 
 - (void)saveWindowPositionIfNeeded {
@@ -133,7 +143,7 @@ willPositionSheet:(NSWindow*)sheet
 }
 
 - (void)layoutSubviews {
-  // With the exception of the tab strip, the subviews which we lay out are
+  // With the exception of the top tab strip, the subviews which we lay out are
   // subviews of the content view, so we mainly work in the content view's
   // coordinate system. Note, however, that the content view's coordinate system
   // and the window's base coordinate system should coincide.
@@ -158,9 +168,9 @@ willPositionSheet:(NSWindow*)sheet
   CGFloat maxY = NSMaxY(contentBounds) + yOffset;
   CGFloat startMaxY = maxY;
 
-  if ([self hasTabStrip]) {
-    // If we need to lay out the tab strip, replace |maxY| and |startMaxY| with
-    // higher values, and then lay out the tab strip.
+  if ([self hasTabStrip] && ![self useVerticalTabs]) {
+    // If we need to lay out the top tab strip, replace |maxY| and |startMaxY|
+    // with higher values, and then lay out the tab strip.
     NSRect windowFrame = [contentView convertRect:[window frame] fromView:nil];
     startMaxY = maxY = NSHeight(windowFrame) + yOffset;
     maxY = [self layoutTabStripAtMaxY:maxY width:width fullscreen:isFullscreen];
@@ -170,22 +180,20 @@ willPositionSheet:(NSWindow*)sheet
   DCHECK_GE(maxY, minY);
   DCHECK_LE(maxY, NSMaxY(contentBounds) + yOffset);
 
-  // Position the vertical tab strip on the left, taking up the entire height.
-  // TODO(pinkerton): Make width not fixed.
-  const CGFloat kSidebarWidth = 200.0;
+  // The base class already positions the side tab strip on the left side
+  // of the window's content area and sizes it to take the entire vertical
+  // height. All that's needed here is to push everything over to the right,
+  // if necessary.
   if ([self useVerticalTabs]) {
-    // TODO(pinkerton): Position the side bar at |minX| when the controller
-    // is implemented.
-
-    // Push everything else over to the right.
-    minX += kSidebarWidth;
-    width -= kSidebarWidth;
+    const CGFloat sideTabWidth = [[self tabStripView] bounds].size.width;
+    minX += sideTabWidth;
+    width -= sideTabWidth;
   }
 
   // Place the toolbar at the top of the reserved area.
   maxY = [self layoutToolbarAtMinX:minX maxY:maxY width:width];
 
- // If we're not displaying the bookmark bar below the infobar, then it goes
+  // If we're not displaying the bookmark bar below the infobar, then it goes
   // immediately below the toolbar.
   BOOL placeBookmarkBarBelowInfoBar = [self placeBookmarkBarBelowInfoBar];
   if (!placeBookmarkBarBelowInfoBar)
@@ -417,7 +425,9 @@ willPositionSheet:(NSWindow*)sheet
   NSView* tabContentView = [self tabContentArea];
   NSRect tabContentFrame = [tabContentView frame];
 
-  bool contentShifted = NSMaxY(tabContentFrame) != NSMaxY(newFrame);
+  bool contentShifted =
+      NSMaxY(tabContentFrame) != NSMaxY(newFrame) ||
+      NSMinX(tabContentFrame) != NSMinX(newFrame);
 
   tabContentFrame = newFrame;
   [tabContentView setFrame:tabContentFrame];

chrome/browser/cocoa/side_tab_strip_controller.h
@@ -1,0 +1,19 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Cocoa/Cocoa.h>
+
+#import "chrome/browser/cocoa/tab_strip_controller.h"
+
+// A controller for the tab strip when side tabs are enabled.
+//
+// TODO(pinkerton): I'm expecting there are more things here that need
+// overriding rather than just tweaking a couple of settings, so I'm creating
+// a full-blown subclass. Clearly, very little is actually necessary at this
+// point for it to work.
+
+@interface SideTabStripController : TabStripController {
+}
+
+@end

chrome/browser/cocoa/side_tab_strip_controller.mm
@@ -1,0 +1,29 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "chrome/browser/cocoa/side_tab_strip_controller.h"
+
+@implementation SideTabStripController
+
+// TODO(pinkerton): Still need to figure out several things:
+//   - new tab button placement and layout
+//   - animating tabs in and out
+//   - being able to drop a tab elsewhere besides the 1st position
+//   - how to load a different tab view nib for each tab.
+
+- (id)initWithView:(TabStripView*)view
+        switchView:(NSView*)switchView
+           browser:(Browser*)browser {
+  self = [super initWithView:view switchView:switchView browser:browser];
+  if (self) {
+    // Side tabs have no indent since they are not sharing space with the
+    // window controls.
+    [self setIndentForControls:0.0];
+    verticalLayout_ = YES;
+  }
+  return self;
+}
+
+@end
+

chrome/browser/cocoa/side_tab_strip_view.h
@@ -1,0 +1,15 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Cocoa/Cocoa.h>
+
+#import "chrome/browser/cocoa/tab_strip_view.h"
+
+// A class that handles drawing the background of the tab strip when side tabs
+// are enabled.
+
+@interface SideTabStripView : TabStripView {
+}
+
+@end

commit 19e0c5f2912c07f667dcf71dfbfe0ec5d98f4398

Fix uninitialized memory error caused by r50606. BUG=48269 TEST=none
git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52514 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/cocoa/tab_window_controller.mm
@@ -65,9 +65,10 @@
 // view hierarchy as a sibling of the content view so it can overlap with the
 // window frame.
 - (void)addTopTabStripToWindow {
+  NSRect contentFrame = [tabContentArea_ frame];
   NSRect tabFrame =
-      NSMakeRect(0, NSMaxY(tabFrame),
-                 NSWidth([tabContentArea_ frame]),
+      NSMakeRect(0, NSMaxY(contentFrame),
+                 NSWidth(contentFrame),
                  NSHeight([topTabStripView_ frame]));
   [topTabStripView_ setFrame:tabFrame];
   NSView* contentParent = [[[self window] contentView] superview];