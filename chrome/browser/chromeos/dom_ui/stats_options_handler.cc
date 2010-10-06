adobe/chromium

commit 18f719e9c56bc7cf35a5f697ee4333aa8f4adaf4

<html> <style type="text/css">   <!--   body { font-family: arial,sans-serif; background-color: #fff; margin-top: 2; }   td {font-family: arial, sans-serif;}   .c { width: 4; height: 4; }   a:link { color: #00c; }   a:visited { color: #551a8b; }   a:active { color: #f00; }   .form-noindent { background-color: #fff; border: 1px solid #c3d9ff; }   --> </style> <style type="text/css"><!-- .gaia.le.lbl { font-family: Arial, Helvetica, sans-serif; font-size: smaller; } .gaia.le.fpwd { font-family: Arial, Helvetica, sans-serif; font-size: 70%; } .gaia.le.chusr { font-family: Arial, Helvetica, sans-serif; font-size: 70%; } .gaia.le.val { font-family: Arial, Helvetica, sans-serif; font-size: smaller; } .gaia.le.button { font-family: Arial, Helvetica, sans-serif; font-size: smaller; } .gaia.le.rem { font-family: Arial, Helvetica, sans-serif; font-size: smaller; }
.gaia.captchahtml.desc { font-family: arial, sans-serif; font-size: smaller; }
.gaia.captchahtml.cmt { font-family: arial, sans-serif; font-size: smaller; font-style: italic; }

--></style>
<head>
  <title>Google Accounts</title>
  <style type="text/css"><!--
    body {
      font-family: arial, sans-serif;
      margin: 0;
      padding: 13px 15px 15px;

    }
    .body {
      margin: 0;
    }
    div.errorbox-good {}

    div.errorbox-bad {}

    div.errormsg { color: red; font-size: smaller; font-family: arial,sans-serif;}
    font.errormsg { color: red; font-size: smaller; font-family: arial,sans-serif;}

    div.pagemsg {
      font-size: smaller;
      font-weight: bold;
      text-align: center;
    }
    div.pagemsg span {
      padding: 5px;
      background: #ff9;
    }

    div.topbar {
      font-size:smaller;
      margin-right: -5px;
      text-align:right;
      white-space:nowrap;
    }
    div.header {
      margin-bottom: 9px;
      margin-left: -2px;
      position:relative;
      zoom: 1
    }
    div.header img.logo {
      border: 0;
      float:left;
    }
    div.header div.headercontent {

chrome/browser/chromeos/dom_ui/stats_options_handler.cc
@@ -1,0 +1,65 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/dom_ui/stats_options_handler.h"
+
+#include "base/basictypes.h"
+#include "base/callback.h"
+#include "base/utf_string_conversions.h"
+#include "base/values.h"
+#include "chrome/browser/chromeos/cros_settings_names.h"
+#include "chrome/browser/chromeos/cros_settings_provider_stats.h"
+#include "chrome/browser/metrics/user_metrics.h"
+
+namespace chromeos {
+
+StatsOptionsHandler::StatsOptionsHandler()
+    : CrosOptionsPageUIHandler(new MetricsCrosSettingsProvider()) {
+}
+
+// OptionsUIHandler implementation.
+void StatsOptionsHandler::GetLocalizedValues(
+    DictionaryValue* localized_strings) {
+}
+void StatsOptionsHandler::Initialize() {
+  SetupMetricsReportingCheckbox(false);
+}
+
+// DOMMessageHandler implementation.
+void StatsOptionsHandler::RegisterMessages() {
+  dom_ui_->RegisterMessageCallback(
+      "metricsReportingCheckboxAction",
+      NewCallback(this, &StatsOptionsHandler::HandleMetricsReportingCheckbox));
+}
+
+MetricsCrosSettingsProvider* StatsOptionsHandler::provider() const {
+  return static_cast<MetricsCrosSettingsProvider*>(settings_provider_.get());
+}
+
+void StatsOptionsHandler::HandleMetricsReportingCheckbox(
+    const ListValue* args) {
+#if defined(GOOGLE_CHROME_BUILD)
+  const std::string checked_str = WideToUTF8(ExtractStringValue(args));
+  const bool enabled = (checked_str == "true");
+  UserMetricsRecordAction(
+      enabled ?
+      UserMetricsAction("Options_MetricsReportingCheckbox_Enable") :
+      UserMetricsAction("Options_MetricsReportingCheckbox_Disable"));
+  const bool is_enabled = GetCurrentStatus();
+  SetupMetricsReportingCheckbox(enabled == is_enabled);
+#endif
+}
+
+void StatsOptionsHandler::SetupMetricsReportingCheckbox(bool user_changed) {
+#if defined(GOOGLE_CHROME_BUILD)
+  FundamentalValue checked(GetCurrentStatus());
+  FundamentalValue disabled(false);
+  FundamentalValue user_has_changed(user_changed);
+  dom_ui_->CallJavascriptFunction(
+      L"options.AdvancedOptions.SetMetricsReportingCheckboxState", checked,
+      disabled, user_has_changed);
+#endif
+}
+
+}  // namespace chromeos

chrome/browser/browser_main.cc
@@ -151,6 +151,7 @@
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/cros/screen_lock_library.h"
+#include "chrome/browser/chromeos/cros_settings_provider_stats.h"
 #include "chrome/browser/chromeos/customization_document.h"
 #include "chrome/browser/chromeos/external_metrics.h"
 #include "chrome/browser/chromeos/login/authenticator.h"
@@ -548,8 +549,10 @@ PrefService* InitializeLocalState(const CommandLine& parsed_command_line,
   // sources. This has to be done before uninstall code path and before prefs
   // are registered.
   local_state->RegisterStringPref(prefs::kApplicationLocale, "");
+#if !defined(OS_CHROMEOS)
   local_state->RegisterBooleanPref(prefs::kMetricsReportingEnabled,
       GoogleUpdateSettings::GetCollectStatsConsent());
+#endif  // !defined(OS_CHROMEOS)
 
   if (is_first_run) {
 #if defined(OS_WIN)
@@ -634,7 +637,11 @@ MetricsService* InitializeMetrics(const CommandLine& parsed_command_line,
     // prefs, we turn on recording.  We disable metrics completely for
     // non-official builds.
 #if defined(GOOGLE_CHROME_BUILD)
+#if defined(OS_CHROMEOS)
+    bool enabled = chromeos::MetricsCrosSettingsProvider::GetMetricsStatus();
+#else
     bool enabled = local_state->GetBoolean(prefs::kMetricsReportingEnabled);
+#endif  // #if defined(OS_CHROMEOS)
     metrics->SetUserPermitsUpload(enabled);
     if (enabled) {
       metrics->Start();
@@ -947,6 +954,7 @@ int BrowserMain(const MainFunctionParams& parameters) {
   // through configuration policy or user preference. The kHeadless environment
   // variable overrides the decision, but only if the crash service is under
   // control of the user.
+#if !defined(OS_CHROMEOS)
   const PrefService::Preference* metrics_reporting_enabled =
       local_state->FindPreference(prefs::kMetricsReportingEnabled);
   CHECK(metrics_reporting_enabled);
@@ -954,6 +962,10 @@ int BrowserMain(const MainFunctionParams& parameters) {
       local_state->GetBoolean(prefs::kMetricsReportingEnabled);
   if (!breakpad_enabled && metrics_reporting_enabled->IsUserModifiable())
     breakpad_enabled = getenv(env_vars::kHeadless) != NULL;
+#else
+  bool breakpad_enabled =
+      chromeos::MetricsCrosSettingsProvider::GetMetricsStatus();
+#endif  // #if !defined(OS_CHROMEOS)
   if (breakpad_enabled)
     InitCrashReporter();
 #endif

chrome/browser/chromeos/cros_settings_names.cc
@@ -16,4 +16,6 @@ const char kAccountsPrefUsers[] = "cros.accounts.users";
 
 const char kSystemTimezone[] = "cros.system.timezone";
 
+const char kStatsReportingPref[] = "cros.metrics.reportingEnabled";
+
 }  // namespace chromeos

chrome/browser/chromeos/cros_settings_names.h
@@ -17,6 +17,8 @@ extern const char kAccountsPrefUsers[];
 
 extern const char kSystemTimezone[];
 
+extern const char kStatsReportingPref[];
+
 }  // namespace chromeos
 
 #endif  // CHROME_BROWSER_CHROMEOS_CROS_SETTINGS_NAMES_H_

chrome/browser/chromeos/cros_settings_provider.h
@@ -25,11 +25,6 @@ class CrosSettingsProvider {
 
   // Gets the namespace prefix provided by this provider
   virtual bool HandlesSetting(const std::string& path) = 0;
-
- protected:
-  // Fires system setting change notification.
-  void FireObservers(const char* path);
-
 };
 
 }  // namespace chromeos

chrome/browser/chromeos/cros_settings_provider_stats.cc
@@ -1,0 +1,65 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/cros_settings_provider_stats.h"
+
+#include "base/string_util.h"
+#include "base/values.h"
+#include "chrome/browser/chromeos/cros_settings.h"
+#include "chrome/browser/chromeos/cros_settings_names.h"
+#include "chrome/browser/options_util.h"
+#include "chrome/installer/util/google_update_settings.h"
+
+#if defined(USE_LINUX_BREAKPAD)
+#include "chrome/app/breakpad_linux.h"
+#endif
+
+namespace chromeos {
+
+void MetricsCrosSettingsProvider::Set(const std::string& path, Value* value) {
+  DCHECK(path == kStatsReportingPref);
+  bool enabled = false;
+  CHECK(value->GetAsBoolean(&enabled));
+  if (SetMetricsStatus(enabled)) {
+    CrosSettings::Get()->FireObservers(path.c_str());
+  }
+}
+
+bool MetricsCrosSettingsProvider::Get(const std::string& path,
+                                      Value** value) const {
+  DCHECK(path == kStatsReportingPref);
+  *value = Value::CreateBooleanValue(GetMetricsStatus());
+  return true;
+}
+
+// static
+bool MetricsCrosSettingsProvider::SetMetricsStatus(bool enabled) {
+  LOG(INFO) << "Setting cros stats/crash metric reporting to " << enabled;
+  if (enabled != GoogleUpdateSettings::GetCollectStatsConsent()) {
+    bool new_enabled = OptionsUtil::ResolveMetricsReportingEnabled(enabled);
+#if defined(USE_LINUX_BREAKPAD)
+    if (new_enabled)
+      InitCrashReporter();
+    // Else, if (!new_enabled), we should have turned crash reporting off
+    // here, but there is no API for that currently (while we use
+    // BreakPad). But this is not a big deal: crash reporting will be off
+    // after reboot for the current process while other Chrome processes
+    // will start when the setting is already set up. Other ChromeOS
+    // processes does not use BreakPad.
+#endif
+    return new_enabled == enabled;
+  }
+  return false;
+}
+
+// static
+bool MetricsCrosSettingsProvider::GetMetricsStatus() {
+  return GoogleUpdateSettings::GetCollectStatsConsent();
+}
+
+bool MetricsCrosSettingsProvider::HandlesSetting(const std::string& path) {
+  return ::StartsWithASCII(path, kStatsReportingPref, true);
+}
+
+};  // namespace chromeos

chrome/browser/chromeos/cros_settings_provider_stats.h
@@ -1,0 +1,39 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_CROS_SETTINGS_PROVIDER_STATS_H_
+#define CHROME_BROWSER_CHROMEOS_CROS_SETTINGS_PROVIDER_STATS_H_
+#pragma once
+
+#include <string>
+
+#include "base/basictypes.h"
+#include "chrome/browser/chromeos/cros_settings_provider.h"
+
+namespace chromeos {
+
+// CrosSettingsProvider that abstracts switching of stats/crash
+// reporting to Google.
+class MetricsCrosSettingsProvider : public CrosSettingsProvider {
+ public:
+  MetricsCrosSettingsProvider() {}
+
+  // CrosSettingsProvider implementation.
+  virtual void Set(const std::string& path, Value* value);
+  virtual bool Get(const std::string& path, Value** value) const;
+  virtual bool HandlesSetting(const std::string& path);
+
+  // Actual methods to control stats/crash reporting. Currently these
+  // methods are public and static so they are accessible directly
+  // from cros code. But this will change soon: crosbug.com/7359
+  static bool SetMetricsStatus(bool enabled);
+  static bool GetMetricsStatus();
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MetricsCrosSettingsProvider);
+};
+
+};  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_CROS_SETTINGS_PROVIDER_STATS_H_

chrome/browser/chromeos/dom_ui/stats_options_handler.h
@@ -1,0 +1,40 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_DOM_UI_STATS_OPTIONS_HANDLER_H_
+#define CHROME_BROWSER_CHROMEOS_DOM_UI_STATS_OPTIONS_HANDLER_H_
+#pragma once
+
+#include "chrome/browser/chromeos/dom_ui/cros_options_page_ui_handler.h"
+
+namespace chromeos {
+
+class MetricsCrosSettingsProvider;
+
+// ChromeOS handler for "Stats/crash reporting to Google" option of the Advanced
+// settings page. This handler does only ChromeOS-specific actions while default
+// code is in Chrome's AdvancedOptionsHandler
+// (chrome/browser/dom_ui/advanced_options_handler.cc).
+class StatsOptionsHandler : public CrosOptionsPageUIHandler {
+ public:
+  StatsOptionsHandler();
+
+  // OptionsUIHandler implementation.
+  virtual void GetLocalizedValues(DictionaryValue* localized_strings);
+  virtual void Initialize();
+
+  // DOMMessageHandler implementation.
+  virtual void RegisterMessages();
+
+ private:
+  MetricsCrosSettingsProvider* provider() const;
+  void HandleMetricsReportingCheckbox(const ListValue* args);
+  void SetupMetricsReportingCheckbox(bool user_changed);
+
+  DISALLOW_COPY_AND_ASSIGN(StatsOptionsHandler);
+};
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_DOM_UI_STATS_OPTIONS_HANDLER_H_

commit 2d3d220d345c8cf6a93f9e856f9e29eb007992b1

Fix build break on Official ChromeOS builder caused by r61684. TBR=glotov
git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61719 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/chromeos/dom_ui/stats_options_handler.cc
@@ -46,14 +46,14 @@ void StatsOptionsHandler::HandleMetricsReportingCheckbox(
       enabled ?
       UserMetricsAction("Options_MetricsReportingCheckbox_Enable") :
       UserMetricsAction("Options_MetricsReportingCheckbox_Disable"));
-  const bool is_enabled = GetCurrentStatus();
+  const bool is_enabled = MetricsCrosSettingsProvider::GetMetricsStatus();
   SetupMetricsReportingCheckbox(enabled == is_enabled);
 #endif
 }
 
 void StatsOptionsHandler::SetupMetricsReportingCheckbox(bool user_changed) {
 #if defined(GOOGLE_CHROME_BUILD)
-  FundamentalValue checked(GetCurrentStatus());
+  FundamentalValue checked(MetricsCrosSettingsProvider::GetMetricsStatus());
   FundamentalValue disabled(false);
   FundamentalValue user_has_changed(user_changed);
   dom_ui_->CallJavascriptFunction(