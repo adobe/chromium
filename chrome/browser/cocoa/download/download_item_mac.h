adobe/chromium

commit 685d5f0244a41d6ec2983759067680849601865e

Use .crdownload file path for 'Show In Folder' while download is in progress
BUG=57037
TEST=download a huge file and click 'Show In Folder' during the download

Review URL: http://codereview.chromium.org/4460002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65418 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/cocoa/download/download_item_mac.h
@@ -55,7 +55,7 @@ class DownloadItemMac : DownloadItem::Observer {
   CancelableRequestConsumerT<int, 0> icon_consumer_;
 
   // Stores the last known name where the file will be saved.
-  FilePath lastFilePath_;
+  FilePath lastFileName_;
 
   DISALLOW_COPY_AND_ASSIGN(DownloadItemMac);
 };

chrome/browser/cocoa/download/download_item_mac.mm
@@ -34,12 +34,12 @@ void DownloadItemMac::OnDownloadUpdated(DownloadItem* download) {
     [item_controller_ clearDangerousMode];
   }
 
-  if (download->full_path() != lastFilePath_) {
-    // Turns out the file path is "unconfirmed %d.download" for dangerous
+  if (download->GetUserVerifiedFileName() != lastFileName_) {
+    // Turns out the file path is "unconfirmed %d.crdownload" for dangerous
     // downloads. When the download is confirmed, the file is renamed on
     // another thread, so reload the icon if the download filename changes.
     LoadIcon();
-    lastFilePath_ = download->full_path();
+    lastFileName_ = download->GetUserVerifiedFileName();
 
     [item_controller_ updateToolTip];
   }
@@ -72,7 +72,7 @@ void DownloadItemMac::LoadIcon() {
   }
 
   // We may already have this particular image cached.
-  FilePath file = download_model_->download()->full_path();
+  FilePath file = download_model_->download()->GetUserVerifiedFileName();
   SkBitmap* icon_bitmap = icon_manager->LookupIcon(file, IconLoader::SMALL);
   if (icon_bitmap) {
     NSImage* icon = gfx::SkBitmapToNSImage(*icon_bitmap);

chrome/browser/dom_ui/downloads_dom_handler.cc
@@ -159,7 +159,8 @@ void DownloadsDOMHandler::HandleDrag(const ListValue* args) {
   DownloadItem* file = GetDownloadByValue(args);
   if (file) {
     IconManager* im = g_browser_process->icon_manager();
-    SkBitmap* icon = im->LookupIcon(file->full_path(), IconLoader::NORMAL);
+    SkBitmap* icon = im->LookupIcon(file->GetUserVerifiedFileName(),
+                                    IconLoader::NORMAL);
     gfx::NativeView view = dom_ui_->tab_contents()->GetNativeView();
     download_util::DragDownload(file, icon, view);
   }

chrome/browser/download/download_item.cc
@@ -57,7 +57,7 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       open_when_complete_(false),
       safety_state_(SAFE),
       auto_opened_(false),
-      original_name_(info.original_name),
+      target_name_(info.original_name),
       render_process_id_(-1),
       request_id_(-1),
       save_as_(false),
@@ -65,7 +65,6 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       is_extension_install_(info.is_extension_install),
       name_finalized_(false),
       is_temporary_(false),
-      need_final_rename_(false),
       opened_(false) {
   if (state_ == IN_PROGRESS)
     state_ = CANCELLED;
@@ -94,7 +93,7 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       open_when_complete_(false),
       safety_state_(info.is_dangerous ? DANGEROUS : SAFE),
       auto_opened_(false),
-      original_name_(info.original_name),
+      target_name_(info.original_name),
       render_process_id_(info.child_id),
       request_id_(info.request_id),
       save_as_(info.prompt_user_for_save_location),
@@ -102,7 +101,6 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       is_extension_install_(info.is_extension_install),
       name_finalized_(false),
       is_temporary_(!info.save_info.file_path.empty()),
-      need_final_rename_(false),
       opened_(false) {
   Init(true /* start progress timer */);
 }
@@ -130,7 +128,6 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       open_when_complete_(false),
       safety_state_(SAFE),
       auto_opened_(false),
-      original_name_(FilePath()),
       render_process_id_(-1),
       request_id_(-1),
       save_as_(false),
@@ -138,7 +135,6 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       is_extension_install_(false),
       name_finalized_(false),
       is_temporary_(false),
-      need_final_rename_(false),
       opened_(false) {
   Init(true /* start progress timer */);
 }
@@ -165,24 +161,21 @@ void DownloadItem::NotifyObserversDownloadFileCompleted() {
 }
 
 bool DownloadItem::CanOpenDownload() {
-  FilePath file_to_use = full_path();
-  if (!original_name().value().empty())
-    file_to_use = original_name();
-
-  return !Extension::IsExtension(file_to_use) &&
-      !download_util::IsExecutableFile(file_to_use);
+  return !Extension::IsExtension(target_name_) &&
+      !download_util::IsExecutableFile(target_name_);
 }
 
 bool DownloadItem::ShouldOpenFileBasedOnExtension() {
-  return download_manager_->ShouldOpenFileBasedOnExtension(full_path());
+  return download_manager_->ShouldOpenFileBasedOnExtension(
+      GetUserVerifiedFileName());
 }
 
 void DownloadItem::OpenFilesBasedOnExtension(bool open) {
   DownloadPrefs* prefs = download_manager_->download_prefs();
   if (open)
-    prefs->EnableAutoOpenBasedOnExtension(full_path());
+    prefs->EnableAutoOpenBasedOnExtension(GetUserVerifiedFileName());
   else
-    prefs->DisableAutoOpenBasedOnExtension(full_path());
+    prefs->DisableAutoOpenBasedOnExtension(GetUserVerifiedFileName());
 }
 
 void DownloadItem::OpenDownload() {
@@ -281,7 +274,8 @@ void DownloadItem::Finished() {
                                        *this);
     auto_opened_ = true;
   } else if (open_when_complete() ||
-             download_manager_->ShouldOpenFileBasedOnExtension(full_path()) ||
+             download_manager_->ShouldOpenFileBasedOnExtension(
+                 GetUserVerifiedFileName()) ||
              is_temporary()) {
     // If the download is temporary, like in drag-and-drop, do not open it but
     // we still need to set it auto-opened so that it can be removed from the
@@ -344,7 +338,6 @@ int DownloadItem::PercentComplete() const {
 void DownloadItem::Rename(const FilePath& full_path) {
   DCHECK(!full_path.empty());
   full_path_ = full_path;
-  file_name_ = full_path_.BaseName();
 }
 
 void DownloadItem::TogglePause() {
@@ -386,26 +379,35 @@ bool DownloadItem::MatchesQuery(const string16& query) const {
   if (url_formatted.find(query) != string16::npos)
     return true;
 
-  string16 path(l10n_util::ToLower(WideToUTF16(full_path_.ToWStringHack())));
+  string16 path(l10n_util::ToLower(WideToUTF16(full_path().ToWStringHack())));
   if (path.find(query) != std::wstring::npos)
     return true;
 
   return false;
 }
 
-FilePath DownloadItem::GetFileName() const {
-  if (safety_state_ == DownloadItem::SAFE)
-    return file_name_;
+FilePath DownloadItem::GetTargetFilePath() const {
+  return full_path_.DirName().Append(target_name_);
+}
+
+FilePath DownloadItem::GetFileNameToReportUser() const {
   if (path_uniquifier_ > 0) {
-    FilePath name(original_name_);
+    FilePath name(target_name_);
     download_util::AppendNumberToPath(&name, path_uniquifier_);
     return name;
   }
-  return original_name_;
+  return target_name_;
+}
+
+FilePath DownloadItem::GetUserVerifiedFileName() const {
+  if (safety_state_ == DownloadItem::SAFE)
+    return target_name_;
+  return full_path_.BaseName();
 }
 
 void DownloadItem::Init(bool start_timer) {
-  file_name_ = full_path_.BaseName();
+  if (target_name_.value().empty())
+    target_name_ = full_path_.BaseName();
   if (start_timer)
     StartProgressTimer();
 }

chrome/browser/download/download_item.h
@@ -187,23 +187,31 @@ class DownloadItem {
     safety_state_ = safety_state;
   }
   bool auto_opened() { return auto_opened_; }
-  FilePath original_name() const { return original_name_; }
+  FilePath target_name() const { return target_name_; }
   bool save_as() const { return save_as_; }
   bool is_otr() const { return is_otr_; }
   bool is_extension_install() const { return is_extension_install_; }
   bool name_finalized() const { return name_finalized_; }
   bool is_temporary() const { return is_temporary_; }
-  bool need_final_rename() const { return need_final_rename_; }
-  void set_need_final_rename(bool need_final_rename) {
-    need_final_rename_ = need_final_rename;
-  }
   void set_opened(bool opened) { opened_ = opened; }
   bool opened() const { return opened_; }
 
+  // Returns the final target file path for the download.
+  FilePath GetTargetFilePath() const;
+
   // Returns the file-name that should be reported to the user, which is
-  // file_name_ for safe downloads and original_name_ for dangerous ones with
-  // the uniquifier number.
-  FilePath GetFileName() const;
+  // target_name_ possibly with the uniquifier number.
+  FilePath GetFileNameToReportUser() const;
+
+  // Returns the user-verified target name for the download.
+  // This returns the same path as target_name() for safe downloads
+  // but does not for dangerous downloads until the name is verified.
+  FilePath GetUserVerifiedFileName() const;
+
+  // Returns true if the current file name is not the final target name yet.
+  bool NeedsRename() const {
+    return target_name_ != full_path_.BaseName();
+  }
 
  private:
   void Init(bool start_timer);
@@ -218,15 +226,12 @@ class DownloadItem {
   // Request ID assigned by the ResourceDispatcherHost.
   int32 id_;
 
-  // Full path to the downloaded file
+  // Full path to the downloaded or downloading file.
   FilePath full_path_;
 
   // A number that should be appended to the path to make it unique, or 0 if the
   // path should be used as is.
   int path_uniquifier_;
-
-  // Short display version of the file
-  FilePath file_name_;
 
   // The URL from whence we came.
   GURL url_;
@@ -283,9 +288,10 @@ class DownloadItem {
   // before the observer is added.
   bool auto_opened_;
 
-  // Dangerous download are given temporary names until the user approves them.
-  // This stores their original name.
-  FilePath original_name_;
+  // Dangerous downloads or ongoing downloads are given temporary names until
+  // the user approves them or the downloads finish.
+  // This stores their final target name.
+  FilePath target_name_;
 
   // For canceling or pausing requests.
   int render_process_id_;
@@ -305,9 +311,6 @@ class DownloadItem {
 
   // True if the item was downloaded temporarily.
   bool is_temporary_;
-
-  // True if the file needs final rename.
-  bool need_final_rename_;
 
   // Did the user open the item either directly or indirectly (such as by
   // setting always open files of this type)? The shelf also sets this field

chrome/browser/gtk/download_item_gtk.cc
@@ -355,8 +355,8 @@ void DownloadItemGtk::OnDownloadUpdated(DownloadItem* download) {
     parent_shelf_->MaybeShowMoreDownloadItems();
   }
 
-  if (download->full_path() != icon_filepath_) {
-    // Turns out the file path is "unconfirmed %d.download" for dangerous
+  if (download->GetUserVerifiedFileName() != icon_filename_) {
+    // Turns out the file path is "unconfirmed %d.crdownload" for dangerous
     // downloads. When the download is confirmed, the file is renamed on
     // another thread, so reload the icon if the download filename changes.
     LoadIcon();
@@ -510,18 +510,18 @@ void DownloadItemGtk::OnLoadLargeIconComplete(IconManager::Handle handle,
 void DownloadItemGtk::LoadIcon() {
   icon_consumer_.CancelAllRequests();
   IconManager* im = g_browser_process->icon_manager();
-  icon_filepath_ = get_download()->full_path();
-  im->LoadIcon(icon_filepath_,
+  icon_filename_ = get_download()->GetUserVerifiedFileName();
+  im->LoadIcon(icon_filename_,
                IconLoader::SMALL, &icon_consumer_,
                NewCallback(this, &DownloadItemGtk::OnLoadSmallIconComplete));
-  im->LoadIcon(icon_filepath_,
+  im->LoadIcon(icon_filename_,
                IconLoader::LARGE, &icon_consumer_,
                NewCallback(this, &DownloadItemGtk::OnLoadLargeIconComplete));
 }
 
 void DownloadItemGtk::UpdateTooltip() {
   string16 elided_filename = gfx::ElideFilename(
-      get_download()->GetFileName(),
+      get_download()->GetFileNameToReportUser(),
       gfx::Font(), kTooltipMaxWidth);
   gtk_widget_set_tooltip_text(body_.get(),
                               UTF16ToUTF8(elided_filename).c_str());
@@ -533,7 +533,7 @@ void DownloadItemGtk::UpdateNameLabel() {
   // much padding when we set the size request. We need to either use gfx::Font
   // or somehow extend TextElider.
   string16 elided_filename = gfx::ElideFilename(
-      get_download()->GetFileName(),
+      get_download()->GetFileNameToReportUser(),
       gfx::Font(), kTextWidth);
 
   GdkColor color = theme_provider_->GetGdkColor(
@@ -584,7 +584,7 @@ void DownloadItemGtk::UpdateDangerWarning() {
           l10n_util::GetStringUTF16(IDS_PROMPT_DANGEROUS_DOWNLOAD_EXTENSION);
     } else {
       string16 elided_filename = gfx::ElideFilename(
-          get_download()->original_name(), gfx::Font(), kTextWidth);
+          get_download()->target_name(), gfx::Font(), kTextWidth);
 
       dangerous_warning =
           l10n_util::GetStringFUTF16(IDS_PROMPT_DANGEROUS_DOWNLOAD,

chrome/browser/gtk/download_item_gtk.h
@@ -202,8 +202,8 @@ class DownloadItemGtk : public DownloadItem::Observer,
   SkBitmap* icon_small_;
   SkBitmap* icon_large_;
 
-  // The last download file path for which we requested an icon.
-  FilePath icon_filepath_;
+  // The last download file name for which we requested an icon.
+  FilePath icon_filename_;
 
   NotificationRegistrar registrar_;
 

chrome/browser/views/download_item_view.cc
@@ -209,7 +209,7 @@ DownloadItemView::DownloadItemView(DownloadItem* download,
   dangerous_mode_body_image_set_ = dangerous_mode_body_image_set;
 
   LoadIcon();
-  tooltip_text_ = download_->GetFileName().ToWStringHack();
+  tooltip_text_ = download_->GetFileNameToReportUser().ToWStringHack();
 
   font_ = ResourceBundle::GetSharedInstance().GetFont(ResourceBundle::BaseFont);
   box_height_ = std::max<int>(2 * kVerticalPadding + font_.GetHeight() +
@@ -257,11 +257,11 @@ DownloadItemView::DownloadItemView(DownloadItem* download,
     // Ensure the file name is not too long.
 
     // Extract the file extension (if any).
-    FilePath filepath(download->original_name());
+    FilePath filename(download->target_name());
 #if defined(OS_LINUX)
-    std::wstring extension = base::SysNativeMBToWide(filepath.Extension());
+    std::wstring extension = base::SysNativeMBToWide(filename.Extension());
 #else
-    std::wstring extension = filepath.Extension();
+    std::wstring extension = filename.Extension();
 #endif
 
     // Remove leading '.'
@@ -269,9 +269,9 @@ DownloadItemView::DownloadItemView(DownloadItem* download,
       extension = extension.substr(1);
 #if defined(OS_LINUX)
     std::wstring rootname =
-        base::SysNativeMBToWide(filepath.BaseName().RemoveExtension().value());
+        base::SysNativeMBToWide(filename.RemoveExtension().value());
 #else
-    std::wstring rootname = filepath.BaseName().RemoveExtension().value();
+    std::wstring rootname = filename.RemoveExtension().value();
 #endif
 
     // Elide giant extensions (this shouldn't currently be hit, but might
@@ -602,7 +602,7 @@ void DownloadItemView::Paint(gfx::Canvas* canvas) {
   if (!IsDangerousMode()) {
     string16 filename;
     if (!disabled_while_opening_) {
-      filename = gfx::ElideFilename(download_->GetFileName(),
+      filename = gfx::ElideFilename(download_->GetFileNameToReportUser(),
                                     font_, kTextWidth);
     } else {
       // First, Calculate the download status opening string width.
@@ -612,7 +612,7 @@ void DownloadItemView::Paint(gfx::Canvas* canvas) {
       int status_string_width = font_.GetStringWidth(status_string);
       // Then, elide the file name.
       string16 filename_string =
-          gfx::ElideFilename(download_->GetFileName(), font_,
+          gfx::ElideFilename(download_->GetFileNameToReportUser(), font_,
                              kTextWidth - status_string_width);
       // Last, concat the whole string.
       filename = l10n_util::GetStringFUTF16(IDS_DOWNLOAD_STATUS_OPENING,
@@ -637,7 +637,7 @@ void DownloadItemView::Paint(gfx::Canvas* canvas) {
   // Paint the icon.
   IconManager* im = g_browser_process->icon_manager();
   SkBitmap* icon = IsDangerousMode() ? warning_icon_ :
-      im->LookupIcon(download_->full_path(), IconLoader::SMALL);
+      im->LookupIcon(download_->GetUserVerifiedFileName(), IconLoader::SMALL);
 
   // We count on the fact that the icon manager will cache the icons and if one
   // is available, it will be cached here. We *don't* want to request the icon
@@ -731,7 +731,7 @@ void DownloadItemView::ClearDangerousMode() {
 
   // We need to load the icon now that the download_ has the real path.
   LoadIcon();
-  tooltip_text_ = download_->GetFileName().ToWStringHack();
+  tooltip_text_ = download_->GetFileNameToReportUser().ToWStringHack();
 
   // Force the shelf to layout again as our size has changed.
   parent_->Layout();
@@ -849,7 +849,7 @@ bool DownloadItemView::OnMouseDragged(const views::MouseEvent& event) {
   if (dragging_) {
     if (download_->state() == DownloadItem::COMPLETE) {
       IconManager* im = g_browser_process->icon_manager();
-      SkBitmap* icon = im->LookupIcon(download_->full_path(),
+      SkBitmap* icon = im->LookupIcon(download_->GetUserVerifiedFileName(),
                                       IconLoader::SMALL);
       if (icon) {
         views::Widget* widget = GetWidget();
@@ -960,7 +960,8 @@ void DownloadItemView::OnExtractIconComplete(IconManager::Handle handle,
 
 void DownloadItemView::LoadIcon() {
   IconManager* im = g_browser_process->icon_manager();
-  im->LoadIcon(download_->full_path(), IconLoader::SMALL, &icon_consumer_,
+  im->LoadIcon(download_->GetUserVerifiedFileName(),
+               IconLoader::SMALL, &icon_consumer_,
                NewCallback(this, &DownloadItemView::OnExtractIconComplete));
 }
 
@@ -1062,7 +1063,7 @@ void DownloadItemView::UpdateAccessibleName() {
     new_name = dangerous_download_label_->GetText();
   } else {
     new_name = status_text_ + L" " +
-        download_->GetFileName().ToWStringHack();
+        download_->GetFileNameToReportUser().ToWStringHack();
   }
 
   // If the name has changed, call SetAccessibleName and notify

commit 08dcf740fc726f6013319f8e5a7600d6743522d8

REGRESSION: Show correct exe icon in the downloadshelf
The bug has been introduced in r65418.
In r65418 I replaced some DownloadItem::full_path() with DownloadItem::GetUserVerifiedFileName() for loading icons, but on Windows it needed to be a full-path (rather than a file name).

BUG=62495
TEST=download mini_installer.exe and see if the correct icon is shown in the downloadshelf after the download is finished.
TEST=download regular files and see if the correct icon is shown (from the beginning).

Review URL: http://codereview.chromium.org/5147006

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66838 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/cocoa/download/download_item_mac.h
@@ -54,8 +54,8 @@ class DownloadItemMac : DownloadItem::Observer {
   // For canceling an in progress icon request.
   CancelableRequestConsumerT<int, 0> icon_consumer_;
 
-  // Stores the last known name where the file will be saved.
-  FilePath lastFileName_;
+  // Stores the last known path where the file will be saved.
+  FilePath lastFilePath_;
 
   DISALLOW_COPY_AND_ASSIGN(DownloadItemMac);
 };

chrome/browser/cocoa/download/download_item_mac.mm
@@ -34,12 +34,12 @@ void DownloadItemMac::OnDownloadUpdated(DownloadItem* download) {
     [item_controller_ clearDangerousMode];
   }
 
-  if (download->GetUserVerifiedFileName() != lastFileName_) {
+  if (download->GetUserVerifiedFilePath() != lastFilePath_) {
     // Turns out the file path is "unconfirmed %d.crdownload" for dangerous
     // downloads. When the download is confirmed, the file is renamed on
     // another thread, so reload the icon if the download filename changes.
     LoadIcon();
-    lastFileName_ = download->GetUserVerifiedFileName();
+    lastFilePath_ = download->GetUserVerifiedFilePath();
 
     [item_controller_ updateToolTip];
   }
@@ -72,7 +72,7 @@ void DownloadItemMac::LoadIcon() {
   }
 
   // We may already have this particular image cached.
-  FilePath file = download_model_->download()->GetUserVerifiedFileName();
+  FilePath file = download_model_->download()->GetUserVerifiedFilePath();
   SkBitmap* icon_bitmap = icon_manager->LookupIcon(file, IconLoader::SMALL);
   if (icon_bitmap) {
     NSImage* icon = gfx::SkBitmapToNSImage(*icon_bitmap);

chrome/browser/dom_ui/downloads_dom_handler.cc
@@ -159,7 +159,7 @@ void DownloadsDOMHandler::HandleDrag(const ListValue* args) {
   DownloadItem* file = GetDownloadByValue(args);
   if (file) {
     IconManager* im = g_browser_process->icon_manager();
-    SkBitmap* icon = im->LookupIcon(file->GetUserVerifiedFileName(),
+    SkBitmap* icon = im->LookupIcon(file->GetUserVerifiedFilePath(),
                                     IconLoader::NORMAL);
     gfx::NativeView view = dom_ui_->tab_contents()->GetNativeView();
     download_util::DragDownload(file, icon, view);

chrome/browser/download/download_item.cc
@@ -169,15 +169,15 @@ bool DownloadItem::CanOpenDownload() {
 
 bool DownloadItem::ShouldOpenFileBasedOnExtension() {
   return download_manager_->ShouldOpenFileBasedOnExtension(
-      GetUserVerifiedFileName());
+      GetUserVerifiedFilePath());
 }
 
 void DownloadItem::OpenFilesBasedOnExtension(bool open) {
   DownloadPrefs* prefs = download_manager_->download_prefs();
   if (open)
-    prefs->EnableAutoOpenBasedOnExtension(GetUserVerifiedFileName());
+    prefs->EnableAutoOpenBasedOnExtension(GetUserVerifiedFilePath());
   else
-    prefs->DisableAutoOpenBasedOnExtension(GetUserVerifiedFileName());
+    prefs->DisableAutoOpenBasedOnExtension(GetUserVerifiedFilePath());
 }
 
 void DownloadItem::OpenDownload() {
@@ -277,7 +277,7 @@ void DownloadItem::Finished() {
     auto_opened_ = true;
   } else if (open_when_complete() ||
              download_manager_->ShouldOpenFileBasedOnExtension(
-                 GetUserVerifiedFileName()) ||
+                 GetUserVerifiedFilePath()) ||
              is_temporary()) {
     // If the download is temporary, like in drag-and-drop, do not open it but
     // we still need to set it auto-opened so that it can be removed from the
@@ -431,10 +431,10 @@ FilePath DownloadItem::GetFileNameToReportUser() const {
   return target_name_;
 }
 
-FilePath DownloadItem::GetUserVerifiedFileName() const {
+FilePath DownloadItem::GetUserVerifiedFilePath() const {
   if (safety_state_ == DownloadItem::SAFE)
-    return target_name_;
-  return full_path_.BaseName();
+    return GetTargetFilePath();
+  return full_path_;
 }
 
 void DownloadItem::Init(bool start_timer) {

chrome/browser/download/download_item.h
@@ -212,10 +212,10 @@ class DownloadItem {
   // target_name_ possibly with the uniquifier number.
   FilePath GetFileNameToReportUser() const;
 
-  // Returns the user-verified target name for the download.
-  // This returns the same path as target_name() for safe downloads
+  // Returns the user-verified target file path for the download.
+  // This returns the same path as GetTargetFilePath() for safe downloads
   // but does not for dangerous downloads until the name is verified.
-  FilePath GetUserVerifiedFileName() const;
+  FilePath GetUserVerifiedFilePath() const;
 
   // Returns true if the current file name is not the final target name yet.
   bool NeedsRename() const {

chrome/browser/gtk/download_item_gtk.cc
@@ -355,7 +355,7 @@ void DownloadItemGtk::OnDownloadUpdated(DownloadItem* download) {
     parent_shelf_->MaybeShowMoreDownloadItems();
   }
 
-  if (download->GetUserVerifiedFileName() != icon_filename_) {
+  if (download->GetUserVerifiedFilePath() != icon_filepath_) {
     // Turns out the file path is "unconfirmed %d.crdownload" for dangerous
     // downloads. When the download is confirmed, the file is renamed on
     // another thread, so reload the icon if the download filename changes.
@@ -510,11 +510,11 @@ void DownloadItemGtk::OnLoadLargeIconComplete(IconManager::Handle handle,
 void DownloadItemGtk::LoadIcon() {
   icon_consumer_.CancelAllRequests();
   IconManager* im = g_browser_process->icon_manager();
-  icon_filename_ = get_download()->GetUserVerifiedFileName();
-  im->LoadIcon(icon_filename_,
+  icon_filepath_ = get_download()->GetUserVerifiedFilePath();
+  im->LoadIcon(icon_filepath_,
                IconLoader::SMALL, &icon_consumer_,
                NewCallback(this, &DownloadItemGtk::OnLoadSmallIconComplete));
-  im->LoadIcon(icon_filename_,
+  im->LoadIcon(icon_filepath_,
                IconLoader::LARGE, &icon_consumer_,
                NewCallback(this, &DownloadItemGtk::OnLoadLargeIconComplete));
 }

chrome/browser/gtk/download_item_gtk.h
@@ -202,8 +202,8 @@ class DownloadItemGtk : public DownloadItem::Observer,
   SkBitmap* icon_small_;
   SkBitmap* icon_large_;
 
-  // The last download file name for which we requested an icon.
-  FilePath icon_filename_;
+  // The last download file path for which we requested an icon.
+  FilePath icon_filepath_;
 
   NotificationRegistrar registrar_;
 

chrome/browser/ui/views/download_item_view.cc
@@ -638,7 +638,7 @@ void DownloadItemView::Paint(gfx::Canvas* canvas) {
   // Paint the icon.
   IconManager* im = g_browser_process->icon_manager();
   SkBitmap* icon = IsDangerousMode() ? warning_icon_ :
-      im->LookupIcon(download_->GetUserVerifiedFileName(), IconLoader::SMALL);
+      im->LookupIcon(download_->GetUserVerifiedFilePath(), IconLoader::SMALL);
 
   // We count on the fact that the icon manager will cache the icons and if one
   // is available, it will be cached here. We *don't* want to request the icon
@@ -850,7 +850,7 @@ bool DownloadItemView::OnMouseDragged(const views::MouseEvent& event) {
   if (dragging_) {
     if (download_->state() == DownloadItem::COMPLETE) {
       IconManager* im = g_browser_process->icon_manager();
-      SkBitmap* icon = im->LookupIcon(download_->GetUserVerifiedFileName(),
+      SkBitmap* icon = im->LookupIcon(download_->GetUserVerifiedFilePath(),
                                       IconLoader::SMALL);
       if (icon) {
         views::Widget* widget = GetWidget();
@@ -961,7 +961,7 @@ void DownloadItemView::OnExtractIconComplete(IconManager::Handle handle,
 
 void DownloadItemView::LoadIcon() {
   IconManager* im = g_browser_process->icon_manager();
-  im->LoadIcon(download_->GetUserVerifiedFileName(),
+  im->LoadIcon(download_->GetUserVerifiedFilePath(),
                IconLoader::SMALL, &icon_consumer_,
                NewCallback(this, &DownloadItemView::OnExtractIconComplete));
 }