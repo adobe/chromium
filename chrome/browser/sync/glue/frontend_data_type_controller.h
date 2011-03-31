adobe/chromium

commit bc7edbf10a377bcbc5bcfb3deed9b53234027226

[Sync] Refactor controllers for datatypes that live on ui thread.
In addition, changes have been made to the datatype controller interface in preparation of future refactoring. Enabled(), which is not used, has been removed. Immutable functions have been declared const. name() is now derived from the ModelTypeToString method.

Refactor of non-frontend datatypes will be coming in a later patch.

BUG=76232
TEST=anything sync related

Review URL: http://codereview.chromium.org/6777014

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80049 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/sync/glue/frontend_data_type_controller.h
@@ -1,0 +1,113 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SYNC_GLUE_FRONTEND_DATA_TYPE_CONTROLLER_H__
+#define CHROME_BROWSER_SYNC_GLUE_FRONTEND_DATA_TYPE_CONTROLLER_H__
+#pragma once
+
+#include <string>
+
+#include "base/basictypes.h"
+#include "base/memory/scoped_ptr.h"
+#include "chrome/browser/sync/glue/data_type_controller.h"
+
+class Profile;
+class ProfileSyncService;
+class ProfileSyncFactory;
+
+namespace base { class TimeDelta; }
+namespace browser_sync {
+
+class AssociatorInterface;
+class ChangeProcessor;
+
+// Implementation for datatypes that reside on the frontend thread
+// (UI thread). This is the same thread we perform initialization on, so we
+// don't have to worry about thread safety. The main start/stop funtionality is
+// implemented by default.
+// Derived classes must implement (at least):
+//    syncable::ModelType type() const
+//    void CreateSyncComponents();
+//    void RecordUnrecoverableError(
+//        const tracked_objects::Location& from_here,
+//        const std::string& message);
+//    void RecordAssociationTime(base::TimeDelta time);
+//    void RecordStartFailure(StartResult result);
+class FrontendDataTypeController : public DataTypeController {
+ public:
+  FrontendDataTypeController(
+      ProfileSyncFactory* profile_sync_factory,
+      Profile* profile,
+      ProfileSyncService* sync_service);
+  virtual ~FrontendDataTypeController();
+
+  // DataTypeController interface.
+  virtual void Start(StartCallback* start_callback);
+
+  virtual void Stop();
+
+  virtual syncable::ModelType type() const = 0;
+
+  browser_sync::ModelSafeGroup model_safe_group() const;
+
+  virtual std::string name() const;
+
+  virtual State state() const;
+
+  // UnrecoverableErrorHandler interface.
+  virtual void OnUnrecoverableError(const tracked_objects::Location& from_here,
+                                    const std::string& message);
+ protected:
+  // For testing only.
+  FrontendDataTypeController();
+
+  // Kick off any dependent services that need to be running before we can
+  // associate models. The default implementation is a no-op.
+  virtual bool StartModels();
+
+  // Build sync components and associate models.
+  virtual bool Associate();
+
+  // Perform any DataType controller specific state cleanup before stopping
+  // the datatype controller. The default implementation is a no-op.
+  virtual void CleanupState();
+
+  // Helper method to run the stashed start callback with a given result.
+  virtual void FinishStart(StartResult result,
+      const tracked_objects::Location& from_here);
+
+  // Cleans up state and calls callback when start fails.
+  virtual void StartFailed(StartResult result,
+      const tracked_objects::Location& from_here);
+
+  // Datatype specific creation of sync components.
+  virtual void CreateSyncComponents() = 0;
+
+  // DataType specific histogram methods. Because histograms use static's, the
+  // specific datatype controllers must implement this themselves.
+  // Record unrecoverable errors.
+  virtual void RecordUnrecoverableError(
+      const tracked_objects::Location& from_here,
+      const std::string& message) = 0;
+  // Record association time.
+  virtual void RecordAssociationTime(base::TimeDelta time) = 0;
+  // Record causes of start failure.
+  virtual void RecordStartFailure(StartResult result) = 0;
+
+  ProfileSyncFactory* const profile_sync_factory_;
+  Profile* const profile_;
+  ProfileSyncService* const sync_service_;
+
+  State state_;
+
+  scoped_ptr<StartCallback> start_callback_;
+  scoped_ptr<AssociatorInterface> model_associator_;
+  scoped_ptr<ChangeProcessor> change_processor_;
+
+  DISALLOW_COPY_AND_ASSIGN(FrontendDataTypeController);
+};
+
+}  // namespace browser_sync
+
+#endif  // CHROME_BROWSER_SYNC_GLUE_FRONTEND_DATA_TYPE_CONTROLLER_H__

chrome/browser/sync/glue/app_data_type_controller.cc
@@ -5,13 +5,8 @@
 #include "chrome/browser/sync/glue/app_data_type_controller.h"
 
 #include "base/metrics/histogram.h"
-#include "base/logging.h"
-#include "base/time.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/profile_sync_factory.h"
-#include "chrome/browser/sync/unrecoverable_error_handler.h"
-#include "content/browser/browser_thread.h"
 
 namespace browser_sync {
 
@@ -19,120 +14,42 @@ AppDataTypeController::AppDataTypeController(
     ProfileSyncFactory* profile_sync_factory,
     Profile* profile,
     ProfileSyncService* sync_service)
-    : profile_sync_factory_(profile_sync_factory),
-      profile_(profile),
-      sync_service_(sync_service),
-      state_(NOT_RUNNING) {
-  DCHECK(profile_sync_factory);
-  DCHECK(sync_service);
+    : FrontendDataTypeController(profile_sync_factory,
+                                 profile,
+                                 sync_service) {
 }
 
 AppDataTypeController::~AppDataTypeController() {
 }
 
-void AppDataTypeController::Start(StartCallback* start_callback) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  DCHECK(start_callback);
-  if (state_ != NOT_RUNNING) {
-    start_callback->Run(BUSY, FROM_HERE);
-    delete start_callback;
-    return;
-  }
+syncable::ModelType AppDataTypeController::type() const {
+  return syncable::APPS;
+}
 
-  start_callback_.reset(start_callback);
-
+bool AppDataTypeController::StartModels() {
   profile_->InitExtensions();
+  return true;
+}
+
+void AppDataTypeController::CreateSyncComponents() {
   ProfileSyncFactory::SyncComponents sync_components =
       profile_sync_factory_->CreateAppSyncComponents(sync_service_,
                                                      this);
   model_associator_.reset(sync_components.model_associator);
   change_processor_.reset(sync_components.change_processor);
-
-  if (!model_associator_->CryptoReadyIfNecessary()) {
-    StartFailed(NEEDS_CRYPTO, FROM_HERE);
-    return;
-  }
-
-  bool sync_has_nodes = false;
-  if (!model_associator_->SyncModelHasUserCreatedNodes(&sync_has_nodes)) {
-    StartFailed(UNRECOVERABLE_ERROR, FROM_HERE);
-    return;
-  }
-
-  base::TimeTicks start_time = base::TimeTicks::Now();
-  bool merge_success = model_associator_->AssociateModels();
-  UMA_HISTOGRAM_TIMES("Sync.AppAssociationTime",
-                      base::TimeTicks::Now() - start_time);
-  if (!merge_success) {
-    StartFailed(ASSOCIATION_FAILED, FROM_HERE);
-    return;
-  }
-
-  sync_service_->ActivateDataType(this, change_processor_.get());
-  state_ = RUNNING;
-  FinishStart(!sync_has_nodes ? OK_FIRST_RUN : OK, FROM_HERE);
 }
 
-void AppDataTypeController::Stop() {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-
-  if (state_ == MODEL_STARTING || state_ == ASSOCIATING)
-    FinishStart(ABORTED, FROM_HERE);
-  DCHECK(!start_callback_.get());
-
-  if (change_processor_ != NULL)
-    sync_service_->DeactivateDataType(this, change_processor_.get());
-
-  if (model_associator_ != NULL)
-    model_associator_->DisassociateModels();
-
-  change_processor_.reset();
-  model_associator_.reset();
-
-  state_ = NOT_RUNNING;
-}
-
-bool AppDataTypeController::enabled() {
-  return true;
-}
-
-syncable::ModelType AppDataTypeController::type() {
-  return syncable::APPS;
-}
-
-browser_sync::ModelSafeGroup AppDataTypeController::model_safe_group() {
-  return browser_sync::GROUP_UI;
-}
-
-const char* AppDataTypeController::name() const {
-  // For logging only.
-  return "app";
-}
-
-DataTypeController::State AppDataTypeController::state() {
-  return state_;
-}
-
-void AppDataTypeController::OnUnrecoverableError(
+void AppDataTypeController::RecordUnrecoverableError(
     const tracked_objects::Location& from_here,
     const std::string& message) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   UMA_HISTOGRAM_COUNTS("Sync.AppRunFailures", 1);
-  sync_service_->OnUnrecoverableError(from_here, message);
 }
 
-void AppDataTypeController::FinishStart(StartResult result,
-     const tracked_objects::Location& location) {
-  start_callback_->Run(result, location);
-  start_callback_.reset();
+void AppDataTypeController::RecordAssociationTime(base::TimeDelta time) {
+  UMA_HISTOGRAM_TIMES("Sync.AppAssociationTime", time);
 }
 
-void AppDataTypeController::StartFailed(StartResult result,
-    const tracked_objects::Location& location) {
-  model_associator_.reset();
-  change_processor_.reset();
-  start_callback_->Run(result, location);
-  start_callback_.reset();
+void AppDataTypeController::RecordStartFailure(StartResult result) {
   UMA_HISTOGRAM_ENUMERATION("Sync.AppStartFailures",
                             result,
                             MAX_START_RESULT);

chrome/browser/sync/glue/app_data_type_controller.h
@@ -8,20 +8,11 @@
 
 #include <string>
 
-#include "base/basictypes.h"
-#include "base/memory/scoped_ptr.h"
-#include "chrome/browser/sync/glue/data_type_controller.h"
-
-class Profile;
-class ProfileSyncFactory;
-class ProfileSyncService;
+#include "chrome/browser/sync/glue/frontend_data_type_controller.h"
 
 namespace browser_sync {
 
-class AssociatorInterface;
-class ChangeProcessor;
-
-class AppDataTypeController : public DataTypeController {
+class AppDataTypeController : public FrontendDataTypeController {
  public:
   AppDataTypeController(
       ProfileSyncFactory* profile_sync_factory,
@@ -30,43 +21,17 @@ class AppDataTypeController : public DataTypeController {
   virtual ~AppDataTypeController();
 
   // DataTypeController implementation.
-  virtual void Start(StartCallback* start_callback);
-
-  virtual void Stop();
-
-  virtual bool enabled();
-
-  virtual syncable::ModelType type();
-
-  virtual browser_sync::ModelSafeGroup model_safe_group();
-
-  virtual const char* name() const;
-
-  virtual State state();
-
-  // UnrecoverableErrorHandler interface.
-  virtual void OnUnrecoverableError(
-      const tracked_objects::Location& from_here,
-      const std::string& message);
+  virtual syncable::ModelType type() const;
 
  private:
-  // Helper method to run the stashed start callback with a given result.
-  void FinishStart(StartResult result,
-      const tracked_objects::Location& location);
-
-  // Cleans up state and calls callback when start fails.
-  void StartFailed(StartResult result,
-      const tracked_objects::Location& location);
-
-  ProfileSyncFactory* profile_sync_factory_;
-  Profile* profile_;
-  ProfileSyncService* sync_service_;
-
-  State state_;
-
-  scoped_ptr<StartCallback> start_callback_;
-  scoped_ptr<AssociatorInterface> model_associator_;
-  scoped_ptr<ChangeProcessor> change_processor_;
+  // DataTypeController implementations.
+  virtual bool StartModels();
+  virtual void CreateSyncComponents();
+  virtual void RecordUnrecoverableError(
+      const tracked_objects::Location& from_here,
+      const std::string& message);
+  virtual void RecordAssociationTime(base::TimeDelta time);
+  virtual void RecordStartFailure(StartResult result);
 
   DISALLOW_COPY_AND_ASSIGN(AppDataTypeController);
 };

chrome/browser/sync/glue/autofill_data_type_controller.cc
@@ -165,20 +165,21 @@ bool AutofillDataTypeController::enabled() {
   return true;
 }
 
-syncable::ModelType AutofillDataTypeController::type() {
+syncable::ModelType AutofillDataTypeController::type() const {
   return syncable::AUTOFILL;
 }
 
-browser_sync::ModelSafeGroup AutofillDataTypeController::model_safe_group() {
+browser_sync::ModelSafeGroup AutofillDataTypeController::model_safe_group()
+    const {
   return browser_sync::GROUP_DB;
 }
 
-const char* AutofillDataTypeController::name() const {
+std::string AutofillDataTypeController::name() const {
   // For logging only.
   return "autofill";
 }
 
-DataTypeController::State AutofillDataTypeController::state() {
+DataTypeController::State AutofillDataTypeController::state() const {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   return state_;
 }

chrome/browser/sync/glue/autofill_data_type_controller.h
@@ -43,13 +43,13 @@ class AutofillDataTypeController : public DataTypeController,
 
   virtual bool enabled();
 
-  virtual syncable::ModelType type();
+  virtual syncable::ModelType type() const;
 
-  virtual browser_sync::ModelSafeGroup model_safe_group();
+  virtual browser_sync::ModelSafeGroup model_safe_group() const;
 
-  virtual const char* name() const;
+  virtual std::string name() const;
 
-  virtual State state();
+  virtual State state() const;
 
   // UnrecoverableHandler implementation
   virtual void OnUnrecoverableError(const tracked_objects::Location& from_here,

chrome/browser/sync/glue/autofill_profile_data_type_controller.cc
@@ -19,11 +19,11 @@ AutofillProfileDataTypeController::AutofillProfileDataTypeController(
 
 AutofillProfileDataTypeController::~AutofillProfileDataTypeController() {}
 
-syncable::ModelType AutofillProfileDataTypeController::type() {
+syncable::ModelType AutofillProfileDataTypeController::type() const {
   return syncable::AUTOFILL_PROFILE;
 }
 
-const char* AutofillProfileDataTypeController::name() const {
+std::string AutofillProfileDataTypeController::name() const {
   // For logging only.
   return "autofill_profile";
 }

chrome/browser/sync/glue/autofill_profile_data_type_controller.h
@@ -19,9 +19,9 @@ class AutofillProfileDataTypeController : public AutofillDataTypeController {
       ProfileSyncService* sync_service);
   virtual ~AutofillProfileDataTypeController();
 
-  virtual syncable::ModelType type();
+  virtual syncable::ModelType type() const;
 
-  virtual const char* name() const;
+  virtual std::string name() const;
 
  protected:
   virtual ProfileSyncFactory::SyncComponents CreateSyncComponents(

chrome/browser/sync/glue/bookmark_data_type_controller.cc
@@ -4,13 +4,9 @@
 
 #include "chrome/browser/sync/glue/bookmark_data_type_controller.h"
 
-#include "base/logging.h"
 #include "base/metrics/histogram.h"
-#include "base/time.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/sync/glue/bookmark_change_processor.h"
-#include "chrome/browser/sync/glue/bookmark_model_associator.h"
 #include "chrome/browser/sync/profile_sync_factory.h"
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "content/browser/browser_thread.h"
@@ -24,96 +20,31 @@ BookmarkDataTypeController::BookmarkDataTypeController(
     ProfileSyncFactory* profile_sync_factory,
     Profile* profile,
     ProfileSyncService* sync_service)
-    : profile_sync_factory_(profile_sync_factory),
-      profile_(profile),
-      sync_service_(sync_service),
-      state_(NOT_RUNNING) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  DCHECK(profile_sync_factory);
-  DCHECK(profile);
-  DCHECK(sync_service);
+    : FrontendDataTypeController(profile_sync_factory,
+                                 profile,
+                                 sync_service) {
 }
 
 BookmarkDataTypeController::~BookmarkDataTypeController() {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 }
 
-void BookmarkDataTypeController::Start(StartCallback* start_callback) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  DCHECK(start_callback);
-  if (state_ != NOT_RUNNING) {
-    start_callback->Run(BUSY, FROM_HERE);
-    delete start_callback;
-    return;
-  }
-
-  start_callback_.reset(start_callback);
-
-  if (!enabled()) {
-    FinishStart(NOT_ENABLED);
-    return;
-  }
-
-  state_ = MODEL_STARTING;
-
+// We want to start the bookmark model before we begin associating.
+bool BookmarkDataTypeController::StartModels() {
   // If the bookmarks model is loaded, continue with association.
   BookmarkModel* bookmark_model = profile_->GetBookmarkModel();
   if (bookmark_model && bookmark_model->IsLoaded()) {
-    Associate();
-    return;
+    return true;  // Continue to Associate().
   }
 
   // Add an observer and continue when the bookmarks model is loaded.
   registrar_.Add(this, NotificationType::BOOKMARK_MODEL_LOADED,
                  Source<Profile>(sync_service_->profile()));
+  return false;  // Don't continue Start.
 }
 
-void BookmarkDataTypeController::Stop() {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  // If Stop() is called while Start() is waiting for the bookmark
-  // model to load, abort the start.
-  if (state_ == MODEL_STARTING)
-    FinishStart(ABORTED);
-
+// Cleanup for our extra registrar usage.
+void BookmarkDataTypeController::CleanupState() {
   registrar_.RemoveAll();
-  if (change_processor_ != NULL)
-    sync_service_->DeactivateDataType(this, change_processor_.get());
-
-  if (model_associator_ != NULL)
-    model_associator_->DisassociateModels();
-
-  change_processor_.reset();
-  model_associator_.reset();
-
-  state_ = NOT_RUNNING;
-}
-
-bool BookmarkDataTypeController::enabled() {
-  return true;
-}
-
-syncable::ModelType BookmarkDataTypeController::type() {
-  return syncable::BOOKMARKS;
-}
-
-browser_sync::ModelSafeGroup BookmarkDataTypeController::model_safe_group() {
-  return browser_sync::GROUP_UI;
-}
-
-const char* BookmarkDataTypeController::name() const {
-  // For logging only.
-  return "bookmark";
-}
-
-DataTypeController::State BookmarkDataTypeController::state() {
-  return state_;
-}
-
-void BookmarkDataTypeController::OnUnrecoverableError(
-    const tracked_objects::Location& from_here, const std::string& message) {
-  // The ProfileSyncService will invoke our Stop() method in response to this.
-  UMA_HISTOGRAM_COUNTS("Sync.BookmarkRunFailures", 1);
-  sync_service_->OnUnrecoverableError(from_here, message);
 }
 
 void BookmarkDataTypeController::Observe(NotificationType type,
@@ -122,58 +53,33 @@ void BookmarkDataTypeController::Observe(NotificationType type,
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   DCHECK_EQ(NotificationType::BOOKMARK_MODEL_LOADED, type.value);
   registrar_.RemoveAll();
+  DCHECK_EQ(state_, MODEL_STARTING);
+  state_ = ASSOCIATING;
   Associate();
 }
 
-void BookmarkDataTypeController::Associate() {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  DCHECK_EQ(state_, MODEL_STARTING);
-  state_ = ASSOCIATING;
+syncable::ModelType BookmarkDataTypeController::type() const {
+  return syncable::BOOKMARKS;
+}
 
-  ProfileSyncFactory::SyncComponents sync_components =
-      profile_sync_factory_->CreateBookmarkSyncComponents(sync_service_, this);
+void BookmarkDataTypeController::CreateSyncComponents() {
+  ProfileSyncFactory::SyncComponents sync_components = profile_sync_factory_->
+      CreateBookmarkSyncComponents(sync_service_, this);
   model_associator_.reset(sync_components.model_associator);
   change_processor_.reset(sync_components.change_processor);
-
-  if (!model_associator_->CryptoReadyIfNecessary()) {
-    StartFailed(NEEDS_CRYPTO, FROM_HERE);
-    return;
-  }
-
-  bool sync_has_nodes = false;
-  if (!model_associator_->SyncModelHasUserCreatedNodes(&sync_has_nodes)) {
-    StartFailed(UNRECOVERABLE_ERROR, FROM_HERE);
-    return;
-  }
-
-  base::TimeTicks start_time = base::TimeTicks::Now();
-  bool merge_success = model_associator_->AssociateModels();
-  UMA_HISTOGRAM_TIMES("Sync.BookmarkAssociationTime",
-                      base::TimeTicks::Now() - start_time);
-  if (!merge_success) {
-    StartFailed(ASSOCIATION_FAILED, FROM_HERE);
-    return;
-  }
-
-  sync_service_->ActivateDataType(this, change_processor_.get());
-  state_ = RUNNING;
-  FinishStart(!sync_has_nodes ? OK_FIRST_RUN : OK);
 }
 
-void BookmarkDataTypeController::FinishStart(StartResult result) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  start_callback_->Run(result, FROM_HERE);
-  start_callback_.reset();
+void BookmarkDataTypeController::RecordUnrecoverableError(
+    const tracked_objects::Location& from_here,
+    const std::string& message) {
+  UMA_HISTOGRAM_COUNTS("Sync.BookmarkRunFailures", 1);
 }
 
-void BookmarkDataTypeController::StartFailed(StartResult result,
-    const tracked_objects::Location& location) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  model_associator_.reset();
-  change_processor_.reset();
-  state_ = NOT_RUNNING;
-  start_callback_->Run(result, location);
-  start_callback_.reset();
+void BookmarkDataTypeController::RecordAssociationTime(base::TimeDelta time) {
+  UMA_HISTOGRAM_TIMES("Sync.BookmarkAssociationTime", time);
+}
+
+void BookmarkDataTypeController::RecordStartFailure(StartResult result) {
   UMA_HISTOGRAM_ENUMERATION("Sync.BookmarkStartFailures",
                             result,
                             MAX_START_RESULT);

commit 01eed595cf372b12155001302db68ca0985ba84f

Fix Clang breakage from r80049
TBR=zea
BUG=76232

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80057 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/sync/glue/frontend_data_type_controller.h
@@ -49,7 +49,7 @@ class FrontendDataTypeController : public DataTypeController {
 
   virtual syncable::ModelType type() const = 0;
 
-  browser_sync::ModelSafeGroup model_safe_group() const;
+  virtual browser_sync::ModelSafeGroup model_safe_group() const;
 
   virtual std::string name() const;
 