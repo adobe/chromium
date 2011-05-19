adobe/chromium

commit d03cbbb31093d50a5cb08b3f48f760110a488e90

Convert PPB_Buffer_Impl to use SharedMemory for efficient IPC use.
BUG=none
TEST=trybots

Review URL: http://codereview.chromium.org/7039022

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85773 0039d316-1c4b-4281-b951-d872f2087c98

webkit/plugins/ppapi/ppb_buffer_impl.cc
@@ -71,8 +71,7 @@ const PPB_Buffer_Dev ppb_buffer = {
 }  // namespace
 
 PPB_Buffer_Impl::PPB_Buffer_Impl(PluginInstance* instance)
-    : Resource(instance),
-      size_(0) {
+    : Resource(instance), size_(0) {
 }
 
 PPB_Buffer_Impl::~PPB_Buffer_Impl() {
@@ -88,11 +87,11 @@ PPB_Buffer_Impl* PPB_Buffer_Impl::AsPPB_Buffer_Impl() {
 }
 
 bool PPB_Buffer_Impl::Init(uint32_t size) {
-  if (size == 0)
+  if (size == 0 || !instance())
     return false;
-  Unmap();
-  size_ = size;
-  return true;
+  shared_memory_.reset(
+      instance()->delegate()->CreateAnonymousSharedMemory(size));
+  return shared_memory_.get() != NULL;
 }
 
 void PPB_Buffer_Impl::Describe(uint32_t* size_in_bytes) const {
@@ -100,20 +99,14 @@ void PPB_Buffer_Impl::Describe(uint32_t* size_in_bytes) const {
 }
 
 void* PPB_Buffer_Impl::Map() {
-  if (size_ == 0)
+  if (!shared_memory_.get() || !shared_memory_->Map(size_))
     return NULL;
-
-  if (!is_mapped()) {
-    mem_buffer_.reset(new unsigned char[size_]);
-    memset(mem_buffer_.get(), 0, size_);
-  }
-  return mem_buffer_.get();
+  return shared_memory_->memory();
 }
 
 void PPB_Buffer_Impl::Unmap() {
-  mem_buffer_.reset();
+  shared_memory_->Unmap();
 }
 
 }  // namespace ppapi
 }  // namespace webkit
-

content/renderer/pepper_plugin_delegate_impl.cc
@@ -1296,3 +1296,26 @@ std::string PepperPluginDelegateImpl::GetFlashCommandLineArgs() {
   return CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
       switches::kPpapiFlashArgs);
 }
+
+base::SharedMemory* PepperPluginDelegateImpl::CreateAnonymousSharedMemory(
+    uint32_t size) {
+  if (size == 0)
+    return NULL;
+  scoped_ptr<base::SharedMemory> shm(new base::SharedMemory());
+  if (shm->CreateAnonymous(size))
+    return shm.release();
+  // The sandbox can prevent CreateAnonymous from succeeding, in which case we
+  // need to IPC to the browser process.
+  base::SharedMemoryHandle handle;
+  if (!render_view_->Send(
+          new ViewHostMsg_AllocateSharedMemoryBuffer(size, &handle))) {
+    DLOG(WARNING) << "Browser allocation request message failed";
+    return NULL;
+  }
+  if (!base::SharedMemory::IsHandleValid(handle)) {
+    DLOG(WARNING) << "Browser failed to allocate shared memory";
+    return NULL;
+  }
+  shm.reset(new base::SharedMemory(handle, false));
+  return shm.release();
+}

content/renderer/pepper_plugin_delegate_impl.h
@@ -272,6 +272,7 @@ class PepperPluginDelegateImpl
   virtual webkit_glue::P2PTransport* CreateP2PTransport();
   virtual double GetLocalTimeZoneOffset(base::Time t);
   virtual std::string GetFlashCommandLineArgs();
+  virtual base::SharedMemory* CreateAnonymousSharedMemory(uint32_t size);
 
  private:
   // Asynchronously attempts to create a PPAPI broker for the given plugin.

webkit/plugins/ppapi/mock_plugin_delegate.cc
@@ -243,5 +243,10 @@ std::string MockPluginDelegate::GetFlashCommandLineArgs() {
   return std::string();
 }
 
+base::SharedMemory* MockPluginDelegate::CreateAnonymousSharedMemory(
+    uint32_t size) {
+  return NULL;
+}
+
 }  // namespace ppapi
 }  // namespace webkit

webkit/plugins/ppapi/mock_plugin_delegate.h
@@ -104,6 +104,7 @@ class MockPluginDelegate : public PluginDelegate {
   virtual webkit_glue::P2PTransport* CreateP2PTransport();
   virtual double GetLocalTimeZoneOffset(base::Time t);
   virtual std::string GetFlashCommandLineArgs();
+  virtual base::SharedMemory* CreateAnonymousSharedMemory(uint32_t size);
 };
 
 }  // namespace ppapi

webkit/plugins/ppapi/plugin_delegate.h
@@ -394,6 +394,10 @@ class PluginDelegate {
   // TODO(viettrungluu): Generalize this for use with other plugins if it proves
   // necessary.
   virtual std::string GetFlashCommandLineArgs() = 0;
+
+  // Create an anonymous shared memory segment of size |size| bytes, and return
+  // a pointer to it, or NULL on error.  Caller owns the returned pointer.
+  virtual base::SharedMemory* CreateAnonymousSharedMemory(uint32_t size) = 0;
 };
 
 }  // namespace ppapi

webkit/plugins/ppapi/ppb_buffer_impl.h
@@ -7,6 +7,7 @@
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/shared_memory.h"
 #include "webkit/plugins/ppapi/resource.h"
 
 struct PPB_Buffer_Dev;
@@ -22,28 +23,31 @@ class PPB_Buffer_Impl : public Resource {
   virtual ~PPB_Buffer_Impl();
 
   uint32_t size() const { return size_; }
-  unsigned char* mapped_buffer() { return mem_buffer_.get(); }
+  unsigned char* mapped_buffer() const {
+    return static_cast<unsigned char*>(shared_memory_->memory());
+  }
+  base::SharedMemoryHandle handle() const { return shared_memory_->handle(); }
 
   // Returns true if this buffer is mapped. False means that the buffer is
   // either invalid or not mapped.
-  bool is_mapped() const { return !!mem_buffer_.get(); }
+  bool is_mapped() const { return mapped_buffer() != NULL; }
 
-  // Returns a pointer to the interface implementing PPB_PPB_Buffer_Impl that is
+  // Returns a pointer to the interface implementing PPB_Buffer_Impl that is
   // exposed to the plugin.
   static const PPB_Buffer_Dev* GetInterface();
 
   // Resource overrides.
   virtual PPB_Buffer_Impl* AsPPB_Buffer_Impl();
 
-  // PPB_PPB_Buffer_Impl implementation.
+  // PPB_Buffer_Impl implementation.
   bool Init(uint32_t size);
   void Describe(uint32_t* size_in_bytes) const;
   void* Map();
   void Unmap();
 
  private:
+  scoped_ptr<base::SharedMemory> shared_memory_;
   uint32_t size_;
-  scoped_array<unsigned char> mem_buffer_;
 
   DISALLOW_COPY_AND_ASSIGN(PPB_Buffer_Impl);
 };
@@ -52,4 +56,3 @@ class PPB_Buffer_Impl : public Resource {
 }  // namespace webkit
 
 #endif  // WEBKIT_PLUGINS_PPAPI_PPB_BUFFER_IMPL_H_
-

commit 23206a04c6f78f2b526c0feae0f04a4c0d848399

Fix bug introduced in r85773: PPB_Buffer_Impl::Init() never set size_!
BUG=83207
TEST=./ninja/chrome --enable-print-preview /path/to/file.pdf has working print preview again, instead of either DCHECK crash (dbg build) or blank preview (release build)

Review URL: http://codereview.chromium.org/7040019

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85986 0039d316-1c4b-4281-b951-d872f2087c98

webkit/plugins/ppapi/ppb_buffer_impl.cc
@@ -89,6 +89,7 @@ PPB_Buffer_Impl* PPB_Buffer_Impl::AsPPB_Buffer_Impl() {
 bool PPB_Buffer_Impl::Init(uint32_t size) {
   if (size == 0 || !instance())
     return false;
+  size_ = size;
   shared_memory_.reset(
       instance()->delegate()->CreateAnonymousSharedMemory(size));
   return shared_memory_.get() != NULL;
@@ -99,7 +100,9 @@ void PPB_Buffer_Impl::Describe(uint32_t* size_in_bytes) const {
 }
 
 void* PPB_Buffer_Impl::Map() {
-  if (!shared_memory_.get() || !shared_memory_->Map(size_))
+  DCHECK(size_);
+  DCHECK(shared_memory_.get());
+  if (!shared_memory_->Map(size_))
     return NULL;
   return shared_memory_->memory();
 }