adobe/chromium

commit beafa063ad3449ccbac90f44405950d5b7c66b5f

Reland 87371 - Support for glSetSurfaceCHROMIUM.
This command allows a previously created GPU surface to be made current for a command buffer. There are no surfaces registered at this point so this command is currently a no-op.
Review URL: http://codereview.chromium.org/7077001
Review URL: http://codereview.chromium.org/7006019

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87408 0039d316-1c4b-4281-b951-d872f2087c98

content/common/gpu/gpu_channel.h
@@ -14,6 +14,7 @@
 #include "base/memory/scoped_ptr.h"
 #include "base/process.h"
 #include "build/build_config.h"
+#include "gpu/command_buffer/service/surface_manager.h"
 #include "content/common/gpu/gpu_command_buffer_stub.h"
 #include "content/common/gpu/gpu_surface_stub.h"
 #include "content/common/message_router.h"
@@ -31,10 +32,15 @@ class MessageLoopProxy;
 class WaitableEvent;
 }
 
+namespace gfx {
+class GLSurface;
+}
+
 // Encapsulates an IPC channel between the GPU process and one renderer
 // process. On the renderer side there's a corresponding GpuChannelHost.
 class GpuChannel : public IPC::Channel::Listener,
                    public IPC::Message::Sender,
+                   public gpu::SurfaceManager,
                    public base::RefCountedThreadSafe<GpuChannel> {
  public:
   // Takes ownership of the renderer process handle.
@@ -85,6 +91,9 @@ class GpuChannel : public IPC::Channel::Listener,
 #endif
 
   void LoseAllContexts();
+
+  // Look up a GLSurface by ID. In this case the ID is the IPC routing ID.
+  gfx::GLSurface* LookupSurface(int surface_id);
 
   // Get the TransportTexture by ID.
   TransportTexture* GetTransportTexture(int32 route_id);

gpu/command_buffer/service/surface_manager_mock.h
@@ -1,0 +1,23 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GPU_COMMAND_BUFFER_SERVICE_SURFACE_MANAGER_MOCK_H_
+#define GPU_COMMAND_BUFFER_SERVICE_SURFACE_MANAGER_MOCK_H_
+
+#include "gpu/command_buffer/service/surface_manager.h"
+
+namespace gpu {
+
+class MockSurfaceManager : public SurfaceManager {
+ public:
+  MockSurfaceManager() {}
+  MOCK_METHOD1(LookupSurface, gfx::GLSurface*(int id));
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MockSurfaceManager);
+};
+
+}  // namespace gpu
+
+#endif  // GPU_COMMAND_BUFFER_SERVICE_SURFACE_MANAGER_MOCK_H_

gpu/gpu.gyp
@@ -226,6 +226,8 @@
         'command_buffer/service/shader_manager.cc',
         'command_buffer/service/shader_translator.h',
         'command_buffer/service/shader_translator.cc',
+        'command_buffer/service/surface_manager.cc',
+        'command_buffer/service/surface_manager.h',
         'command_buffer/service/texture_manager.h',
         'command_buffer/service/texture_manager.cc',
       ],
@@ -293,6 +295,7 @@
         'command_buffer/service/renderbuffer_manager_unittest.cc',
         'command_buffer/service/shader_manager_unittest.cc',
         'command_buffer/service/shader_translator_unittest.cc',
+        'command_buffer/service/surface_manager_mock.h',
         'command_buffer/service/test_helper.cc',
         'command_buffer/service/test_helper.h',
         'command_buffer/service/texture_manager_unittest.cc',

content/common/gpu/gpu_channel.cc
@@ -126,6 +126,14 @@ void GpuChannel::LoseAllContexts() {
   gpu_channel_manager_->LoseAllContexts();
 }
 
+gfx::GLSurface* GpuChannel::LookupSurface(int surface_id) {
+  GpuSurfaceStub *surface_stub = surfaces_.Lookup(surface_id);
+  if (!surface_stub)
+    return NULL;
+
+  return surface_stub->surface();
+}
+
 void GpuChannel::CreateViewCommandBuffer(
     gfx::PluginWindowHandle window,
     int32 render_view_id,
@@ -259,7 +267,7 @@ void GpuChannel::OnCreateOffscreenSurface(const gfx::Size& size,
   *route_id = MSG_ROUTING_NONE;
 
 #if defined(ENABLE_GPU)
-  scoped_ptr<gfx::GLSurface> surface(
+  scoped_refptr<gfx::GLSurface> surface(
        gfx::GLSurface::CreateOffscreenGLSurface(size));
   if (!surface.get())
     return;

content/common/gpu/gpu_command_buffer_stub.cc
@@ -134,7 +134,9 @@ void GpuCommandBufferStub::OnInitialize(
   if (command_buffer_->Initialize(&shared_memory, size)) {
     gpu::GpuScheduler* parent_processor =
         parent_ ? parent_->scheduler_.get() : NULL;
-    scheduler_.reset(new gpu::GpuScheduler(command_buffer_.get(), NULL));
+    scheduler_.reset(new gpu::GpuScheduler(command_buffer_.get(),
+                                           channel_,
+                                           NULL));
     if (scheduler_->Initialize(
         handle_,
         initial_size_,

content/common/gpu/gpu_surface_stub.h
@@ -8,8 +8,7 @@
 
 #if defined(ENABLE_GPU)
 
-#include "base/memory/scoped_ptr.h"
-#include "base/memory/weak_ptr.h"
+#include "base/memory/ref_counted.h"
 #include "ipc/ipc_channel.h"
 #include "ipc/ipc_message.h"
 #include "ui/gfx/gl/gl_surface.h"
@@ -18,12 +17,13 @@ class GpuChannel;
 
 class GpuSurfaceStub
     : public IPC::Channel::Listener,
-      public IPC::Message::Sender,
-      public base::SupportsWeakPtr<GpuSurfaceStub> {
+      public IPC::Message::Sender {
  public:
   // Takes ownership of surface.
   GpuSurfaceStub(GpuChannel* channel, int route_id, gfx::GLSurface* surface);
   virtual ~GpuSurfaceStub();
+
+  gfx::GLSurface* surface() const { return surface_.get(); }
 
   // IPC::Channel::Listener implementation:
   virtual bool OnMessageReceived(const IPC::Message& message);
@@ -40,7 +40,7 @@ class GpuSurfaceStub
   GpuChannel* channel_;
 
   int route_id_;
-  scoped_ptr<gfx::GLSurface> surface_;
+  scoped_refptr<gfx::GLSurface> surface_;
   DISALLOW_COPY_AND_ASSIGN(GpuSurfaceStub);
 };
 

content/gpu/gpu_info_collector.cc
@@ -18,20 +18,20 @@
 
 namespace {
 
-gfx::GLSurface* InitializeGLSurface() {
-  scoped_ptr<gfx::GLSurface> surface(gfx::GLSurface::CreateOffscreenGLSurface(
-      gfx::Size(1, 1)));
+scoped_refptr<gfx::GLSurface> InitializeGLSurface() {
+  scoped_refptr<gfx::GLSurface> surface(
+      gfx::GLSurface::CreateOffscreenGLSurface(gfx::Size(1, 1)));
   if (!surface.get()) {
     LOG(ERROR) << "gfx::GLContext::CreateOffscreenGLSurface failed";
     return NULL;
   }
 
-  return surface.release();
+  return surface;
 }
 
-gfx::GLContext* InitializeGLContext(gfx::GLSurface* surface) {
+scoped_refptr<gfx::GLContext> InitializeGLContext(gfx::GLSurface* surface) {
 
-  scoped_ptr<gfx::GLContext> context(gfx::GLContext::CreateGLContext(NULL,
+  scoped_refptr<gfx::GLContext> context(gfx::GLContext::CreateGLContext(NULL,
                                                                      surface));
   if (!context.get()) {
     LOG(ERROR) << "gfx::GLContext::CreateGLContext failed";
@@ -43,7 +43,7 @@ gfx::GLContext* InitializeGLContext(gfx::GLSurface* surface) {
     return NULL;
   }
 
-  return context.release();
+  return context;
 }
 
 std::string GetGLString(unsigned int pname) {
@@ -84,11 +84,11 @@ bool CollectGraphicsInfoGL(GPUInfo* gpu_info) {
     return false;
   }
 
-  scoped_ptr<gfx::GLSurface> surface(InitializeGLSurface());
+  scoped_refptr<gfx::GLSurface> surface(InitializeGLSurface());
   if (!surface.get())
     return false;
 
-  scoped_ptr<gfx::GLContext> context(InitializeGLContext(surface.get()));
+  scoped_refptr<gfx::GLContext> context(InitializeGLContext(surface.get()));
   if (!context.get())
     return false;
 

gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -214,6 +214,7 @@ GL_APICALL void         GL_APIENTRY glRequestExtensionCHROMIUM (const char* exte
 GL_APICALL void         GL_APIENTRY glSetLatchCHROMIUM (GLuint latch_id);
 GL_APICALL void         GL_APIENTRY glWaitLatchCHROMIUM (GLuint latch_id);
 GL_APICALL void         GL_APIENTRY glRateLimitOffscreenContextCHROMIUM (void);
+GL_APICALL void         GL_APIENTRY glSetSurfaceCHROMIUM (GLint surface_id);
 """
 
 # This is the list of all commmands that will be generated and their Id.
@@ -419,6 +420,7 @@ _CMD_ID_TABLE = {
   'RequestExtensionCHROMIUM':                                  450,
   'SetLatchCHROMIUM':                                          451,
   'WaitLatchCHROMIUM':                                         452,
+  'SetSurfaceCHROMIUM':                                        453,
 }
 
 # This is a list of enum names and their valid values. It is used to map
@@ -1488,6 +1490,12 @@ _FUNCTION_INFO = {
   },
   'ReleaseShaderCompiler': {
     'decoder_func': 'DoReleaseShaderCompiler',
+    'unit_test': False,
+  },
+  'SetSurfaceCHROMIUM': {
+    'decoder_func': 'DoSetSurfaceCHROMIUM',
+    'extension': True,
+    'chromium': True,
     'unit_test': False,
   },
   'ShaderBinary': {'type': 'Custom'},

commit 22f622a3e0340539325812fc834113664432e23c

Fix clang build failure introduced by r87408. Review URL: http://codereview.chromium.org/7027020
git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87416 0039d316-1c4b-4281-b951-d872f2087c98

content/common/gpu/gpu_channel.h
@@ -93,7 +93,7 @@ class GpuChannel : public IPC::Channel::Listener,
   void LoseAllContexts();
 
   // Look up a GLSurface by ID. In this case the ID is the IPC routing ID.
-  gfx::GLSurface* LookupSurface(int surface_id);
+  virtual gfx::GLSurface* LookupSurface(int surface_id);
 
   // Get the TransportTexture by ID.
   TransportTexture* GetTransportTexture(int32 route_id);

gpu/command_buffer/service/surface_manager_mock.cc
@@ -1,0 +1,15 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu/command_buffer/service/surface_manager_mock.h"
+
+namespace gpu {
+
+MockSurfaceManager::MockSurfaceManager() {
+}
+
+MockSurfaceManager::~MockSurfaceManager() {
+}
+
+}  // namespace gpu

gpu/command_buffer/service/surface_manager_mock.h
@@ -6,12 +6,15 @@
 #define GPU_COMMAND_BUFFER_SERVICE_SURFACE_MANAGER_MOCK_H_
 
 #include "gpu/command_buffer/service/surface_manager.h"
+#include "testing/gmock/include/gmock/gmock.h"
 
 namespace gpu {
 
 class MockSurfaceManager : public SurfaceManager {
  public:
-  MockSurfaceManager() {}
+  MockSurfaceManager();
+  virtual ~MockSurfaceManager();
+
   MOCK_METHOD1(LookupSurface, gfx::GLSurface*(int id));
 
  private:

gpu/gpu.gyp
@@ -295,6 +295,7 @@
         'command_buffer/service/renderbuffer_manager_unittest.cc',
         'command_buffer/service/shader_manager_unittest.cc',
         'command_buffer/service/shader_translator_unittest.cc',
+        'command_buffer/service/surface_manager_mock.cc',
         'command_buffer/service/surface_manager_mock.h',
         'command_buffer/service/test_helper.cc',
         'command_buffer/service/test_helper.h',