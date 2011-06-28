adobe/chromium

commit dce50d80faaed44d93fce342992df88432279665

Fixing AutoReset to be a template.
I've run into a couple of times this week when I needed one of these, for two different types besides bool.  Time to fix the TODO.

TEST=trybots FTW, and built locally.
BUG=none

Review URL: http://codereview.chromium.org/2394001

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48644 0039d316-1c4b-4281-b951-d872f2087c98

base/auto_reset.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,27 +7,30 @@
 
 #include "base/basictypes.h"
 
-// AutoReset is useful for setting a variable to some value only during a
-// particular scope.  If you have code that has to add "var = false;" or
-// "var = old_var;" at all the exit points of a block, for example, you would
-// benefit from using this instead.
+// AutoResetValue is useful for setting a variable to some value only
+// during a particular scope.  If you have code that has to add "var =
+// false;" or "var = old_var;" at all the exit points of a block, for
+// example, you would benefit from using this instead.
 //
-// NOTE: Right now this is hardcoded to work on bools, since that covers all the
-// cases where we've used it.  It would be reasonable to turn it into a template
-// class in the future.
+// This should be obvious, but note that the AutoResetValue instance
+// should have a shorter lifetime than the scoped_variable, to prevent
+// writing to invalid memory when the AutoResetValue goes out of
+// scope.
 
+template<typename T>
 class AutoReset {
  public:
-  explicit AutoReset(bool* scoped_variable, bool new_value)
+  AutoReset(T* scoped_variable, T new_value)
       : scoped_variable_(scoped_variable),
         original_value_(*scoped_variable) {
     *scoped_variable_ = new_value;
   }
+
   ~AutoReset() { *scoped_variable_ = original_value_; }
 
  private:
-  bool* scoped_variable_;
-  bool original_value_;
+  T* scoped_variable_;
+  T original_value_;
 
   DISALLOW_COPY_AND_ASSIGN(AutoReset);
 };

base/message_pump_libevent.cc
@@ -229,7 +229,7 @@ static void timer_callback(int fd, short events, void *context)
 // Reentrant!
 void MessagePumpLibevent::Run(Delegate* delegate) {
   DCHECK(keep_running_) << "Quit must have been called outside of Run!";
-  AutoReset auto_reset_in_run(&in_run_, true);
+  AutoReset<bool> auto_reset_in_run(&in_run_, true);
 
   // event_base_loopexit() + EVLOOP_ONCE is leaky, see http://crbug.com/25641.
   // Instead, make our own timer and reuse it on each call to event_base_loop().

chrome/browser/app_controller_mac.mm
@@ -84,7 +84,7 @@ Browser* ActivateBrowser(Profile* profile) {
 // to the new |Browser|.
 Browser* CreateBrowser(Profile* profile) {
   {
-    AutoReset auto_reset_in_run(&g_is_opening_new_window, true);
+    AutoReset<bool> auto_reset_in_run(&g_is_opening_new_window, true);
     Browser::OpenEmptyWindow(profile);
   }
 
@@ -804,7 +804,7 @@ void RecordLastRunAppBundlePath() {
 
   // Otherwise open a new window.
   {
-    AutoReset auto_reset_in_run(&g_is_opening_new_window, true);
+    AutoReset<bool> auto_reset_in_run(&g_is_opening_new_window, true);
     Browser::OpenEmptyWindow([self defaultProfile]);
   }
 

chrome/browser/autocomplete/autocomplete_edit_view_win.cc
@@ -1693,7 +1693,7 @@ LRESULT AutocompleteEditViewWin::OnSetText(const wchar_t* text) {
   // We wouldn't need to do this update anyway, because either we're in the
   // middle of updating the omnibox already or the caller of SetWindowText()
   // is going to update the omnibox next.
-  AutoReset auto_reset_ignore_ime_messages(&ignore_ime_messages_, true);
+  AutoReset<bool> auto_reset_ignore_ime_messages(&ignore_ime_messages_, true);
   return DefWindowProc(WM_SETTEXT, 0, reinterpret_cast<LPARAM>(text));
 }
 
@@ -2336,7 +2336,7 @@ void AutocompleteEditViewWin::StartDragIfNecessary(const CPoint& point) {
 
   scoped_refptr<BaseDragSource> drag_source(new BaseDragSource);
   DWORD dropped_mode;
-  AutoReset auto_reset_in_drag(&in_drag_, true);
+  AutoReset<bool> auto_reset_in_drag(&in_drag_, true);
   if (DoDragDrop(OSExchangeDataProviderWin::GetIDataObject(data), drag_source,
                  supported_modes, &dropped_mode) == DRAGDROP_S_DROP) {
     if ((dropped_mode == DROPEFFECT_MOVE) && (start_text == GetText())) {

chrome/browser/debugger/devtools_manager.cc
@@ -363,7 +363,7 @@ void DevToolsManager::ToggleDevToolsWindow(RenderViewHost* inspected_rvh,
   // If window is docked and visible, we hide it on toggle. If window is
   // undocked, we show (activate) it.
   if (!window->is_docked() || do_open) {
-    AutoReset auto_reset_in_initial_show(&in_initial_show_, true);
+    AutoReset<bool> auto_reset_in_initial_show(&in_initial_show_, true);
     window->Show(open_console);
   } else
     UnregisterDevToolsClientHostFor(inspected_rvh);

chrome/browser/gtk/task_manager_gtk.cc
@@ -316,7 +316,7 @@ void TaskManagerGtk::OnItemsChanged(int start, int length) {
 }
 
 void TaskManagerGtk::OnItemsAdded(int start, int length) {
-  AutoReset autoreset(&ignore_selection_changed_, true);
+  AutoReset<bool> autoreset(&ignore_selection_changed_, true);
 
   GtkTreeIter iter;
   if (start == 0) {
@@ -342,7 +342,7 @@ void TaskManagerGtk::OnItemsAdded(int start, int length) {
 
 void TaskManagerGtk::OnItemsRemoved(int start, int length) {
   {
-    AutoReset autoreset(&ignore_selection_changed_, true);
+    AutoReset<bool> autoreset(&ignore_selection_changed_, true);
 
     GtkTreeIter iter;
     gtk_tree_model_iter_nth_child(GTK_TREE_MODEL(process_list_), &iter,
@@ -848,7 +848,7 @@ void TaskManagerGtk::OnTreeViewRealize(GtkTreeView* treeview,
 void TaskManagerGtk::OnSelectionChanged(GtkTreeSelection* selection) {
   if (ignore_selection_changed_)
     return;
-  AutoReset autoreset(&ignore_selection_changed_, true);
+  AutoReset<bool> autoreset(&ignore_selection_changed_, true);
 
   // The set of groups that should be selected.
   std::set<std::pair<int, int> > ranges;

chrome/browser/renderer_host/render_widget_host.cc
@@ -312,7 +312,7 @@ BackingStore* RenderWidgetHost::GetBackingStore(bool force_create) {
   // We should never be called recursively; this can theoretically lead to
   // infinite recursion and almost certainly leads to lower performance.
   DCHECK(!in_get_backing_store_) << "GetBackingStore called recursively!";
-  AutoReset auto_reset_in_get_backing_store(&in_get_backing_store_, true);
+  AutoReset<bool> auto_reset_in_get_backing_store(&in_get_backing_store_, true);
 
   // We might have a cached backing store that we can reuse!
   BackingStore* backing_store =

chrome/browser/safe_browsing/safe_browsing_database_bloom.cc
@@ -63,7 +63,7 @@ bool SafeBrowsingDatabaseBloom::ResetDatabase() {
   if (performing_reset_)
     return false;  // Don't recurse.
 
-  AutoReset auto_reset_performing_reset(&performing_reset_, true);
+  AutoReset<bool> auto_reset_performing_reset(&performing_reset_, true);
 
   // Delete files on disk.
   bool rv = Close();

commit 1a73476cd2dff418c108a0042590bddfaecb51ab

Fix comment in base/auto_reset.h to refer to AutoReset instead of AutoResetValue.
This inconsistency was introduced in r48644. The review history for that
change, http://codereview.chromium.org/2394001, indicates that AutoResetValue
was initially used in this file but not at any of the use sites. When it
changed to AutoReset in patch set 2, matching the use sites, the comment still
was not updated.

BUG=none, cleanup
TEST=none
Review URL: http://codereview.chromium.org/7253032

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90707 0039d316-1c4b-4281-b951-d872f2087c98

base/auto_reset.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,15 +8,14 @@
 
 #include "base/basictypes.h"
 
-// AutoResetValue is useful for setting a variable to some value only
-// during a particular scope.  If you have code that has to add "var =
-// false;" or "var = old_var;" at all the exit points of a block, for
-// example, you would benefit from using this instead.
+// AutoReset<> is useful for setting a variable to a new value only within a
+// particular scope. An AutoReset<> object resets a variable to its original
+// value upon destruction, making it an alternative to writing "var = false;"
+// or "var = old_val;" at all of a block's exit points.
 //
-// This should be obvious, but note that the AutoResetValue instance
-// should have a shorter lifetime than the scoped_variable, to prevent
-// writing to invalid memory when the AutoResetValue goes out of
-// scope.
+// This should be obvious, but note that an AutoReset<> instance should have a
+// shorter lifetime than its scoped_variable, to prevent invalid memory writes
+// when the AutoReset<> object is destroyed.
 
 template<typename T>
 class AutoReset {