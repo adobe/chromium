adobe/chromium

commit bb55bee9ecbb7b7f0e4d8de32d70041c13bd6378

Print preview page selection should not require a rerendering of draft pages.
On page selection, regenerate the complete document with the selection, but preserve the existing draft pages and simply display a subset of them.

Added a new param |clear all preview data| to |PrintHostMsg_DidGetPreviewPageCount|.

BUG=84383
TEST=print preview works after code changes.

Review URL: http://codereview.chromium.org/7647010

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98926 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/print_preview/print_preview.js
@@ -24,6 +24,12 @@ const MORE_PRINTERS = 'morePrinters';
 const SIGN_IN = 'signIn';
 const PRINT_TO_PDF = 'Print to PDF';
 
+// State of the print preview settings.
+var printSettings = new PrintSettings();
+
+// Print ready data index.
+const PRINT_READY_DATA_INDEX = -1;
+
 // The name of the default or last used printer.
 var defaultOrLastUsedPrinterName = '';
 
@@ -38,6 +44,9 @@ var initialPreviewRequestID = -1;
 
 // True when a pending print file request exists.
 var hasPendingPrintDocumentRequest = false;
+
+// True when a pending print ready document request exists.
+var hasPendingPrintReadyDocumentRequest = false;
 
 // True when preview tab is hidden.
 var isTabHidden = false;
@@ -68,6 +77,11 @@ var showingSystemDialog = false;
 var firstCloudPrintOptionPos = 0;
 var lastCloudPrintOptionPos = firstCloudPrintOptionPos;
 
+// Store the current previewUid.
+var currentPreviewUid = '';
+
+// True if we need to generate draft preview data.
+var generateDraftData = true;
 
 // TODO(abodenha@chromium.org) A lot of cloud print specific logic has
 // made its way into this file.  Refactor to create a cleaner boundary
@@ -336,7 +350,8 @@ function getSettings() {
        'headerFooterEnabled': headerFooterSettings.hasHeaderFooter(),
        'defaultMarginsSelected': marginSettings.isDefaultMarginsSelected(),
        'margins': marginSettings.customMargins,
-       'requestID': -1};
+       'requestID': -1,
+       'generateDraftData': generateDraftData};
 
   var printerList = $('printer-list');
   var selectedPrinter = printerList.selectedIndex;
@@ -413,7 +428,7 @@ function getSelectedPrinterName() {
  * called once the preview loads.
  */
 function requestToPrintDocument() {
-  hasPendingPrintDocumentRequest = hasPendingPreviewRequest;
+  hasPendingPrintDocumentRequest = hasPendingPrintReadyDocumentRequest;
   var printToPDF = getSelectedPrinterName() == PRINT_TO_PDF;
 
   if (hasPendingPrintDocumentRequest) {
@@ -467,15 +482,50 @@ function sendPrintDocumentRequest() {
 }
 
 /**
+ * Loads the selected preview pages.
+ */
+function loadSelectedPages() {
+  hasPendingPreviewRequest = false;
+  pageSettings.updatePageSelection();
+  var pageSet = pageSettings.previouslySelectedPages;
+  var pageCount = pageSet.length;
+  if (pageCount == 0 || currentPreviewUid == '')
+    return;
+
+  cr.dispatchSimpleEvent(document, 'updateSummary');
+  for (var i = 0; i < pageCount; i++)
+    onDidPreviewPage(pageSet[i] - 1, currentPreviewUid, lastPreviewRequestID);
+}
+
+/**
  * Asks the browser to generate a preview PDF based on current print settings.
  */
 function requestPrintPreview() {
-  hasPendingPreviewRequest = true;
-  layoutSettings.updateState();
   if (!isTabHidden)
     showLoadingAnimation();
 
-  chrome.send('getPreview', [JSON.stringify(getSettingsWithRequestID())]);
+  if (!hasPendingPreviewRequest && previewModifiable &&
+      hasOnlyPageSettingsChanged()) {
+    loadSelectedPages();
+    generateDraftData = false;
+  } else {
+    hasPendingPreviewRequest = true;
+    generateDraftData = true;
+    pageSettings.updatePageSelection();
+  }
+
+  printSettings.save();
+  layoutSettings.updateState();
+  hasPendingPrintReadyDocumentRequest = true;
+
+  var totalPageCount = pageSettings.totalPageCount;
+  if (!previewModifiable && totalPageCount > 0)
+    generateDraftData = false;
+
+  var pageCount = totalPageCount != undefined ? totalPageCount : -1;
+  chrome.send('getPreview', [JSON.stringify(getSettingsWithRequestID()),
+                             String(pageCount),
+                             previewModifiable]);
 }
 
 /**
@@ -889,12 +939,18 @@ function onDidPreviewPage(pageNumber, previewUid, previewResponseId) {
   if (!previewModifiable)
     return;
 
-  var pageIndex = pageSettings.previouslySelectedPages.indexOf(pageNumber + 1);
-
   if (pageSettings.requestPrintPreviewIfNeeded())
     return;
-  if (pageIndex == 0)
-    createPDFPlugin(previewUid);
+
+  var pageIndex = pageSettings.previouslySelectedPages.indexOf(pageNumber + 1);
+  if (pageIndex == -1)
+    return;
+
+  currentPreviewUid = previewUid;
+  if (pageIndex == 0) {
+    createPDFPlugin(pageNumber);
+    hasPendingPreviewRequest = false;
+  }
 
   $('pdf-viewer').loadPreviewPage(
       getPageSrcURL(previewUid, pageNumber), pageIndex);
@@ -913,10 +969,12 @@ function updatePrintPreview(previewUid, previewResponseId) {
   if (!isExpectedPreviewResponse(previewResponseId))
     return;
   hasPendingPreviewRequest = false;
+  hasPendingPrintReadyDocumentRequest = false;
 
   if (!previewModifiable) {
     // If the preview is not modifiable the plugin has not been created yet.
-    createPDFPlugin(previewUid);
+    currentPreviewUid = previewUid;
+    createPDFPlugin(PRINT_READY_DATA_INDEX);
   }
 
   cr.dispatchSimpleEvent(document, 'updatePrintButton');
@@ -926,28 +984,43 @@ function updatePrintPreview(previewUid, previewResponseId) {
 }
 
 /**
- * Create the PDF plugin or reload the existing one.
- * @param {string} previewUid Preview unique identifier.
+ * Check if only page selection has been changed since the last preview request
+ * and is valid.
+ * @return {boolean} true if the new page selection is valid.
  */
-function createPDFPlugin(previewUid) {
+function hasOnlyPageSettingsChanged() {
+  var tempPrintSettings = new PrintSettings();
+  tempPrintSettings.save();
+
+  return !!(printSettings.deviceName == tempPrintSettings.deviceName &&
+            printSettings.isLandscape == tempPrintSettings.isLandscape &&
+            printSettings.hasHeaderFooter ==
+                tempPrintSettings.hasHeaderFooter &&
+            pageSettings.hasPageSelectionChangedAndIsValid());
+}
+
+/**
+ * Create the PDF plugin or reload the existing one.
+ * @param {number} srcDataIndex Preview data source index.
+ */
+function createPDFPlugin(srcDataIndex) {
   var pdfViewer = $('pdf-viewer');
+  var srcURL = getPageSrcURL(currentPreviewUid, srcDataIndex);
   if (pdfViewer) {
     // Need to call this before the reload(), where the plugin resets its
     // internal page count.
     pdfViewer.goToPage('0');
+    pdfViewer.resetPrintPreviewUrl(srcURL);
     pdfViewer.reload();
     pdfViewer.grayscale(!colorSettings.isColor());
     return;
   }
 
-  // Get the complete preview document.
-  var dataIndex = previewModifiable ? '0' : '-1';
-
   pdfViewer = document.createElement('embed');
   pdfViewer.setAttribute('id', 'pdf-viewer');
   pdfViewer.setAttribute('type',
                          'application/x-google-chrome-print-preview-pdf');
-  pdfViewer.setAttribute('src', getPageSrcURL(previewUid, dataIndex));
+  pdfViewer.setAttribute('src', srcURL);
   pdfViewer.setAttribute('aria-live', 'polite');
   pdfViewer.setAttribute('aria-atomic', 'true');
   $('mainview').appendChild(pdfViewer);
@@ -965,7 +1038,8 @@ function checkCompatiblePluginExists() {
             dummyPlugin.goToPage &&
             dummyPlugin.removePrintButton &&
             dummyPlugin.loadPreviewPage &&
-            dummyPlugin.printPreviewPageCount);
+            dummyPlugin.printPreviewPageCount &&
+            dummyPlugin.resetPrintPreviewUrl);
 }
 
 window.addEventListener('DOMContentLoaded', onLoad);
@@ -979,6 +1053,24 @@ function setDefaultValuesAndRegeneratePreview(resetMargins) {
     marginSettings.resetMarginsIfNeeded();
   pageSettings.resetState();
   requestPrintPreview();
+}
+
+/**
+ * Class that represents the state of the print settings.
+ */
+function PrintSettings() {
+  this.deviceName = '';
+  this.isLandscape = '';
+  this.hasHeaderFooter = '';
+}
+
+/**
+ *  Takes a snapshot of the print settings.
+ */
+PrintSettings.prototype.save = function() {
+  this.deviceName = getSelectedPrinterName();
+  this.isLandscape = layoutSettings.isLandscape();
+  this.hasHeaderFooter = headerFooterSettings.hasHeaderFooter();
 }
 
 /// Pull in all other scripts in a single shot.

chrome/renderer/print_web_view_helper.cc
@@ -497,6 +497,7 @@ PrintWebViewHelper::PrintWebViewHelper(RenderView* render_view)
       RenderViewObserverTracker<PrintWebViewHelper>(render_view),
       print_web_view_(NULL),
       is_preview_(switches::IsPrintPreviewEnabled()),
+      is_print_ready_metafile_sent_(false),
       user_cancelled_scripted_print_count_(0),
       notify_browser_of_print_failure_(true) {
 }
@@ -656,14 +657,24 @@ void PrintWebViewHelper::OnPrintPreview(const DictionaryValue& settings) {
     preview_params.preview_request_id =
         print_pages_params_->params.preview_request_id;
 
-    Send(new PrintHostMsg_PagesReadyForPreview(routing_id(), preview_params));
+    Send(new PrintHostMsg_MetafileReadyForPrinting(routing_id(),
+                                                   preview_params));
     return;
   }
   // Always clear |old_print_pages_params_| before rendering the pages.
   old_print_pages_params_.reset();
+  is_print_ready_metafile_sent_ = false;
 
   // PDF printer device supports alpha blending.
   print_pages_params_->params.supports_alpha_blend = true;
+
+  bool generate_draft_pages = false;
+  if (!settings.GetBoolean(printing::kSettingGenerateDraftData,
+                           &generate_draft_pages)) {
+    NOTREACHED();
+  }
+  print_preview_context_.set_generate_draft_pages(generate_draft_pages);
+
   if (CreatePreviewDocument()) {
     DidFinishPrinting(OK);
   } else {
@@ -683,6 +694,7 @@ bool PrintWebViewHelper::CreatePreviewDocument() {
   params.is_modifiable = print_preview_context_.IsModifiable();
   params.document_cookie = print_pages_params_->params.document_cookie;
   params.preview_request_id = print_pages_params_->params.preview_request_id;
+  params.clear_preview_data = print_preview_context_.generate_draft_pages();
   Send(new PrintHostMsg_DidGetPreviewPageCount(routing_id(), params));
   if (CheckForCancel())
     return false;
@@ -693,17 +705,20 @@ bool PrintWebViewHelper::CreatePreviewDocument() {
       return false;
     if (CheckForCancel())
       return false;
-  };
-
-  // Finished generating preview. Finalize the document.
-  if (!FinalizePreviewDocument())
-    return false;
+    if (print_preview_context_.IsLastPageOfPrintReadyMetafile()) {
+      // Finished generating preview. Finalize the document.
+      if (!FinalizePrintReadyDocument())
+        return false;
+    }
+  }
+  print_preview_context_.AllPagesRendered();
   print_preview_context_.Finished();
   return true;
 }
 
-bool PrintWebViewHelper::FinalizePreviewDocument() {
-  print_preview_context_.FinalizePreviewDocument();
+bool PrintWebViewHelper::FinalizePrintReadyDocument() {
+  DCHECK(!is_print_ready_metafile_sent_);
+  print_preview_context_.FinalizePrintReadyDocument();
 
   // Get the size of the resulting metafile.
   printing::PreviewMetafile* metafile = print_preview_context_.metafile();
@@ -726,7 +741,9 @@ bool PrintWebViewHelper::FinalizePreviewDocument() {
     LOG(ERROR) << "CopyMetafileDataToSharedMem failed";
     return false;
   }
-  Send(new PrintHostMsg_PagesReadyForPreview(routing_id(), preview_params));
+  is_print_ready_metafile_sent_ = true;
+
+  Send(new PrintHostMsg_MetafileReadyForPrinting(routing_id(), preview_params));
   return true;
 }
 
@@ -1254,8 +1271,10 @@ bool PrintWebViewHelper::PreviewPageRendered(int page_number,
   DCHECK_GE(page_number, printing::FIRST_PAGE_INDEX);
 
   // For non-modifiable files, |metafile| should be NULL, so do not bother
-  // sending a message.
-  if (!print_preview_context_.IsModifiable()) {
+  // sending a message. If we don't generate draft metafiles, |metafile| is
+  // NULL.
+  if (!print_preview_context_.IsModifiable() ||
+      !print_preview_context_.generate_draft_pages()) {
     DCHECK(!metafile);
     return true;
   }
@@ -1286,8 +1305,9 @@ bool PrintWebViewHelper::PreviewPageRendered(int page_number,
 PrintWebViewHelper::PrintPreviewContext::PrintPreviewContext()
     : frame_(NULL),
       total_page_count_(0),
-      actual_page_count_(0),
       current_page_index_(0),
+      generate_draft_pages_(true),
+      print_ready_metafile_page_count_(0),
       state_(UNINITIALIZED) {
 }
 
@@ -1341,22 +1361,24 @@ bool PrintWebViewHelper::PrintPreviewContext::CreatePreviewDocument(
     return false;
   }
 
+  int selected_page_count = pages.size();
   current_page_index_ = 0;
-  if (pages.empty()) {
-    actual_page_count_ = total_page_count_;
-    for (int i = 0; i < actual_page_count_; ++i)
+  print_ready_metafile_page_count_ = selected_page_count;
+  pages_to_render_ = pages;
+
+  if (selected_page_count == 0) {
+    print_ready_metafile_page_count_ = total_page_count_;
+    // Render all pages.
+    for (int i = 0; i < total_page_count_; i++)
       pages_to_render_.push_back(i);
-  } else {
-    actual_page_count_ = pages.size();
-    for (int i = 0; i < actual_page_count_; ++i) {
-      int page_number = pages[i];
-      if (page_number < printing::FIRST_PAGE_INDEX ||
-          page_number >= total_page_count_) {
-        LOG(ERROR) << "CreatePreviewDocument got invalid page count "
-                << page_number << " at index " << i;
-        return false;
+  } else if (generate_draft_pages_) {
+    int pages_index = 0;
+    for (int i = 0; i < total_page_count_; i++) {
+      if (pages_index < selected_page_count && i == pages[pages_index]) {
+        pages_index++;
+        continue;
       }
-      pages_to_render_.push_back(page_number);
+      pages_to_render_.push_back(i);
     }
   }
 
@@ -1373,16 +1395,20 @@ void PrintWebViewHelper::PrintPreviewContext::RenderedPreviewPage(
   UMA_HISTOGRAM_TIMES("PrintPreview.RenderPDFPageTime", page_time);
 }
 
-void PrintWebViewHelper::PrintPreviewContext::FinalizePreviewDocument() {
+void PrintWebViewHelper::PrintPreviewContext::AllPagesRendered() {
   DCHECK_EQ(RENDERING, state_);
   state_ = DONE;
+  prep_frame_view_->FinishPrinting();
+}
+
+void PrintWebViewHelper::PrintPreviewContext::FinalizePrintReadyDocument() {
+  if (state_ != DONE && state_ != RENDERING)
+    NOTREACHED();
 
   base::TimeTicks begin_time = base::TimeTicks::Now();
-
-  prep_frame_view_->FinishPrinting();
   metafile_->FinishDocument();
 
-  if (actual_page_count_ <= 0) {
+  if (print_ready_metafile_page_count_ <= 0) {
     NOTREACHED();
     return;
   }
@@ -1394,7 +1420,7 @@ void PrintWebViewHelper::PrintPreviewContext::FinalizePreviewDocument() {
   UMA_HISTOGRAM_MEDIUM_TIMES("PrintPreview.RenderAndGeneratePDFTime",
                              total_time);
   UMA_HISTOGRAM_MEDIUM_TIMES("PrintPreview.RenderAndGeneratePDFTimeAvgPerPage",
-                             total_time / actual_page_count_);
+                             total_time / pages_to_render_.size());
 }
 
 void PrintWebViewHelper::PrintPreviewContext::Finished() {
@@ -1410,7 +1436,7 @@ void PrintWebViewHelper::PrintPreviewContext::Failed() {
 
 int PrintWebViewHelper::PrintPreviewContext::GetNextPageNumber() {
   DCHECK_EQ(RENDERING, state_);
-  if (current_page_index_ >= actual_page_count_)
+  if ((size_t)(current_page_index_) == pages_to_render_.size())
     return -1;
   return pages_to_render_[current_page_index_++];
 }
@@ -1429,6 +1455,16 @@ bool PrintWebViewHelper::PrintPreviewContext::IsModifiable() const {
   return mime != "application/pdf";
 }
 
+bool PrintWebViewHelper::PrintPreviewContext::IsLastPageOfPrintReadyMetafile()
+    const {
+  return current_page_index_ == print_ready_metafile_page_count_;
+}
+
+void PrintWebViewHelper::PrintPreviewContext::set_generate_draft_pages(
+    bool generate_draft_pages) {
+  generate_draft_pages_ = generate_draft_pages;
+}
+
 WebKit::WebFrame* PrintWebViewHelper::PrintPreviewContext::frame() const {
   return frame_;
 }
@@ -1440,6 +1476,10 @@ WebKit::WebNode* PrintWebViewHelper::PrintPreviewContext::node() const {
 int PrintWebViewHelper::PrintPreviewContext::total_page_count() const {
   DCHECK(IsReadyToRender());
   return total_page_count_;
+}
+
+bool PrintWebViewHelper::PrintPreviewContext::generate_draft_pages() {
+  return generate_draft_pages_;
 }
 
 printing::PreviewMetafile*

chrome/renderer/print_web_view_helper.h
@@ -6,7 +6,6 @@
 #define CHROME_RENDERER_PRINT_WEB_VIEW_HELPER_H_
 #pragma once
 
-#include <utility>
 #include <vector>
 
 #include "base/memory/scoped_ptr.h"
@@ -109,6 +108,8 @@ class PrintWebViewHelper : public RenderViewObserver,
                            OnPrintForPrintPreview);
   FRIEND_TEST_ALL_PREFIXES(PrintWebViewHelperPreviewTest,
                            OnPrintForPrintPreviewFail);
+  FRIEND_TEST_ALL_PREFIXES(PrintWebViewHelperPreviewTest,
+                           OnPrintPreviewForSelectedPages);
 
 #if defined(OS_WIN) || defined(OS_MACOSX)
   FRIEND_TEST_ALL_PREFIXES(PrintWebViewHelperTest, PrintLayoutTest);
@@ -139,8 +140,8 @@ class PrintWebViewHelper : public RenderViewObserver,
   // Returns true if print preview should continue, false on failure.
   bool RenderPreviewPage(int page_number);
 
-  // Finalize the print preview document.
-  bool FinalizePreviewDocument();
+  // Finalize the print ready preview document.
+  bool FinalizePrintReadyDocument();
 
   // Print / preview the node under the context menu.
   void OnPrintNodeUnderContextMenu();
@@ -302,6 +303,7 @@ class PrintWebViewHelper : public RenderViewObserver,
 
   scoped_ptr<PrintMsg_PrintPages_Params> print_pages_params_;
   bool is_preview_;
+  bool is_print_ready_metafile_sent_;
 
   // Used for scripted initiated printing blocking.
   base::Time last_cancelled_script_print_;
@@ -339,8 +341,11 @@ class PrintWebViewHelper : public RenderViewObserver,
     // rendering took.
     void RenderedPreviewPage(const base::TimeDelta& page_time);
 
-    // Finalizes the print preview document.
-    void FinalizePreviewDocument();
+    // Updates the print preview context when the required pages are rendered.
+    void AllPagesRendered();
+
+    // Finalizes the print ready preview document.
+    void FinalizePrintReadyDocument();
 
     // Cleanup after print preview finishes.
     void Finished();
@@ -352,11 +357,16 @@ class PrintWebViewHelper : public RenderViewObserver,
     int GetNextPageNumber();
     bool IsReadyToRender() const;
     bool IsModifiable() const;
+    bool IsLastPageOfPrintReadyMetafile() const;
+
+    // Setters
+    void set_generate_draft_pages(bool generate_draft_pages);
 
     // Getters
     WebKit::WebFrame* frame() const;
     WebKit::WebNode* node() const;
     int total_page_count() const;
+    bool generate_draft_pages();
     printing::PreviewMetafile* metafile() const;
     const PrintMsg_Print_Params& print_params() const;
     const gfx::Size& GetPrintCanvasSize() const;
@@ -383,13 +393,17 @@ class PrintWebViewHelper : public RenderViewObserver,
     // Total page count in the renderer.
     int total_page_count_;
 
-    // Number of pages to render.
-    int actual_page_count_;
-
     // The current page to render.
     int current_page_index_;
 
+    // List of page indices that need to be rendered.
     std::vector<int> pages_to_render_;
+
+    // True, when draft pages needs to be generated.
+    bool generate_draft_pages_;
+
+    // Specifies the total number of pages in the print ready metafile.
+    int print_ready_metafile_page_count_;
 
     base::TimeDelta document_render_time_;
     base::TimeTicks begin_time_;

chrome/test/data/webui/print_preview.js
@@ -39,7 +39,7 @@ PrintPreviewWebUITest.prototype = {
     MockPrintPreviewHandler.prototype = {
       getDefaultPrinter: function() {},
       getPrinters: function() {},
-      getPreview: function(settings) {},
+      getPreview: function(settings, draft_page_count, modifiable) {},
       print: function(settings) {},
       getPrinterCapabilities: function(printerName) {},
       showSystemDialog: function() {},
@@ -72,8 +72,8 @@ PrintPreviewWebUITest.prototype = {
         }));
     var savedArgs = new SaveMockArguments();
     mockHandler.stubs().getPreview(savedArgs.match(NOT_NULL)).
-        will(callFunctionWithSavedArgs(savedArgs, function(options) {
-          updatePrintPreview('title', true, 1, JSON.parse(options).requestID);
+        will(callFunctionWithSavedArgs(savedArgs, function(args) {
+          updatePrintPreview(1, JSON.parse(args[0]).requestID);
         }));
 
     mockHandler.stubs().getPrinters().
@@ -143,19 +143,21 @@ PrintPreviewWebUITest.prototype = {
    * attaches an HTMLDivElement to the |mainview| element with attributes and
    * empty methods, which are used by testing and that would be provided by the
    * HTMLEmbedElement when the PDF plugin exists.
-   * @param {string} previewUid Preview unique identifier.
+   * @param {number} srcDataIndex Preview data source index.
    */
-  createPDFPlugin: function(previewUid) {
+  createPDFPlugin: function(srcDataIndex) {
     var pdfViewer = $('pdf-viewer');
     if (pdfViewer)
       return;
 
+    var previewUid = 1;
     pdfViewer = document.createElement('div');
     pdfViewer.setAttribute('id', 'pdf-viewer');
     pdfViewer.setAttribute('type',
                            'application/x-google-chrome-print-preview-pdf');
     pdfViewer.setAttribute(
-        'src', 'chrome://print/' + previewUid + '/print.pdf');
+        'src',
+        'chrome://print/' + previewUid + '/' + srcDataIndex + '/print.pdf');
     pdfViewer.setAttribute('aria-live', 'polite');
     pdfViewer.setAttribute('aria-atomic', 'true');
     function fakeFunction() {}
@@ -341,9 +343,8 @@ TEST_F('PrintPreviewWebUITest', 'TestColorSettings', function() {
 TEST_F('PrintPreviewWebUITest', 'TestPrinterChangeUpdatesPreview', function() {
   var savedArgs = new SaveMockArguments();
   this.mockHandler.expects(once()).getPreview(savedArgs.match(ANYTHING)).
-      will(callFunctionWithSavedArgs(savedArgs, function(options) {
-        updatePrintPreview('title', true, 2,
-                           JSON.parse(options).requestID);
+      will(callFunctionWithSavedArgs(savedArgs, function(args) {
+        updatePrintPreview(2, JSON.parse(args[0]).requestID);
       }));
 
   this.mockGlobals.expects(once()).updateWithPrinterCapabilities(

chrome/browser/printing/print_preview_data_service.cc
@@ -49,6 +49,16 @@ class PrintPreviewDataStore : public base::RefCounted<PrintPreviewDataStore> {
     page_data_map_[index] = const_cast<RefCountedBytes*>(data);
   }
 
+  // Returns the available draft page count.
+  int GetAvailableDraftPageCount() {
+    int page_data_map_size = page_data_map_.size();
+    if (page_data_map_.find(printing::COMPLETE_PREVIEW_DOCUMENT_INDEX) !=
+        page_data_map_.end()) {
+      page_data_map_size--;
+    }
+    return page_data_map_size;
+  }
+
  private:
   friend class base::RefCounted<PrintPreviewDataStore>;
 
@@ -104,4 +114,11 @@ void PrintPreviewDataService::RemoveEntry(
   PreviewDataStoreMap::iterator it = data_store_map_.find(preview_ui_addr_str);
   if (it != data_store_map_.end())
     data_store_map_.erase(it);
+}
+
+int PrintPreviewDataService::GetAvailableDraftPageCount(
+    const std::string& preview_ui_addr_str) {
+  if (data_store_map_.find(preview_ui_addr_str) != data_store_map_.end())
+    return data_store_map_[preview_ui_addr_str]->GetAvailableDraftPageCount();
+  return 0;
 }

chrome/browser/printing/print_preview_data_service.h
@@ -40,6 +40,9 @@ class PrintPreviewDataService {
   // Remove the corresponding PrintPreviewUI entry from the map.
   void RemoveEntry(const std::string& preview_ui_addr_str);
 
+  // Returns the available draft page count.
+  int GetAvailableDraftPageCount(const std::string& preview_ui_addr_str);
+
  private:
   friend struct DefaultSingletonTraits<PrintPreviewDataService>;
 

commit 01047a8997502e7c396c084971e9900f810103c7

Fix the print preview regression bug caused by r98926.
If we are previewing a PDF file, always invoke "PrepareFrameAndPrint"
FinishPrinting() before calling metafile FinishDocument() function.

If we are previewing a normal html page, we can invoke FinishDocument()
as soon as the print ready document is ready and then we can call
FinishPrinting() after rendering all the draft pages.

BUG=94998
TEST=Preview PDFs results in valid preview data.


Review URL: http://codereview.chromium.org/7822014

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99344 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/print_preview/print_preview.js
@@ -904,6 +904,9 @@ function onDidGetPreviewPageCount(pageCount, isModifiable, previewResponseId,
   pageSettings.updateState(pageCount);
   previewModifiable = isModifiable;
   document.title = localStrings.getStringF('printPreviewTitleFormat', jobTitle);
+  if (!previewModifiable && pageSettings.requestPrintPreviewIfNeeded())
+    return;
+
   cr.dispatchSimpleEvent(document, 'updateSummary');
 }
 

chrome/renderer/print_web_view_helper.cc
@@ -702,19 +702,32 @@ bool PrintWebViewHelper::CreatePreviewDocument() {
   if (CheckForCancel())
     return false;
 
-  int page_number;
-  while ((page_number = print_preview_context_.GetNextPageNumber()) >= 0) {
+  while (!print_preview_context_.IsFinalPageRendered()) {
+    int page_number = print_preview_context_.GetNextPageNumber();
+    DCHECK(page_number >= 0);
     if (!RenderPreviewPage(page_number))
       return false;
+
     if (CheckForCancel())
       return false;
+
+    // We must call PrepareFrameAndViewForPrint::FinishPrinting() (by way of
+    // print_preview_context_.AllPagesRendered()) before calling
+    // FinalizePrintReadyDocument() when printing a PDF because the plugin
+    // code does not generate output until we call FinishPrinting().  We do not
+    // generate draft pages for PDFs, so IsFinalPageRendered() and
+    // IsLastPageOfPrintReadyMetafile() will be true in the same iteration of
+    // the loop.
+    if (print_preview_context_.IsFinalPageRendered())
+      print_preview_context_.AllPagesRendered();
+
     if (print_preview_context_.IsLastPageOfPrintReadyMetafile()) {
-      // Finished generating preview. Finalize the document.
+      DCHECK(print_preview_context_.IsModifiable() ||
+             print_preview_context_.IsFinalPageRendered());
       if (!FinalizePrintReadyDocument())
         return false;
     }
   }
-  print_preview_context_.AllPagesRendered();
   print_preview_context_.Finished();
   return true;
 }
@@ -1379,11 +1392,11 @@ bool PrintWebViewHelper::PrintPreviewContext::CreatePreviewDocument(
   if (selected_page_count == 0) {
     print_ready_metafile_page_count_ = total_page_count_;
     // Render all pages.
-    for (int i = 0; i < total_page_count_; i++)
+    for (int i = 0; i < total_page_count_; ++i)
       pages_to_render_.push_back(i);
   } else if (generate_draft_pages_) {
     int pages_index = 0;
-    for (int i = 0; i < total_page_count_; i++) {
+    for (int i = 0; i < total_page_count_; ++i) {
       if (pages_index < selected_page_count && i == pages[pages_index]) {
         pages_index++;
         continue;
@@ -1446,7 +1459,7 @@ void PrintWebViewHelper::PrintPreviewContext::Failed() {
 
 int PrintWebViewHelper::PrintPreviewContext::GetNextPageNumber() {
   DCHECK_EQ(RENDERING, state_);
-  if ((size_t)(current_page_index_) == pages_to_render_.size())
+  if (IsFinalPageRendered())
     return -1;
   return pages_to_render_[current_page_index_++];
 }
@@ -1468,6 +1481,10 @@ bool PrintWebViewHelper::PrintPreviewContext::IsModifiable() const {
 bool PrintWebViewHelper::PrintPreviewContext::IsLastPageOfPrintReadyMetafile()
     const {
   return current_page_index_ == print_ready_metafile_page_count_;
+}
+
+bool  PrintWebViewHelper::PrintPreviewContext::IsFinalPageRendered() const {
+  return static_cast<size_t>(current_page_index_) == pages_to_render_.size();
 }
 
 void PrintWebViewHelper::PrintPreviewContext::set_generate_draft_pages(

chrome/renderer/print_web_view_helper.h
@@ -364,6 +364,7 @@ class PrintWebViewHelper : public RenderViewObserver,
     bool IsReadyToRender() const;
     bool IsModifiable() const;
     bool IsLastPageOfPrintReadyMetafile() const;
+    bool IsFinalPageRendered() const;
 
     // Setters
     void set_generate_draft_pages(bool generate_draft_pages);