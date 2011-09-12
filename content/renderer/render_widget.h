adobe/chromium

commit b2e214824c93c61003d9eed2dc56093de2c128b4

Rework SwapBuffers sync to allow enough SwapBuffers without blocking
This change allows the renderer to not block on SwapBuffers when using the SwapBuffers-callback-based draw scheduler, for better interactivity.

BUG=none
TEST=nytimes with Nat's CL 6902106, run the about:gpu profiler, observe no blocking FlushSync

Review URL: http://codereview.chromium.org/6913036

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84372 0039d316-1c4b-4281-b951-d872f2087c98

gpu/command_buffer/client/gles2_implementation.cc
@@ -684,15 +684,22 @@ void GLES2Implementation::Finish() {
 }
 
 void GLES2Implementation::SwapBuffers() {
-  // Wait if this would add too many swap buffers.
-  if (swap_buffers_tokens_.size() == kMaxSwapBuffers) {
+  // TODO(piman): Strictly speaking we'd want to insert the token after the
+  // swap, but the state update with the updated token might not have happened
+  // by the time the SwapBuffer callback gets called, forcing us to synchronize
+  // with the GPU process more than needed. So instead, make it happen before.
+  // All it means is that we could be slightly looser on the kMaxSwapBuffers
+  // semantics if the client doesn't use the callback mechanism, and by chance
+  // the scheduler yields between the InsertToken and the SwapBuffers.
+  swap_buffers_tokens_.push(helper_->InsertToken());
+  helper_->SwapBuffers();
+  helper_->YieldScheduler();
+  helper_->CommandBufferHelper::Flush();
+  // Wait if we added too many swap buffers.
+  if (swap_buffers_tokens_.size() > kMaxSwapBuffers) {
     helper_->WaitForToken(swap_buffers_tokens_.front());
     swap_buffers_tokens_.pop();
   }
-  helper_->SwapBuffers();
-  swap_buffers_tokens_.push(helper_->InsertToken());
-  helper_->YieldScheduler();
-  Flush();
 }
 
 void GLES2Implementation::CopyTextureToParentTextureCHROMIUM(

content/common/gpu/gpu_command_buffer_stub.cc
@@ -258,6 +258,7 @@ void GpuCommandBufferStub::OnResizeOffscreenFrameBuffer(const gfx::Size& size) {
 
 void GpuCommandBufferStub::OnSwapBuffers() {
   GPU_TRACE_EVENT0("gpu", "GpuCommandBufferStub::OnSwapBuffers");
+  ReportState();
   Send(new GpuCommandBufferMsg_SwapBuffers(route_id_));
 }
 

gpu/command_buffer/service/gpu_scheduler.cc
@@ -163,6 +163,11 @@ void GpuScheduler::ProcessCommands() {
          !parser_->IsEmpty()) {
     error = parser_->ProcessCommand();
 
+    // TODO(piman): various classes duplicate various pieces of state, leading
+    // to needlessly complex update logic. It should be possible to simply share
+    // the state across all of them.
+    command_buffer_->SetGetOffset(static_cast<int32>(parser_->get()));
+
     if (error == error::kWaiting || error == error::kYield) {
       break;
     } else if (error::IsError(error)) {
@@ -178,8 +183,6 @@ void GpuScheduler::ProcessCommands() {
       command_processed_callback_->Run();
     }
   }
-
-  command_buffer_->SetGetOffset(static_cast<int32>(parser_->get()));
 
   if (unscheduled_count_ == 0 &&
       error != error::kWaiting &&

gpu/command_buffer/service/gpu_scheduler_unittest.cc
@@ -93,7 +93,6 @@ TEST_F(GpuSchedulerTest, SchedulerDoesNothingIfRingBufferIsEmpty) {
   state.put_offset = 0;
   EXPECT_CALL(*command_buffer_, GetState())
     .WillRepeatedly(Return(state));
-  EXPECT_CALL(*command_buffer_, SetGetOffset(0));
 
   EXPECT_CALL(*command_buffer_, SetParseError(_))
     .Times(0);
@@ -136,13 +135,14 @@ TEST_F(GpuSchedulerTest, ProcessesTwoCommands) {
   state.put_offset = 3;
   EXPECT_CALL(*command_buffer_, GetState())
     .WillRepeatedly(Return(state));
-  EXPECT_CALL(*command_buffer_, SetGetOffset(3));
 
   EXPECT_CALL(*async_api_, DoCommand(7, 1, &buffer_[0]))
     .WillOnce(Return(error::kNoError));
+  EXPECT_CALL(*command_buffer_, SetGetOffset(2));
 
   EXPECT_CALL(*async_api_, DoCommand(8, 0, &buffer_[2]))
     .WillOnce(Return(error::kNoError));
+  EXPECT_CALL(*command_buffer_, SetGetOffset(3));
 
   scheduler_->PutChanged(true);
 }
@@ -156,8 +156,6 @@ TEST_F(GpuSchedulerTest, SchedulerSetsTheGLContext) {
   state.put_offset = 0;
   EXPECT_CALL(*command_buffer_, GetState())
     .WillRepeatedly(Return(state));
-
-  EXPECT_CALL(*command_buffer_, SetGetOffset(0));
 
   scheduler_->PutChanged(true);
 }
@@ -180,10 +178,10 @@ TEST_F(GpuSchedulerTest, PostsTaskToFinishRemainingCommands) {
 
   EXPECT_CALL(*async_api_, DoCommand(7, 1, &buffer_[0]))
     .WillOnce(Return(error::kNoError));
+  EXPECT_CALL(*command_buffer_, SetGetOffset(2));
 
   EXPECT_CALL(*async_api_, DoCommand(8, 0, &buffer_[2]))
     .WillOnce(Return(error::kNoError));
-
   EXPECT_CALL(*command_buffer_, SetGetOffset(3));
 
   scheduler_->PutChanged(true);
@@ -216,6 +214,7 @@ TEST_F(GpuSchedulerTest, SetsErrorCodeOnCommandBuffer) {
   EXPECT_CALL(*async_api_, DoCommand(7, 0, &buffer_[0]))
     .WillOnce(Return(
         error::kUnknownCommand));
+  EXPECT_CALL(*command_buffer_, SetGetOffset(1));
 
   EXPECT_CALL(*command_buffer_,
       SetParseError(error::kUnknownCommand));

commit 0b06dcd0a27b75830573b954029bdaf6341f3f3e

Ensure that compositor does not block the renderer process by issuing too many SwapBuffers.
This might have regressed with r84372. It caused there to be one additional token queued in the swap buffers token queue than before.

BUG=95649
Review URL: http://codereview.chromium.org/7867053

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100801 0039d316-1c4b-4281-b951-d872f2087c98

content/renderer/render_widget.h
@@ -13,6 +13,7 @@
 #include "base/memory/scoped_ptr.h"
 #include "base/time.h"
 #include "content/renderer/paint_aggregator.h"
+#include "gpu/command_buffer/client/gles2_implementation.h"
 #include "ipc/ipc_channel.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebCompositionUnderline.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebPopupType.h"
@@ -367,7 +368,8 @@ class RenderWidget : public IPC::Channel::Listener,
   // When accelerated rendering is on, is the maximum number of swapbuffers that
   // can be outstanding before we start throttling based on
   // OnSwapBuffersComplete callback.
-  static const int kMaxSwapBuffersPending = 2;
+  static const int kMaxSwapBuffersPending =
+      gpu::gles2::GLES2Implementation::kMaxSwapBuffers;
 
   // Set to true if we should ignore RenderWidget::Show calls.
   bool did_show_;

gpu/command_buffer/client/gles2_implementation.cc
@@ -964,8 +964,9 @@ void GLES2Implementation::SwapBuffers() {
   swap_buffers_tokens_.push(helper_->InsertToken());
   helper_->SwapBuffers();
   helper_->CommandBufferHelper::Flush();
-  // Wait if we added too many swap buffers.
-  if (swap_buffers_tokens_.size() > kMaxSwapBuffers) {
+  // Wait if we added too many swap buffers. Add 1 to kMaxSwapBuffers to
+  // compensate for TODO above.
+  if (swap_buffers_tokens_.size() > kMaxSwapBuffers + 1) {
     helper_->WaitForToken(swap_buffers_tokens_.front());
     swap_buffers_tokens_.pop();
   }