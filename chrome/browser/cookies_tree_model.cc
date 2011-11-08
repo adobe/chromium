adobe/chromium

commit 18725f05dca00118634a0a649089995b7d834445

Creat BrowsingDataCookieHelper and CannedBrowsingDataCookieHelper for logging cookies at UI thread. BUG=XXX TEST=XXX
Review URL: http://codereview.chromium.org/7355025

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95534 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/cookies_tree_model.cc
@@ -8,10 +8,12 @@
 #include <functional>
 #include <vector>
 
+#include "base/bind.h"
 #include "base/callback.h"
 #include "base/memory/linked_ptr.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
+#include "chrome/browser/browsing_data_cookie_helper.h"
 #include "chrome/browser/content_settings/host_content_settings_map.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "content/browser/in_process_webkit/webkit_context.h"
@@ -62,7 +64,7 @@ void CookieTreeCookieNode::DeleteStoredObjects() {
   // vector storing the cookies in-tact and not delete from there (that would
   // invalidate our pointers), and the fact that it contains semi out-of-date
   // data is not problematic as we don't re-build the model based on that.
-  GetModel()->cookie_monster_->DeleteCanonicalCookie(*cookie_);
+  GetModel()->cookie_helper_->DeleteCookie(*cookie_);
 }
 
 CookieTreeNode::DetailedInfo CookieTreeCookieNode::GetDetailedInfo() const {
@@ -551,7 +553,7 @@ void CookieTreeNode::AddChildSortedByTitle(CookieTreeNode* new_child) {
 // CookiesTreeModel, public:
 
 CookiesTreeModel::CookiesTreeModel(
-    net::CookieMonster* cookie_monster,
+    BrowsingDataCookieHelper* cookie_helper,
     BrowsingDataDatabaseHelper* database_helper,
     BrowsingDataLocalStorageHelper* local_storage_helper,
     BrowsingDataLocalStorageHelper* session_storage_helper,
@@ -561,8 +563,8 @@ CookiesTreeModel::CookiesTreeModel(
     bool use_cookie_source)
     : ALLOW_THIS_IN_INITIALIZER_LIST(ui::TreeNodeModel<CookieTreeNode>(
           new CookieTreeRootNode(this))),
-      cookie_monster_(cookie_monster),
       appcache_helper_(appcache_helper),
+      cookie_helper_(cookie_helper),
       database_helper_(database_helper),
       local_storage_helper_(local_storage_helper),
       session_storage_helper_(session_storage_helper),
@@ -570,7 +572,10 @@ CookiesTreeModel::CookiesTreeModel(
       file_system_helper_(file_system_helper),
       batch_update_(0),
       use_cookie_source_(use_cookie_source) {
-  LoadCookies();
+  DCHECK(cookie_helper_);
+  cookie_helper_->StartFetching(
+      base::Bind(&CookiesTreeModel::OnCookiesModelInfoLoaded,
+                 base::Unretained(this)));
   DCHECK(database_helper_);
   database_helper_->StartFetching(NewCallback(
       this, &CookiesTreeModel::OnDatabaseModelInfoLoaded));
@@ -601,6 +606,7 @@ CookiesTreeModel::CookiesTreeModel(
 }
 
 CookiesTreeModel::~CookiesTreeModel() {
+  cookie_helper_->CancelNotification();
   database_helper_->CancelNotification();
   local_storage_helper_->CancelNotification();
   if (session_storage_helper_)
@@ -656,42 +662,6 @@ int CookiesTreeModel::GetIconIndex(ui::TreeModelNode* node) {
   return -1;
 }
 
-void CookiesTreeModel::LoadCookies() {
-  LoadCookiesWithFilter(std::wstring());
-}
-
-void CookiesTreeModel::LoadCookiesWithFilter(const std::wstring& filter) {
-  // mmargh mmargh mmargh! delicious!
-
-  all_cookies_ = cookie_monster_->GetAllCookies();
-  CookieTreeRootNode* root = static_cast<CookieTreeRootNode*>(GetRoot());
-  for (CookieList::iterator it = all_cookies_.begin();
-       it != all_cookies_.end(); ++it) {
-    std::string source_string = it->Source();
-    if (source_string.empty() || !use_cookie_source_) {
-      std::string domain = it->Domain();
-      if (domain.length() > 1 && domain[0] == '.')
-        domain = domain.substr(1);
-
-      // We treat secure cookies just the same as normal ones.
-      source_string = std::string(chrome::kHttpScheme) +
-          chrome::kStandardSchemeSeparator + domain + "/";
-    }
-
-    GURL source(source_string);
-    if (!filter.size() ||
-        (CookieTreeOriginNode::TitleForUrl(source).find(filter) !=
-         std::string::npos)) {
-      CookieTreeOriginNode* origin_node =
-          root->GetOrCreateOriginNode(source);
-      CookieTreeCookiesNode* cookies_node =
-          origin_node->GetOrCreateCookiesNode();
-      CookieTreeCookieNode* new_cookie = new CookieTreeCookieNode(&*it);
-      cookies_node->AddCookieNode(new_cookie);
-    }
-  }
-}
-
 void CookiesTreeModel::DeleteAllStoredObjects() {
   NotifyObserverBeginBatch();
   CookieTreeNode* root = GetRoot();
@@ -719,7 +689,7 @@ void CookiesTreeModel::UpdateSearchResults(const std::wstring& filter) {
   NotifyObserverBeginBatch();
   for (int i = num_children - 1; i >= 0; --i)
     delete Remove(root, root->GetChild(i));
-  LoadCookiesWithFilter(filter);
+  PopulateCookieInfoWithFilter(filter);
   PopulateDatabaseInfoWithFilter(filter);
   PopulateLocalStorageInfoWithFilter(filter);
   PopulateSessionStorageInfoWithFilter(filter);
@@ -778,6 +748,44 @@ void CookiesTreeModel::PopulateAppCacheInfoWithFilter(
   }
   NotifyObserverTreeNodeChanged(root);
   NotifyObserverEndBatch();
+}
+
+void CookiesTreeModel::OnCookiesModelInfoLoaded(
+    const CookieList& cookie_list) {
+  cookie_list_ = cookie_list;
+  PopulateCookieInfoWithFilter(std::wstring());
+}
+
+void CookiesTreeModel::PopulateCookieInfoWithFilter(
+    const std::wstring& filter) {
+  // mmargh mmargh mmargh! delicious!
+
+  CookieTreeRootNode* root = static_cast<CookieTreeRootNode*>(GetRoot());
+  for (CookieList::iterator it = cookie_list_.begin();
+       it != cookie_list_.end(); ++it) {
+    std::string source_string = it->Source();
+    if (source_string.empty() || !use_cookie_source_) {
+      std::string domain = it->Domain();
+      if (domain.length() > 1 && domain[0] == '.')
+        domain = domain.substr(1);
+
+      // We treat secure cookies just the same as normal ones.
+      source_string = std::string(chrome::kHttpScheme) +
+          chrome::kStandardSchemeSeparator + domain + "/";
+    }
+
+    GURL source(source_string);
+    if (!filter.size() ||
+        (CookieTreeOriginNode::TitleForUrl(source).find(filter) !=
+         std::string::npos)) {
+      CookieTreeOriginNode* origin_node =
+          root->GetOrCreateOriginNode(source);
+      CookieTreeCookiesNode* cookies_node =
+          origin_node->GetOrCreateCookiesNode();
+      CookieTreeCookieNode* new_cookie = new CookieTreeCookieNode(&*it);
+      cookies_node->AddCookieNode(new_cookie);
+    }
+  }
 }
 
 void CookiesTreeModel::OnDatabaseModelInfoLoaded(

chrome/browser/browsing_data_cookie_helper.cc
@@ -1,0 +1,178 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/browsing_data_cookie_helper.h"
+
+#include "base/bind.h"
+#include "base/logging.h"
+#include "base/memory/scoped_ptr.h"
+#include "chrome/browser/profiles/profile.h"
+#include "content/browser/browser_thread.h"
+#include "googleurl/src/gurl.h"
+#include "net/url_request/url_request_context.h"
+#include "net/url_request/url_request_context_getter.h"
+
+BrowsingDataCookieHelper::BrowsingDataCookieHelper(Profile* profile)
+  : is_fetching_(false),
+    profile_(profile),
+    request_context_getter_(profile->GetRequestContext()) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+}
+
+BrowsingDataCookieHelper::~BrowsingDataCookieHelper() {
+}
+
+void BrowsingDataCookieHelper::StartFetching(
+    const base::Callback<void(const net::CookieList& cookies)>& callback) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  DCHECK(!is_fetching_);
+  DCHECK(!callback.is_null());
+  DCHECK(completion_callback_.is_null());
+  is_fetching_ = true;
+  completion_callback_ = callback;
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      base::Bind(&BrowsingDataCookieHelper::FetchCookiesOnIOThread, this));
+}
+
+void BrowsingDataCookieHelper::CancelNotification() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  completion_callback_.Reset();
+}
+
+void BrowsingDataCookieHelper::DeleteCookie(
+    const net::CookieMonster::CanonicalCookie& cookie) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      base::Bind(&BrowsingDataCookieHelper::DeleteCookieOnIOThread,
+                 this, cookie));
+}
+
+void BrowsingDataCookieHelper::FetchCookiesOnIOThread() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  scoped_refptr<net::CookieMonster> cookie_monster =
+      request_context_getter_->GetURLRequestContext()->
+      cookie_store()->GetCookieMonster();
+  if (cookie_monster) {
+    cookie_monster->GetAllCookiesAsync(
+        base::Bind(&BrowsingDataCookieHelper::OnFetchComplete, this));
+  } else {
+    OnFetchComplete(net::CookieList());
+  }
+}
+
+void BrowsingDataCookieHelper::OnFetchComplete(const net::CookieList& cookies) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  BrowserThread::PostTask(
+      BrowserThread::UI, FROM_HERE,
+      base::Bind(&BrowsingDataCookieHelper::NotifyInUIThread, this, cookies));
+}
+
+void BrowsingDataCookieHelper::NotifyInUIThread(
+    const net::CookieList& cookies) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  DCHECK(is_fetching_);
+  is_fetching_ = false;
+  if (!completion_callback_.is_null()) {
+    completion_callback_.Run(cookies);
+    completion_callback_.Reset();
+  }
+}
+
+void BrowsingDataCookieHelper::DeleteCookieOnIOThread(
+    const net::CookieMonster::CanonicalCookie& cookie) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  scoped_refptr<net::CookieMonster> cookie_monster =
+      request_context_getter_->GetURLRequestContext()->
+      cookie_store()->GetCookieMonster();
+  if (cookie_monster) {
+    cookie_monster->DeleteCanonicalCookieAsync(
+        cookie, net::CookieMonster::DeleteCookieCallback());
+  }
+}
+
+CannedBrowsingDataCookieHelper::CannedBrowsingDataCookieHelper(
+    Profile* profile)
+    : BrowsingDataCookieHelper(profile),
+    profile_(profile) {
+}
+
+CannedBrowsingDataCookieHelper::~CannedBrowsingDataCookieHelper() {}
+
+CannedBrowsingDataCookieHelper* CannedBrowsingDataCookieHelper::Clone() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  CannedBrowsingDataCookieHelper* clone =
+      new CannedBrowsingDataCookieHelper(profile_);
+
+  clone->cookie_list_ = cookie_list_;
+  return clone;
+}
+
+void CannedBrowsingDataCookieHelper::AddReadCookies(
+    const GURL& url,
+    const net::CookieList& cookie_list) {
+  typedef net::CookieList::const_iterator cookie_iterator;
+  for (cookie_iterator add_cookie = cookie_list.begin();
+       add_cookie != cookie_list.end(); ++add_cookie) {
+    DeleteMetchingCookie(*add_cookie);
+    cookie_list_.push_back(*add_cookie);
+  }
+}
+
+void CannedBrowsingDataCookieHelper::AddChangedCookie(
+    const GURL& url,
+    const std::string& cookie_line,
+    const net::CookieOptions& options) {
+  typedef net::CookieList::iterator cookie_iterator;
+
+  net::CookieMonster::ParsedCookie pc(cookie_line);
+  if (options.exclude_httponly() && pc.IsHttpOnly()) {
+    // Return if a Javascript cookie illegally specified the HTTP only flag.
+    return;
+  }
+
+  scoped_ptr<net::CookieMonster::CanonicalCookie> cc;
+  cc.reset(net::CookieMonster::CanonicalCookie::Create(url, pc));
+  // Fails creating canonical cookie, if the normalized cookie domain form
+  // cookie line and the url don't have the same domain+registry, or url host
+  // isn't cookie domain or one of its subdomains.
+
+  if (cc.get()) {
+    DeleteMetchingCookie(*cc);
+    cookie_list_.push_back(*cc);
+  }
+}
+
+void CannedBrowsingDataCookieHelper::Reset() {
+  cookie_list_.clear();
+}
+
+bool CannedBrowsingDataCookieHelper::empty() const {
+  return cookie_list_.empty();
+}
+
+void CannedBrowsingDataCookieHelper::StartFetching(
+    const net::CookieMonster::GetCookieListCallback& callback) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  if (!callback.is_null())
+    callback.Run(cookie_list_);
+}
+
+void CannedBrowsingDataCookieHelper::CancelNotification() {}
+
+bool CannedBrowsingDataCookieHelper::DeleteMetchingCookie(
+    const net::CookieMonster::CanonicalCookie& add_cookie) {
+  typedef net::CookieList::iterator cookie_iterator;
+  for (cookie_iterator cookie = cookie_list_.begin();
+      cookie != cookie_list_.end(); ++cookie) {
+    if (cookie->Name() == add_cookie.Name() &&
+        cookie->Domain() == add_cookie.Domain()&&
+        cookie->Path() == add_cookie.Path()) {
+      cookie_list_.erase(cookie);
+      return true;
+    }
+  }
+  return false;
+}

chrome/browser/browsing_data_cookie_helper.h
@@ -1,0 +1,134 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_BROWSING_DATA_COOKIE_HELPER_H_
+#define CHROME_BROWSER_BROWSING_DATA_COOKIE_HELPER_H_
+#pragma once
+
+#include <string>
+
+#include "base/basictypes.h"
+#include "base/callback.h"
+#include "base/memory/ref_counted.h"
+#include "net/base/cookie_monster.h"
+
+class GURL;
+class Profile;
+
+namespace net {
+class URLRequestContextGetter;
+}
+
+// This class fetches cookie information on behalf of a caller
+// on the UI thread.
+// A client of this class need to call StartFetching from the UI thread to
+// initiate the flow, and it'll be notified by the callback in its UI
+// thread at some later point.
+// The client must call CancelNotification() if it's destroyed before the
+// callback is notified.
+class BrowsingDataCookieHelper
+    : public base::RefCountedThreadSafe<BrowsingDataCookieHelper> {
+ public:
+  explicit BrowsingDataCookieHelper(Profile* profile);
+
+  // Starts the fetching process, which will notify its completion via
+  // callback.
+  // This must be called only in the UI thread.
+  virtual void StartFetching(
+      const base::Callback<void(const net::CookieList& cookies)>& callback);
+
+  // Cancels the notification callback (i.e., the window that created it no
+  // longer exists).
+  // This must be called only in the UI thread.
+  virtual void CancelNotification();
+
+  // Requests a single cookie to be deleted in the IO thread. This must be
+  // called in the UI thread.
+  virtual void DeleteCookie(const net::CookieMonster::CanonicalCookie& cookie);
+
+ protected:
+  friend class base::RefCountedThreadSafe<BrowsingDataCookieHelper>;
+  virtual ~BrowsingDataCookieHelper();
+
+ private:
+  // Fetch the cookies. This must be called in the IO thread.
+  void FetchCookiesOnIOThread();
+
+  // Callback function for get cookie. This must be called in the IO thread.
+  void OnFetchComplete(const net::CookieList& cookies);
+
+  // Notifies the completion callback. This must be called in the UI thread.
+  void NotifyInUIThread(const net::CookieList& cookies);
+
+  // Delete a single cookie. This must be called in IO thread.
+  void DeleteCookieOnIOThread(
+      const net::CookieMonster::CanonicalCookie& cookie);
+
+  // Indicates whether or not we're currently fetching information:
+  // it's true when StartFetching() is called in the UI thread, and it's reset
+  // after we notify the callback in the UI thread.
+  // This only mutates on the UI thread.
+  bool is_fetching_;
+
+  Profile* profile_;
+
+  scoped_refptr<net::URLRequestContextGetter> request_context_getter_;
+
+  // This only mutates on the UI thread.
+  base::Callback<void(const net::CookieList& cookies)> completion_callback_;
+
+  DISALLOW_COPY_AND_ASSIGN(BrowsingDataCookieHelper);
+};
+
+// This class is a thin wrapper around BrowsingDataCookieHelper that does not
+// fetch its information from the persistent cookie store, but gets them passed
+// as a parameter during construction.
+class CannedBrowsingDataCookieHelper : public BrowsingDataCookieHelper {
+ public:
+  explicit CannedBrowsingDataCookieHelper(Profile* profile);
+
+  // Return a copy of the cookie helper. Only one consumer can use the
+  // StartFetching method at a time, so we need to create a copy of the helper
+  // everytime we instantiate a cookies tree model for it.
+  CannedBrowsingDataCookieHelper* Clone();
+
+  // Adds cookies and delete the current cookies with the same Name, Domain,
+  // and Path as the newly created ones.
+  void AddReadCookies(const GURL& url,
+                      const net::CookieList& cookie_list);
+
+  // Adds cookies that will be stored by the CookieMonster. Designed to mirror
+  // the logic of SetCookieWithOptions.
+  void AddChangedCookie(const GURL& url,
+                        const std::string& cookie_line,
+                        const net::CookieOptions& options);
+
+  // Clears the list of canned cookies.
+  void Reset();
+
+  // True if no cookie are currently stored.
+  bool empty() const;
+
+  // BrowsingDataCookieHelper methods.
+  virtual void StartFetching(
+      const net::CookieMonster::GetCookieListCallback& callback);
+  virtual void CancelNotification();
+
+ private:
+  // Check if the cookie list contains a cookie with the same name,
+  // domain, and path as the newly created cookie. Delete the old cookie
+  // if does.
+  bool DeleteMetchingCookie(
+      const net::CookieMonster::CanonicalCookie& add_cookie);
+
+  virtual ~CannedBrowsingDataCookieHelper();
+
+  net::CookieList cookie_list_;
+
+  Profile* profile_;
+
+  DISALLOW_COPY_AND_ASSIGN(CannedBrowsingDataCookieHelper);
+};
+
+#endif  // CHROME_BROWSER_BROWSING_DATA_COOKIE_HELPER_H_

chrome/browser/browsing_data_cookie_helper_unittest.cc
@@ -1,0 +1,181 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/browsing_data_cookie_helper.h"
+
+
+#include "base/bind.h"
+#include "base/message_loop.h"
+#include "chrome/test/base/testing_profile.h"
+#include "content/browser/browser_thread.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+class BrowsingDataCookieHelperTest : public testing::Test {
+ public:
+  virtual void SetUp() {
+    ui_thread_.reset(new BrowserThread(BrowserThread::UI, &message_loop_));
+    // Note: we're starting a real IO thread because parts of the
+    // BrowsingDataCookieHelper expect to run on that thread.
+    io_thread_.reset(new BrowserThread(BrowserThread::IO));
+    ASSERT_TRUE(io_thread_->Start());
+  }
+
+  virtual void TearDown() {
+    message_loop_.RunAllPending();
+    io_thread_.reset();
+    ui_thread_.reset();
+  }
+
+  void CreateCookiesForTest() {
+    testing_profile_.CreateRequestContext();
+    scoped_refptr<net::CookieMonster> cookie_monster =
+        testing_profile_.GetCookieMonster();
+    cookie_monster->SetCookieWithOptionsAsync(
+        GURL("http://www.google.com"), "A=1", net::CookieOptions(),
+        net::CookieMonster::SetCookiesCallback());
+    cookie_monster->SetCookieWithOptionsAsync(
+        GURL("http://www.gmail.google.com"), "B=1", net::CookieOptions(),
+        net::CookieMonster::SetCookiesCallback());
+  }
+
+  void FetchCallback(const net::CookieList& cookies) {
+    DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+    ASSERT_EQ(2UL, cookies.size());
+    cookie_list_ = cookies;
+    net::CookieList::const_iterator it = cookies.begin();
+
+    // Correct because fetching cookies will get a sorted cookie list.
+    ASSERT_TRUE(it != cookies.end());
+    EXPECT_EQ("www.google.com", it->Domain());
+    EXPECT_EQ("A", it->Name());
+
+    ASSERT_TRUE(++it != cookies.end());
+    EXPECT_EQ("www.gmail.google.com", it->Domain());
+    EXPECT_EQ("B", it->Name());
+
+    ASSERT_TRUE(++it == cookies.end());
+    MessageLoop::current()->Quit();
+  }
+
+  void DeleteCallback(const net::CookieList& cookies) {
+    DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+    ASSERT_EQ(1UL, cookies.size());
+    net::CookieList::const_iterator it = cookies.begin();
+
+    ASSERT_TRUE(it != cookies.end());
+    EXPECT_EQ("www.gmail.google.com", it->Domain());
+    EXPECT_EQ("B", it->Name());
+
+    ASSERT_TRUE(++it == cookies.end());
+    MessageLoop::current()->Quit();
+  }
+
+  void CannedUniqueCallback(const net::CookieList& cookies) {
+    DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+    ASSERT_EQ(1UL, cookies.size());
+    cookie_list_ = cookies;
+    net::CookieList::const_iterator it = cookies.begin();
+
+    ASSERT_TRUE(it != cookies.end());
+    EXPECT_EQ("http://www.google.com/", it->Source());
+    EXPECT_EQ("A", it->Name());
+
+    ASSERT_TRUE(++it == cookies.end());
+  }
+
+ protected:
+  MessageLoop message_loop_;
+  scoped_ptr<BrowserThread> ui_thread_;
+  scoped_ptr<BrowserThread> io_thread_;
+  TestingProfile testing_profile_;
+
+  net::CookieList cookie_list_;
+};
+
+TEST_F(BrowsingDataCookieHelperTest, FetchData) {
+  CreateCookiesForTest();
+  scoped_refptr<BrowsingDataCookieHelper> cookie_helper(
+      new BrowsingDataCookieHelper(&testing_profile_));
+
+  cookie_helper->StartFetching(
+      base::Bind(&BrowsingDataCookieHelperTest::FetchCallback,
+                 base::Unretained(this)));
+
+  // Blocks until BrowsingDataCookieHelperTest::FetchCallback is notified.
+  MessageLoop::current()->Run();
+}
+
+TEST_F(BrowsingDataCookieHelperTest, DeleteCookie) {
+  CreateCookiesForTest();
+  scoped_refptr<BrowsingDataCookieHelper> cookie_helper(
+      new BrowsingDataCookieHelper(&testing_profile_));
+
+  cookie_helper->StartFetching(
+      base::Bind(&BrowsingDataCookieHelperTest::FetchCallback,
+                 base::Unretained(this)));
+
+  // Blocks until BrowsingDataCookieHelperTest::FetchCallback is notified.
+  MessageLoop::current()->Run();
+
+  net::CookieMonster::CanonicalCookie cookie = cookie_list_[0];
+  cookie_helper->DeleteCookie(cookie);
+
+  cookie_helper->StartFetching(
+      base::Bind(&BrowsingDataCookieHelperTest::DeleteCallback,
+                 base::Unretained(this)));
+  MessageLoop::current()->Run();
+}
+
+TEST_F(BrowsingDataCookieHelperTest, CannedUnique) {
+  const GURL origin("http://www.google.com");
+  net::CookieList cookie;
+
+  scoped_refptr<CannedBrowsingDataCookieHelper> helper(
+      new CannedBrowsingDataCookieHelper(&testing_profile_));
+
+  ASSERT_TRUE(helper->empty());
+  helper->AddChangedCookie(origin, "A=1", net::CookieOptions());
+  helper->AddChangedCookie(origin, "A=1", net::CookieOptions());
+  helper->StartFetching(
+      base::Bind(&BrowsingDataCookieHelperTest::CannedUniqueCallback,
+                 base::Unretained(this)));
+  cookie = cookie_list_;
+  helper->Reset();
+  ASSERT_TRUE(helper->empty());
+
+  helper->AddReadCookies(origin, cookie);
+  helper->AddReadCookies(origin, cookie);
+  helper->StartFetching(
+      base::Bind(&BrowsingDataCookieHelperTest::CannedUniqueCallback,
+                 base::Unretained(this)));
+}
+
+TEST_F(BrowsingDataCookieHelperTest, CannedEmpty) {
+  const GURL url_google("http://www.google.com");
+
+  scoped_refptr<CannedBrowsingDataCookieHelper> helper(
+      new CannedBrowsingDataCookieHelper(&testing_profile_));
+
+  ASSERT_TRUE(helper->empty());
+  helper->AddChangedCookie(url_google, "a=1",
+                          net::CookieOptions());
+  ASSERT_FALSE(helper->empty());
+  helper->Reset();
+  ASSERT_TRUE(helper->empty());
+
+  net::CookieList cookies;
+  net::CookieMonster::ParsedCookie pc("a=1");
+  scoped_ptr<net::CookieMonster::CanonicalCookie> cookie(
+      new net::CookieMonster::CanonicalCookie(url_google, pc));
+  cookies.push_back(*cookie);
+
+  helper->AddReadCookies(url_google, cookies);
+  ASSERT_FALSE(helper->empty());
+  helper->Reset();
+  ASSERT_TRUE(helper->empty());
+}
+
+}  // namespace

commit 00d08850b70acc9fe792340a9f66562b0ee8fc33

Properly batch cookie tree model updates on initial fetch.
This avoids sending each cookie one by one to the cookie manager UI, which
makes it really janky. This behavior appears to have regressed in r95534.

This fix should be merged to M16 and possibly also M15.
BUG=100391
Review URL: http://codereview.chromium.org/8483002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109124 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/cookies_tree_model.cc
@@ -851,6 +851,7 @@ void CookiesTreeModel::PopulateCookieInfoWithFilter(
   // mmargh mmargh mmargh! delicious!
 
   CookieTreeRootNode* root = static_cast<CookieTreeRootNode*>(GetRoot());
+  NotifyObserverBeginBatch();
   for (CookieList::iterator it = cookie_list_.begin();
        it != cookie_list_.end(); ++it) {
     std::string source_string = it->Source();
@@ -876,6 +877,8 @@ void CookiesTreeModel::PopulateCookieInfoWithFilter(
       cookies_node->AddCookieNode(new_cookie);
     }
   }
+  NotifyObserverTreeNodeChanged(root);
+  NotifyObserverEndBatch();
 }
 
 void CookiesTreeModel::OnDatabaseModelInfoLoaded(