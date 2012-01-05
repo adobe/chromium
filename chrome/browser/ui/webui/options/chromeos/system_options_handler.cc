adobe/chromium

commit 5b2594d0d9f7ac7018cde8e2903debffebac740a

Add stubs to options menu for additional accessibility features
R=dmazzoni@chromium.org
BUG=None
TEST=None


Review URL: http://codereview.chromium.org/8932002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116447 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/webui/options/chromeos/system_options_handler.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -114,9 +114,18 @@ void SystemOptionsHandler::GetLocalizedValues(
   localized_strings->SetString("accessibilityTitle",
       l10n_util::GetStringUTF16(
           IDS_OPTIONS_SETTINGS_SECTION_TITLE_ACCESSIBILITY));
-  localized_strings->SetString("accessibility",
+  localized_strings->SetString("accessibilitySpokenFeedback",
       l10n_util::GetStringUTF16(
           IDS_OPTIONS_SETTINGS_ACCESSIBILITY_DESCRIPTION));
+  localized_strings->SetString("accessibilityHighContrast",
+      l10n_util::GetStringUTF16(
+          IDS_OPTIONS_SETTINGS_ACCESSIBILITY_HIGH_CONTRAST_DESCRIPTION));
+  localized_strings->SetString("accessibilityScreenMagnifier",
+      l10n_util::GetStringUTF16(
+          IDS_OPTIONS_SETTINGS_ACCESSIBILITY_SCREEN_MAGNIFIER_DESCRIPTION));
+  localized_strings->SetString("accessibilityVirtualKeyboard",
+      l10n_util::GetStringUTF16(
+          IDS_OPTIONS_SETTINGS_ACCESSIBILITY_VIRTUAL_KEYBOARD_DESCRIPTION));
 
   // TODO(pastarmovj): replace this with a call to the CrosSettings list
   // handling functionality to come.
@@ -128,10 +137,22 @@ void SystemOptionsHandler::GetLocalizedValues(
 
 void SystemOptionsHandler::Initialize() {
   PrefService* pref_service = g_browser_process->local_state();
-  bool acc_enabled = pref_service->GetBoolean(prefs::kSpokenFeedbackEnabled);
-  base::FundamentalValue checked(acc_enabled);
+  bool enabled = pref_service->GetBoolean(prefs::kSpokenFeedbackEnabled);
   web_ui()->CallJavascriptFunction(
-      "options.SystemOptions.SetAccessibilityCheckboxState", checked);
+      "options.SystemOptions.setSpokenFeedbackCheckboxState",
+      base::FundamentalValue(enabled));
+  enabled = pref_service->GetBoolean(prefs::kHighContrastEnabled);
+  web_ui()->CallJavascriptFunction(
+      "options.SystemOptions.setHighContrastCheckboxState",
+      base::FundamentalValue(enabled));
+  enabled = pref_service->GetBoolean(prefs::kScreenMagnifierEnabled);
+  web_ui()->CallJavascriptFunction(
+      "options.SystemOptions.setScreenMagnifierCheckboxState",
+      base::FundamentalValue(enabled));
+  enabled = pref_service->GetBoolean(prefs::kVirtualKeyboardEnabled);
+  web_ui()->CallJavascriptFunction(
+      "options.SystemOptions.setVirtualKeyboardCheckboxState",
+      base::FundamentalValue(enabled));
 
   chromeos::XInputHierarchyChangedEventListener::GetInstance()
       ->AddObserver(this);
@@ -167,14 +188,29 @@ void SystemOptionsHandler::MouseExists(bool* exists) {
 }
 
 void SystemOptionsHandler::RegisterMessages() {
-  web_ui()->RegisterMessageCallback("accessibilityChange",
-      base::Bind(&SystemOptionsHandler::AccessibilityChangeCallback,
+  web_ui()->RegisterMessageCallback(
+      "spokenFeedbackChange",
+      base::Bind(&SystemOptionsHandler::SpokenFeedbackChangeCallback,
+                 base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "highContrastChange",
+      base::Bind(&SystemOptionsHandler::HighContrastChangeCallback,
+                 base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "screenMagnifierChange",
+      base::Bind(&SystemOptionsHandler::ScreenMagnifierChangeCallback,
+                 base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "virtualKeyboardChange",
+      base::Bind(&SystemOptionsHandler::VirtualKeyboardChangeCallback,
                  base::Unretained(this)));
 
-  web_ui()->RegisterMessageCallback("decreaseScreenBrightness",
+  web_ui()->RegisterMessageCallback(
+      "decreaseScreenBrightness",
       base::Bind(&SystemOptionsHandler::DecreaseScreenBrightnessCallback,
                  base::Unretained(this)));
-  web_ui()->RegisterMessageCallback("increaseScreenBrightness",
+  web_ui()->RegisterMessageCallback(
+      "increaseScreenBrightness",
       base::Bind(&SystemOptionsHandler::IncreaseScreenBrightnessCallback,
                  base::Unretained(this)));
 }
@@ -184,12 +220,34 @@ void SystemOptionsHandler::DeviceHierarchyChanged() {
   CheckTouchpadExists();
 }
 
-void SystemOptionsHandler::AccessibilityChangeCallback(const ListValue* args) {
-  std::string checked_str;
-  args->GetString(0, &checked_str);
-  bool accessibility_enabled = (checked_str == "true");
+void SystemOptionsHandler::SpokenFeedbackChangeCallback(const ListValue* args) {
+  bool enabled = false;
+  args->GetBoolean(0, &enabled);
 
-  chromeos::accessibility::EnableAccessibility(accessibility_enabled, NULL);
+  chromeos::accessibility::EnableAccessibility(enabled, NULL);
+}
+
+void SystemOptionsHandler::HighContrastChangeCallback(const ListValue* args) {
+  bool enabled = false;
+  args->GetBoolean(0, &enabled);
+
+  chromeos::accessibility::EnableHighContrast(enabled);
+}
+
+void SystemOptionsHandler::ScreenMagnifierChangeCallback(
+    const ListValue* args) {
+  bool enabled = false;
+  args->GetBoolean(0, &enabled);
+
+  chromeos::accessibility::EnableScreenMagnifier(enabled);
+}
+
+void SystemOptionsHandler::VirtualKeyboardChangeCallback(
+    const ListValue* args) {
+  bool enabled = false;
+  args->GetBoolean(0, &enabled);
+
+  chromeos::accessibility::EnableVirtualKeyboard(enabled);
 }
 
 void SystemOptionsHandler::DecreaseScreenBrightnessCallback(

chrome/app/generated_resources.grd
@@ -12172,8 +12172,17 @@ changed since last sign in
       <message name="IDS_OPTIONS_SETTINGS_BLUETOOTH_REMOTE_PASSKEY_REQUEST" desc="Bluetooth pairing message typically displayed when pairing a wireless keyboard.  The label %1 is replaced with the name of the device.">
         Please enter this passkey on "<ph name="DEVICE_NAME">%1<ex>Nexus S</ex></ph>":
       </message>
-      <message name="IDS_OPTIONS_SETTINGS_ACCESSIBILITY_DESCRIPTION" desc="In the settings tab, the text next to the checkbox for accessbility.">
-        Enable accessibility features
+      <message name="IDS_OPTIONS_SETTINGS_ACCESSIBILITY_DESCRIPTION" desc="In the settings tab, the text next to the checkbox for spoken feedback.">
+        Enable spoken feedback
+      </message>
+      <message name="IDS_OPTIONS_SETTINGS_ACCESSIBILITY_HIGH_CONTRAST_DESCRIPTION" desc="In the settings tab, the text next to the checkbox for high contrast mode.">
+        Enable high contrast mode
+      </message>
+      <message name="IDS_OPTIONS_SETTINGS_ACCESSIBILITY_SCREEN_MAGNIFIER_DESCRIPTION" desc="In the settings tab, the text next to the checkbox for screen magnifier.">
+        Enable screen magnifier
+      </message>
+      <message name="IDS_OPTIONS_SETTINGS_ACCESSIBILITY_VIRTUAL_KEYBOARD_DESCRIPTION" desc="In the settings tab, the text next to the checkbox for virtual keyboard.">
+        Enable virtual keyboard
       </message>
       <message name="IDS_OPTIONS_SETTINGS_KEYBOARD_LAYOUT_TEXT" desc="In the settings tab, the text of the keyboard layout label.">
         Keyboard Layout:

chrome/browser/chromeos/accessibility/accessibility_util.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -151,6 +151,24 @@ void EnableAccessibility(bool enabled, WebUI* login_web_ui) {
     extension_service->component_loader()->Remove(path);
     LOG(INFO) << "ChromeVox was Unloaded.";
   }
+}
+
+void EnableHighContrast(bool enabled) {
+  PrefService* pref_service = g_browser_process->local_state();
+  pref_service->SetBoolean(prefs::kHighContrastEnabled, enabled);
+  pref_service->ScheduleSavePersistentPrefs();
+}
+
+void EnableScreenMagnifier(bool enabled) {
+  PrefService* pref_service = g_browser_process->local_state();
+  pref_service->SetBoolean(prefs::kScreenMagnifierEnabled, enabled);
+  pref_service->ScheduleSavePersistentPrefs();
+}
+
+void EnableVirtualKeyboard(bool enabled) {
+  PrefService* pref_service = g_browser_process->local_state();
+  pref_service->SetBoolean(prefs::kVirtualKeyboardEnabled, enabled);
+  pref_service->ScheduleSavePersistentPrefs();
 }
 
 void ToggleAccessibility(WebUI* login_web_ui) {

chrome/browser/chromeos/accessibility/accessibility_util.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,6 +16,15 @@ namespace accessibility {
 // login screen, pass the WebUI object in login_web_ui so that ChromeVox
 // can be injected directly into that screen, otherwise it should be NULL.
 void EnableAccessibility(bool enabled, WebUI* login_web_ui);
+
+// Enable or disable the high contrast mode for Chrome.
+void EnableHighContrast(bool enabled);
+
+// Enable or disable the screen magnifier.
+void EnableScreenMagnifier(bool enabled);
+
+// Enable or disable the virtual keyboard.
+void EnableVirtualKeyboard(bool enabled);
 
 // Toggles whether Chrome OS accessibility is on or off. See docs for
 // EnableAccessibility, above.

chrome/browser/chromeos/login/wizard_controller.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -354,6 +354,21 @@ void WizardController::RegisterPrefs(PrefService* local_state) {
   // Preferences::RegisterUserPrefs runs before this code in the future.
   if (local_state->FindPreference(prefs::kSpokenFeedbackEnabled) == NULL) {
     local_state->RegisterBooleanPref(prefs::kSpokenFeedbackEnabled,
+                                     false,
+                                     PrefService::UNSYNCABLE_PREF);
+  }
+  if (local_state->FindPreference(prefs::kHighContrastEnabled) == NULL) {
+    local_state->RegisterBooleanPref(prefs::kHighContrastEnabled,
+                                     false,
+                                     PrefService::UNSYNCABLE_PREF);
+  }
+  if (local_state->FindPreference(prefs::kScreenMagnifierEnabled) == NULL) {
+    local_state->RegisterBooleanPref(prefs::kScreenMagnifierEnabled,
+                                     false,
+                                     PrefService::UNSYNCABLE_PREF);
+  }
+  if (local_state->FindPreference(prefs::kVirtualKeyboardEnabled) == NULL) {
+    local_state->RegisterBooleanPref(prefs::kVirtualKeyboardEnabled,
                                      false,
                                      PrefService::UNSYNCABLE_PREF);
   }

chrome/browser/chromeos/preferences.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -58,6 +58,21 @@ void Preferences::RegisterUserPrefs(PrefService* prefs) {
   // the pref here in case of Chrome/Linux with ChromeOS=1.
   if (prefs->FindPreference(prefs::kSpokenFeedbackEnabled) == NULL) {
     prefs->RegisterBooleanPref(prefs::kSpokenFeedbackEnabled,
+                               false,
+                               PrefService::UNSYNCABLE_PREF);
+  }
+  if (prefs->FindPreference(prefs::kHighContrastEnabled) == NULL) {
+    prefs->RegisterBooleanPref(prefs::kHighContrastEnabled,
+                               false,
+                               PrefService::UNSYNCABLE_PREF);
+  }
+  if (prefs->FindPreference(prefs::kScreenMagnifierEnabled) == NULL) {
+    prefs->RegisterBooleanPref(prefs::kScreenMagnifierEnabled,
+                               false,
+                               PrefService::UNSYNCABLE_PREF);
+  }
+  if (prefs->FindPreference(prefs::kVirtualKeyboardEnabled) == NULL) {
+    prefs->RegisterBooleanPref(prefs::kVirtualKeyboardEnabled,
                                false,
                                PrefService::UNSYNCABLE_PREF);
   }

chrome/browser/resources/options/chromeos/system_options.html
@@ -107,8 +107,32 @@
         <div class="option-name">
           <div class="checkbox">
             <label>
-              <input id="accesibility-check" type="checkbox">
-              <span i18n-content="accessibility"></span>
+              <input id="accessibility-spoken-feedback-check" type="checkbox">
+              <span i18n-content="accessibilitySpokenFeedback"></span>
+            </label>
+          </div>
+        </div>
+        <div class="option-name">
+          <div class="checkbox">
+            <label>
+              <input id="accessibility-high-contrast-check" type="checkbox">
+              <span i18n-content="accessibilityHighContrast"></span>
+            </label>
+          </div>
+        </div>
+        <div class="option-name">
+          <div class="checkbox">
+            <label>
+              <input id="accessibility-screen-magnifier-check" type="checkbox">
+              <span i18n-content="accessibilityScreenMagnifier"></span>
+            </label>
+          </div>
+        </div>
+        <div class="option-name">
+          <div class="checkbox">
+            <label>
+              <input id="accessibility-virtual-keyboard-check" type="checkbox">
+              <span i18n-content="accessibilityVirtualKeyboard"></span>
             </label>
           </div>
         </div>

chrome/browser/resources/options/chromeos/system_options.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -68,9 +68,21 @@ cr.define('options', function() {
       $('modifier-keys-button').onclick = function(event) {
         OptionsPage.navigateToPage('languageCustomizeModifierKeysOverlay');
       };
-      $('accesibility-check').onchange = function(event) {
-        chrome.send('accessibilityChange',
-                    [String($('accesibility-check').checked)]);
+      $('accessibility-spoken-feedback-check').onchange = function(event) {
+        chrome.send('spokenFeedbackChange',
+                    [$('accessibility-spoken-feedback-check').checked]);
+      };
+      $('accessibility-high-contrast-check').onchange = function(event) {
+        chrome.send('highContrastChange',
+                    [$('accessibility-high-contrast-check').checked]);
+      };
+      $('accessibility-screen-magnifier-check').onchange = function(event) {
+        chrome.send('screenMagnifierChange',
+                    [$('accessibility-screen-magnifier-check').checked]);
+      };
+      $('accessibility-virtual-keyboard-check').onchange = function(event) {
+        chrome.send('virtualKeyboardChange',
+                    [$('accessibility-virtual-keyboard-check').checked]);
       };
       initializeBrightnessButton_('brightness-decrease-button',
           'decreaseScreenBrightness');
@@ -111,10 +123,31 @@ cr.define('options', function() {
   //
 
   /**
-   * Set the initial state of the accessibility checkbox.
+   * Set the initial state of the spoken feedback checkbox.
    */
-  SystemOptions.SetAccessibilityCheckboxState = function(checked) {
-    $('accesibility-check').checked = checked;
+  SystemOptions.setSpokenFeedbackCheckboxState = function(checked) {
+    $('accessibility-spoken-feedback-check').checked = checked;
+  };
+
+  /**
+   * Set the initial state of the high contrast checkbox.
+   */
+  SystemOptions.setHighContrastCheckboxState = function(checked) {
+    $('accessibility-high-contrast-check').checked = checked;
+  };
+
+  /**
+   * Set the initial state of the screen magnifier checkbox.
+   */
+  SystemOptions.setScreenMagnifierCheckboxState = function(checked) {
+    $('accessibility-screen-magnifier-check').checked = checked;
+  };
+
+  /**
+   * Set the initial state of the virtual keyboard checkbox.
+   */
+  SystemOptions.setVirtualKeyboardCheckboxState = function(checked) {
+    $('accessibility-virtual-keyboard-check').checked = checked;
   };
 
   /**

commit 00e40c71c815dda3b7b95606c13a6e39f610ccbf

Fix clang/chromeos build breakage introduced by r116447.
BUG=None
TEST=Linux Clang (ChromeOS dbg)  goes green
TBR=zork@chromium.org

Review URL: http://codereview.chromium.org/9110007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116479 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/webui/options/chromeos/system_options_handler.cc
@@ -14,7 +14,6 @@
 #include "base/string_number_conversions.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
-#include "content/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/accessibility/accessibility_util.h"
 #include "chrome/browser/chromeos/cros_settings.h"
@@ -28,8 +27,9 @@
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/options/chromeos/system_settings_provider.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/common/pref_names.h"
 #include "chrome/common/extensions/extension.h"
+#include "chrome/common/pref_names.h"
+#include "content/browser/tab_contents/tab_contents.h"
 #include "grit/browser_resources.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
@@ -137,22 +137,26 @@ void SystemOptionsHandler::GetLocalizedValues(
 
 void SystemOptionsHandler::Initialize() {
   PrefService* pref_service = g_browser_process->local_state();
-  bool enabled = pref_service->GetBoolean(prefs::kSpokenFeedbackEnabled);
+  base::FundamentalValue spoken_feedback_enabled(
+      pref_service->GetBoolean(prefs::kSpokenFeedbackEnabled));
   web_ui()->CallJavascriptFunction(
       "options.SystemOptions.setSpokenFeedbackCheckboxState",
-      base::FundamentalValue(enabled));
-  enabled = pref_service->GetBoolean(prefs::kHighContrastEnabled);
+      spoken_feedback_enabled);
+  base::FundamentalValue high_contrast_enabled(
+      pref_service->GetBoolean(prefs::kHighContrastEnabled));
   web_ui()->CallJavascriptFunction(
       "options.SystemOptions.setHighContrastCheckboxState",
-      base::FundamentalValue(enabled));
-  enabled = pref_service->GetBoolean(prefs::kScreenMagnifierEnabled);
+      high_contrast_enabled);
+  base::FundamentalValue screen_magnifier_enabled(
+      pref_service->GetBoolean(prefs::kScreenMagnifierEnabled));
   web_ui()->CallJavascriptFunction(
       "options.SystemOptions.setScreenMagnifierCheckboxState",
-      base::FundamentalValue(enabled));
-  enabled = pref_service->GetBoolean(prefs::kVirtualKeyboardEnabled);
+      screen_magnifier_enabled);
+  base::FundamentalValue virtual_keyboard_enabled(
+      pref_service->GetBoolean(prefs::kVirtualKeyboardEnabled));
   web_ui()->CallJavascriptFunction(
       "options.SystemOptions.setVirtualKeyboardCheckboxState",
-      base::FundamentalValue(enabled));
+      virtual_keyboard_enabled);
 
   chromeos::XInputHierarchyChangedEventListener::GetInstance()
       ->AddObserver(this);