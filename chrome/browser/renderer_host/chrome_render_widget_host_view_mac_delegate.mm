adobe/chromium

commit cdfe4b79f3f9041bc297ca511776ce3eae1c9a5a

mac: Always do history swiping on the NTP on lion devices with touchpad.
BUG=104981
TEST=see bug
TBR=ben

Review URL: https://chromiumcodereview.appspot.com/9296040

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119701 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/renderer_host/chrome_render_widget_host_view_mac_delegate.mm
@@ -14,11 +14,13 @@
 #import "chrome/browser/ui/cocoa/history_overlay_controller.h"
 #import "chrome/browser/ui/cocoa/view_id_util.h"
 #include "chrome/common/spellcheck_messages.h"
+#include "chrome/common/url_constants.h"
 #include "content/browser/mac/closure_blocks_leopard_compat.h"
 #include "content/browser/renderer_host/render_view_host.h"
 #include "content/browser/renderer_host/render_widget_host.h"
 #include "content/browser/renderer_host/render_widget_host_view.h"
 #include "content/public/browser/render_view_host_observer.h"
+#include "content/public/browser/web_contents.h"
 
 // Declare things that are part of the 10.7 SDK.
 #if !defined(MAC_OS_X_VERSION_10_7) || \
@@ -136,7 +138,7 @@ class SpellCheckRenderViewObserver : public content::RenderViewHostObserver {
 }
 
 - (void)gotUnhandledWheelEvent {
-  gotUnhandledWheelEvent_ = true;
+  gotUnhandledWheelEvent_ = YES;
 }
 
 - (void)scrollOffsetPinnedToLeft:(BOOL)left toRight:(BOOL)right {
@@ -160,7 +162,7 @@ class SpellCheckRenderViewObserver : public content::RenderViewHostObserver {
   // swiping if they come back unhandled.
   if ([theEvent phase] == NSEventPhaseBegan) {
     totalScrollDelta_ = NSZeroSize;
-    gotUnhandledWheelEvent_ = false;
+    gotUnhandledWheelEvent_ = NO;
   }
 
   if (!render_widget_host_ || !render_widget_host_->IsRenderView())
@@ -169,6 +171,20 @@ class SpellCheckRenderViewObserver : public content::RenderViewHostObserver {
       static_cast<RenderViewHost*>(render_widget_host_));
   if (isDevtoolsRwhv)
     return NO;
+
+  // TODO(thakis): This is wrong, as it will navigate the frontmost browser
+  // for swipe events on background windows. It also navigates the frontmost
+  // browser for swipes on extensions popups. And it's a dependency problem,
+  // too. http://crbug.com/102541
+  Browser* browser = BrowserList::GetLastActive();
+
+  if (browser && [NSEvent isSwipeTrackingFromScrollEventsEnabled]) {
+    content::WebContents* contents = browser->GetSelectedWebContents();
+    if (contents && contents->GetURL() == GURL(chrome::kChromeUINewTabURL)) {
+      // Always do history navigation on the NTP if it's enabled.
+      gotUnhandledWheelEvent_ = YES;
+    }
+  }
 
   if (gotUnhandledWheelEvent_ &&
       [NSEvent isSwipeTrackingFromScrollEventsEnabled] &&
@@ -182,7 +198,7 @@ class SpellCheckRenderViewObserver : public content::RenderViewHostObserver {
     bool isRightScroll = [theEvent scrollingDeltaX] < 0;
     bool goForward = isRightScroll;
     bool canGoBack = false, canGoForward = false;
-    if (Browser* browser = BrowserList::GetLastActive()) {
+    if (browser) {
       canGoBack = browser->CanGoBack();
       canGoForward = browser->CanGoForward();
     }
@@ -263,7 +279,7 @@ class SpellCheckRenderViewObserver : public content::RenderViewHostObserver {
       return YES;
     }
   }
-  return NO;
+  return gotUnhandledWheelEvent_;
 }
 
 - (BOOL)validateUserInterfaceItem:(id<NSValidatedUserInterfaceItem>)item

chrome/browser/ui/browser.cc
@@ -1493,8 +1493,7 @@ bool Browser::IsClosingPermitted() {
 }
 
 bool Browser::CanGoBack() const {
-  return GetSelectedTabContentsWrapper()->
-      web_contents()->GetController().CanGoBack();
+  return GetSelectedWebContents()->GetController().CanGoBack();
 }
 
 void Browser::GoBack(WindowOpenDisposition disposition) {
@@ -1513,8 +1512,7 @@ void Browser::GoBack(WindowOpenDisposition disposition) {
 }
 
 bool Browser::CanGoForward() const {
-  return GetSelectedTabContentsWrapper()->
-      web_contents()->GetController().CanGoForward();
+  return GetSelectedWebContents()->GetController().CanGoForward();
 }
 
 void Browser::GoForward(WindowOpenDisposition disposition) {

commit 1e819d952774d0d3ec83abba66d8f2b3794172fd

mac: Fix iframe scrolling regression introduced in r119701
BUG=112085
TEST=see bug. swiping on ntp still doesn't do pane switching.
TBR=rsesek

Review URL: https://chromiumcodereview.appspot.com/9311002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119918 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/renderer_host/chrome_render_widget_host_view_mac_delegate.mm
@@ -167,17 +167,19 @@ class SpellCheckRenderViewObserver : public content::RenderViewHostObserver {
 
   if (!render_widget_host_ || !render_widget_host_->IsRenderView())
     return NO;
-  bool isDevtoolsRwhv = DevToolsWindow::IsDevToolsWindow(
-      static_cast<RenderViewHost*>(render_widget_host_));
-  if (isDevtoolsRwhv)
+  if (DevToolsWindow::IsDevToolsWindow(
+      static_cast<RenderViewHost*>(render_widget_host_))) {
     return NO;
+  }
 
+  BOOL suppressWheelEvent = NO;
   Browser* browser = BrowserList::FindBrowserWithWindow([theEvent window]);
   if (browser && [NSEvent isSwipeTrackingFromScrollEventsEnabled]) {
     content::WebContents* contents = browser->GetSelectedWebContents();
     if (contents && contents->GetURL() == GURL(chrome::kChromeUINewTabURL)) {
       // Always do history navigation on the NTP if it's enabled.
       gotUnhandledWheelEvent_ = YES;
+      suppressWheelEvent = YES;
     }
   }
 
@@ -275,7 +277,7 @@ class SpellCheckRenderViewObserver : public content::RenderViewHostObserver {
       return YES;
     }
   }
-  return gotUnhandledWheelEvent_;
+  return suppressWheelEvent;
 }
 
 - (BOOL)validateUserInterfaceItem:(id<NSValidatedUserInterfaceItem>)item