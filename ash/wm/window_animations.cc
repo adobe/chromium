adobe/chromium

commit bbf2f2ca50d5423006cfd33a461cef8d930e1298

Adds a new layer animation element which owns an interpolated transform. This allows more control over the interpolation, and in particular, it allows for a rotation about a pivot.
BUG=None
TEST=None


Review URL: http://codereview.chromium.org/9704070

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127269 0039d316-1c4b-4281-b951-d872f2087c98

ash/wm/window_animations.cc
@@ -18,8 +18,10 @@
 #include "ui/aura/window_observer.h"
 #include "ui/aura/window_property.h"
 #include "ui/gfx/compositor/layer_animation_observer.h"
+#include "ui/gfx/compositor/layer_animation_sequence.h"
 #include "ui/gfx/compositor/layer_animator.h"
 #include "ui/gfx/compositor/scoped_layer_animation_settings.h"
+#include "ui/gfx/interpolated_transform.h"
 #include "ui/gfx/screen.h"
 
 DECLARE_WINDOW_PROPERTY_TYPE(int)
@@ -72,7 +74,7 @@ const float kWindowAnimation_HideOpacity = 0.f;
 const float kWindowAnimation_ShowOpacity = 1.f;
 const float kWindowAnimation_TranslateFactor = -0.025f;
 const float kWindowAnimation_ScaleFactor = 1.05f;
-const float kWindowAnimation_MinimizeRotate = -90.f;
+const float kWindowAnimation_MinimizeRotate = -5.f;
 
 const float kWindowAnimation_Vertical_TranslateY = 15.f;
 
@@ -344,18 +346,59 @@ gfx::Rect GetMinimizeRectForWindow(aura::Window* window) {
   return target_bounds;
 }
 
-void AnimateShowWindow_Minimize(aura::Window* window) {
+void AddLayerAnimationsForMinimize(aura::Window* window, bool show) {
   // Recalculate the transform at restore time since the launcher item may have
   // moved while the window was minimized.
+  gfx::Rect bounds = window->bounds();
   gfx::Rect target_bounds = GetMinimizeRectForWindow(window);
-  ui::Transform transform;
-  transform.ConcatScale(
-      static_cast<float>(target_bounds.height()) / window->bounds().width(),
-      static_cast<float>(target_bounds.width()) / window->bounds().height());
-  transform.ConcatTranslate(target_bounds.x() - window->bounds().x(),
-                            target_bounds.y() - window->bounds().y());
+  float scale_x = static_cast<float>(target_bounds.height()) / bounds.width();
+  float scale_y = static_cast<float>(target_bounds.width()) / bounds.height();
 
-  AnimateShowWindowCommon(window, transform, ui::Transform());
+  scoped_ptr<ui::InterpolatedTransform> scale(
+      new ui::InterpolatedScale(gfx::Point3f(1, 1, 1),
+                                gfx::Point3f(scale_x, scale_y, 1)));
+
+  scoped_ptr<ui::InterpolatedTransform> translation(
+      new ui::InterpolatedTranslation(
+          gfx::Point(),
+          gfx::Point(target_bounds.x() - bounds.x(),
+                     target_bounds.y() - bounds.y())));
+
+  scoped_ptr<ui::InterpolatedTransform> rotation(
+      new ui::InterpolatedRotation(0, kWindowAnimation_MinimizeRotate));
+
+  scoped_ptr<ui::InterpolatedTransform> rotation_about_pivot(
+      new ui::InterpolatedTransformAboutPivot(
+          gfx::Point(bounds.width() * 0.5, bounds.height() * 0.5),
+          rotation.release()));
+
+  scale->SetChild(translation.release());
+  rotation_about_pivot->SetChild(scale.release());
+
+  rotation_about_pivot->SetReversed(show);
+
+  base::TimeDelta duration = base::TimeDelta::FromMilliseconds(350);
+
+  scoped_ptr<ui::LayerAnimationElement> transition(
+      ui::LayerAnimationElement::CreateInterpolatedTransformElement(
+          rotation_about_pivot.release(), duration));
+
+  transition->set_tween_type(
+      show ? ui::Tween::EASE_IN : ui::Tween::EASE_IN_OUT);
+
+  window->layer()->GetAnimator()->ScheduleAnimation(
+      new ui::LayerAnimationSequence(transition.release()));
+
+  bool opacity = show ? 1.0f : 0.0f;
+  window->layer()->GetAnimator()->ScheduleAnimation(
+      new ui::LayerAnimationSequence(
+          ui::LayerAnimationElement::CreateOpacityElement(opacity, duration)));
+}
+
+void AnimateShowWindow_Minimize(aura::Window* window) {
+  window->layer()->set_delegate(window);
+  window->layer()->SetOpacity(kWindowAnimation_HideOpacity);
+  AddLayerAnimationsForMinimize(window, true);
 
   // Now that the window has been restored, we need to clear its animation style
   // to default so that normal animation applies.
@@ -364,14 +407,13 @@ void AnimateShowWindow_Minimize(aura::Window* window) {
 }
 
 void AnimateHideWindow_Minimize(aura::Window* window) {
-  gfx::Rect target_bounds = GetMinimizeRectForWindow(window);
-  ui::Transform transform;
-  transform.ConcatScale(
-      static_cast<float>(target_bounds.height()) / window->bounds().width(),
-      static_cast<float>(target_bounds.width()) / window->bounds().height());
-  transform.ConcatTranslate(target_bounds.x() - window->bounds().x(),
-                            target_bounds.y() - window->bounds().y());
-  AnimateHideWindowCommon(window, transform);
+  window->layer()->set_delegate(NULL);
+
+  // Property sets within this scope will be implicitly animated.
+  ui::ScopedLayerAnimationSettings settings(window->layer()->GetAnimator());
+  settings.AddObserver(new HidingWindowAnimationObserver(window));
+
+  AddLayerAnimationsForMinimize(window, false);
 }
 
 bool AnimateShowWindow(aura::Window* window) {

ui/gfx/compositor/layer_animation_element.cc
@@ -7,6 +7,7 @@
 #include "base/compiler_specific.h"
 #include "ui/base/animation/tween.h"
 #include "ui/gfx/compositor/layer_animation_delegate.h"
+#include "ui/gfx/interpolated_transform.h"
 
 namespace ui {
 
@@ -70,6 +71,45 @@ class TransformTransition : public LayerAnimationElement {
   const Transform target_;
 
   DISALLOW_COPY_AND_ASSIGN(TransformTransition);
+};
+
+// InterpolatedTransformTransition ---------------------------------------------
+
+class InterpolatedTransformTransition : public LayerAnimationElement {
+ public:
+  InterpolatedTransformTransition(InterpolatedTransform* interpolated_transform,
+                                  base::TimeDelta duration)
+      : LayerAnimationElement(GetProperties(), duration),
+        interpolated_transform_(interpolated_transform) {
+  }
+  virtual ~InterpolatedTransformTransition() {}
+
+ protected:
+  virtual void OnStart(LayerAnimationDelegate* delegate) OVERRIDE {
+  }
+
+  virtual bool OnProgress(double t, LayerAnimationDelegate* delegate) OVERRIDE {
+    delegate->SetTransformFromAnimation(
+        interpolated_transform_->Interpolate(static_cast<float>(t)));
+    return true;
+  }
+
+  virtual void OnGetTarget(TargetValue* target) const OVERRIDE {
+    target->transform = interpolated_transform_->Interpolate(1.0f);
+  }
+
+  virtual void OnAbort() OVERRIDE {}
+
+ private:
+  static AnimatableProperties GetProperties() {
+    AnimatableProperties properties;
+    properties.insert(LayerAnimationElement::TRANSFORM);
+    return properties;
+  }
+
+  scoped_ptr<InterpolatedTransform> interpolated_transform_;
+
+  DISALLOW_COPY_AND_ASSIGN(InterpolatedTransformTransition);
 };
 
 // BoundsTransition ------------------------------------------------------------
@@ -244,6 +284,13 @@ void LayerAnimationElement::Abort() {
 LayerAnimationElement* LayerAnimationElement::CreateTransformElement(
     const Transform& transform, base::TimeDelta duration) {
   return new TransformTransition(transform, duration);
+}
+
+// static
+LayerAnimationElement*
+LayerAnimationElement::CreateInterpolatedTransformElement(
+    InterpolatedTransform* interpolated_transform, base::TimeDelta duration) {
+  return new InterpolatedTransformTransition(interpolated_transform, duration);
 }
 
 // static

ui/gfx/compositor/layer_animation_element.h
@@ -16,6 +16,7 @@
 
 namespace ui {
 
+class InterpolatedTransform;
 class LayerAnimationDelegate;
 class Transform;
 
@@ -52,6 +53,17 @@ class COMPOSITOR_EXPORT LayerAnimationElement {
   // the return value.
   static LayerAnimationElement* CreateTransformElement(
       const Transform& transform,
+      base::TimeDelta duration);
+
+  // Creates an element that transitions to another in a way determined by an
+  // interpolated transform. The element accepts ownership of the interpolated
+  // transform. NB: at every step, the interpolated transform clobbers the
+  // existing transform. That is, it does not interpolate between the existing
+  // transform and the last value the interpolated transform will assume. It is
+  // therefore important that the value of the interpolated at time 0 matches
+  // the current transform.
+  static LayerAnimationElement* CreateInterpolatedTransformElement(
+      InterpolatedTransform* interpolated_transform,
       base::TimeDelta duration);
 
   // Creates an element that transitions to the given bounds. The caller owns

ui/gfx/interpolated_transform.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -27,18 +27,22 @@ namespace ui {
 
 InterpolatedTransform::InterpolatedTransform()
     : start_time_(0.0f),
-      end_time_(1.0f) {
+      end_time_(1.0f),
+      reversed_(false) {
 }
 
 InterpolatedTransform::InterpolatedTransform(float start_time,
                                              float end_time)
     : start_time_(start_time),
-      end_time_(end_time) {
+      end_time_(end_time),
+      reversed_(false) {
 }
 
 InterpolatedTransform::~InterpolatedTransform() {}
 
 ui::Transform InterpolatedTransform::Interpolate(float t) const {
+  if (reversed_)
+    t = 1.0f - t;
   ui::Transform result = InterpolateButDoNotCompose(t);
   if (child_.get()) {
     result.ConcatTransform(child_->Interpolate(t));
@@ -154,6 +158,41 @@ InterpolatedRotation::~InterpolatedRotation() {}
 ui::Transform InterpolatedRotation::InterpolateButDoNotCompose(float t) const {
   ui::Transform result;
   result.SetRotate(ValueBetween(t, start_degrees_, end_degrees_));
+  return result;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// InterpolatedAxisAngleRotation
+//
+
+InterpolatedAxisAngleRotation::InterpolatedAxisAngleRotation(
+    gfx::Point3f axis,
+    float start_degrees,
+    float end_degrees)
+    : InterpolatedTransform(),
+      axis_(axis),
+      start_degrees_(start_degrees),
+      end_degrees_(end_degrees) {
+}
+
+InterpolatedAxisAngleRotation::InterpolatedAxisAngleRotation(
+    gfx::Point3f axis,
+    float start_degrees,
+    float end_degrees,
+    float start_time,
+    float end_time)
+    : InterpolatedTransform(start_time, end_time),
+      axis_(axis),
+      start_degrees_(start_degrees),
+      end_degrees_(end_degrees) {
+}
+
+InterpolatedAxisAngleRotation::~InterpolatedAxisAngleRotation() {}
+
+ui::Transform
+InterpolatedAxisAngleRotation::InterpolateButDoNotCompose(float t) const {
+  ui::Transform result;
+  result.SetRotateAbout(axis_, ValueBetween(t, start_degrees_, end_degrees_));
   return result;
 }
 

ui/gfx/interpolated_transform.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -42,6 +42,11 @@ class UI_EXPORT InterpolatedTransform {
   // This function takes ownership of the passed InterpolatedTransform.
   void SetChild(InterpolatedTransform* child);
 
+  // If the interpolated transform is reversed, Interpolate(t) will return
+  // Interpolate(1 - t)
+  void SetReversed(bool reversed) { reversed_ = reversed; }
+  bool Reversed() const { return reversed_; }
+
   static bool FactorTRS(const ui::Transform& transform,
                         gfx::Point* translation,
                         float* rotation,
@@ -72,6 +77,8 @@ class UI_EXPORT InterpolatedTransform {
   // transform, we can chain collections of transforms together.
   scoped_ptr<InterpolatedTransform> child_;
 
+  bool reversed_;
+
   DISALLOW_COPY_AND_ASSIGN(InterpolatedTransform);
 };
 
@@ -98,6 +105,35 @@ class UI_EXPORT InterpolatedRotation : public InterpolatedTransform {
   const float end_degrees_;
 
   DISALLOW_COPY_AND_ASSIGN(InterpolatedRotation);
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// class InterpolatedAxisAngleRotation
+//
+// Represents an animated rotation.
+//
+///////////////////////////////////////////////////////////////////////////////
+class UI_EXPORT InterpolatedAxisAngleRotation : public InterpolatedTransform {
+ public:
+  InterpolatedAxisAngleRotation(gfx::Point3f axis,
+                                float start_degrees,
+                                float end_degrees);
+  InterpolatedAxisAngleRotation(gfx::Point3f axis,
+                                float start_degrees,
+                                float end_degrees,
+                                float start_time,
+                                float end_time);
+  virtual ~InterpolatedAxisAngleRotation();
+
+ protected:
+  virtual ui::Transform InterpolateButDoNotCompose(float t) const OVERRIDE;
+
+ private:
+  gfx::Point3f axis_;
+  const float start_degrees_;
+  const float end_degrees_;
+
+  DISALLOW_COPY_AND_ASSIGN(InterpolatedAxisAngleRotation);
 };
 
 ///////////////////////////////////////////////////////////////////////////////

ui/gfx/transform.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -38,6 +38,13 @@ void Transform::SetRotate(float degree) {
   matrix_.setRotateDegreesAbout(0, 0, 1, SkFloatToScalar(degree));
 }
 
+void Transform::SetRotateAbout(const gfx::Point3f& axis, float degree) {
+  matrix_.setRotateDegreesAbout(axis.x(),
+                                axis.y(),
+                                axis.z(),
+                                SkFloatToScalar(degree));
+}
+
 void Transform::SetScaleX(float x) {
   matrix_.set(0, 0, SkFloatToScalar(x));
 }
@@ -47,10 +54,9 @@ void Transform::SetScaleY(float y) {
 }
 
 void Transform::SetScale(float x, float y) {
-  matrix_.setScale(
-    SkFloatToScalar(x),
-    SkFloatToScalar(y),
-    matrix_.get(2, 2));
+  matrix_.setScale(SkFloatToScalar(x),
+                   SkFloatToScalar(y),
+                   matrix_.get(2, 2));
 }
 
 void Transform::SetTranslateX(float x) {
@@ -62,15 +68,23 @@ void Transform::SetTranslateY(float y) {
 }
 
 void Transform::SetTranslate(float x, float y) {
-  matrix_.setTranslate(
-    SkFloatToScalar(x),
-    SkFloatToScalar(y),
-    matrix_.get(2, 3));
+  matrix_.setTranslate(SkFloatToScalar(x),
+                       SkFloatToScalar(y),
+                       matrix_.get(2, 3));
 }
 
 void Transform::ConcatRotate(float degree) {
   SkMatrix44 rot;
   rot.setRotateDegreesAbout(0, 0, 1, SkFloatToScalar(degree));
+  matrix_.postConcat(rot);
+}
+
+void Transform::ConcatRotateAbout(const gfx::Point3f& axis, float degree) {
+  SkMatrix44 rot;
+  rot.setRotateDegreesAbout(axis.x(),
+                            axis.y(),
+                            axis.z(),
+                            SkFloatToScalar(degree));
   matrix_.postConcat(rot);
 }
 

ui/gfx/transform.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -39,6 +39,9 @@ class UI_EXPORT Transform {
   // Sets the rotation of the transformation.
   void SetRotate(float degree);
 
+  // Sets the rotation of the transform (about a vector).
+  void SetRotateAbout(const gfx::Point3f& point, float degree);
+
   // Sets the scaling parameters.
   void SetScaleX(float x);
   void SetScaleY(float y);
@@ -49,8 +52,11 @@ class UI_EXPORT Transform {
   void SetTranslateY(float y);
   void SetTranslate(float x, float y);
 
-  // Applies rotation on the current transformation.
+  // Applies a rotation on the current transformation.
   void ConcatRotate(float degree);
+
+  // Applies an axis-angle rotation on the current transformation.
+  void ConcatRotateAbout(const gfx::Point3f& point, float degree);
 
   // Applies scaling on current transform.
   void ConcatScale(float x, float y);

commit 33fa5c6b7ecd9b415454d625c30a31198f196a80

Fix Win Aura compile break caused by r127269.
Change bool to float.

TBR=vollick
BUG=no
TEST=no

Review URL: https://chromiumcodereview.appspot.com/9719007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127280 0039d316-1c4b-4281-b951-d872f2087c98

ash/wm/window_animations.cc
@@ -389,7 +389,7 @@ void AddLayerAnimationsForMinimize(aura::Window* window, bool show) {
   window->layer()->GetAnimator()->ScheduleAnimation(
       new ui::LayerAnimationSequence(transition.release()));
 
-  bool opacity = show ? 1.0f : 0.0f;
+  float opacity = show ? 1.0f : 0.0f;
   window->layer()->GetAnimator()->ScheduleAnimation(
       new ui::LayerAnimationSequence(
           ui::LayerAnimationElement::CreateOpacityElement(opacity, duration)));